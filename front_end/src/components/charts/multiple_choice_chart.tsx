1ab023ea243a7fedd3e502799c314f7cba5adc42
Nikita <nikita.develops.work@gmail.com>
2024-05-27 15:01:06 +0300

    feat: basic cursor rendering

--- front_end/src/components/charts/multiple_choice_chart.tsx
+++ front_end/src/components/charts/multiple_choice_chart.tsx
@@ -1,16 +1,21 @@
 "use client";
-import React, { FC, useMemo } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 import {
+  CursorCoordinatesPropType,
+  LineSegment,
   VictoryAxis,
   VictoryChart,
-  VictoryGroup,
+  VictoryCursorContainer,
   VictoryLabel,
+  VictoryLabelProps,
   VictoryLine,
 } from "victory";
 
+import ChartCursorLabel from "@/components/chart_cursor_label";
 import chartTheme from "@/contants/chart_theme";
 import { METAC_COLORS } from "@/contants/colors";
 import useContainerSize from "@/hooks/use_container_size";
+import usePrevious from "@/hooks/use_previous";
 import { BaseChartData, Line, MultipleChoiceDataset } from "@/types/charts";
 import {
   generateNumericDomain,
@@ -26,43 +31,138 @@ type Props = {
   dataset: MultipleChoiceDataset;
   height?: number;
   yLabel?: string;
+  onCursorChange?: (value: number) => void;
+  onChartReady?: () => void;
 };
 
-const MultipleChoiceChart: FC<Props> = ({ dataset, height = 150, yLabel }) => {
+const MultipleChoiceChart: FC<Props> = ({
+  dataset,
+  height = 150,
+  yLabel,
+  onCursorChange,
+  onChartReady,
+}) => {
   const {
     ref: chartContainerRef,
     width: chartWidth,
     height: chartHeight,
   } = useContainerSize<HTMLDivElement>();
+
+  const defaultCursor = dataset.timestamps[dataset.timestamps.length - 1];
+  const [isCursorActive, setIsCursorActive] = useState(false);
+
   const { xScale, yScale, lines } = useMemo(
     () => buildChartData(dataset, chartWidth, chartHeight),
     [dataset, chartWidth, chartHeight]
   );
 
+  const prevWidth = usePrevious(chartWidth);
+  useEffect(() => {
+    if (!prevWidth && chartWidth && onChartReady) {
+      onChartReady();
+    }
+  }, [onChartReady, prevWidth, chartWidth]);
+
   return (
     <div ref={chartContainerRef} className="w-full h-full">
-      <VictoryChart width={chartWidth} height={height} theme={chartTheme}>
-        <VictoryGroup colorScale={COLOR_SCALE}>
-          {lines.map((line, index) => (
-            <VictoryLine key={`multiple-choice-line-${index}`} data={line} />
+      {!!chartWidth && (
+        <VictoryChart
+          width={chartWidth}
+          height={height}
+          theme={chartTheme}
+          events={[
+            {
+              target: "parent",
+              eventHandlers: {
+                onMouseOverCapture: () => {
+                  setIsCursorActive(true);
+                },
+                onMouseOutCapture: () => {
+                  setIsCursorActive(false);
+                },
+              },
+            },
+          ]}
+          containerComponent={
+            <VictoryCursorContainer
+              cursorDimension={"x"}
+              defaultCursorValue={defaultCursor}
+              cursorLabelOffset={{
+                x: 0,
+                y: 0,
+              }}
+              cursorLabel={({ datum }: VictoryLabelProps) => {
+                if (datum) {
+                  return datum.x === defaultCursor
+                    ? "now"
+                    : `${xScale.tickFormat(datum.x)}`;
+                }
+              }}
+              cursorComponent={
+                <LineSegment
+                  style={{
+                    stroke: METAC_COLORS.gray["600"].DEFAULT,
+                    strokeDasharray: "2,1",
+                  }}
+                />
+              }
+              cursorLabelComponent={
+                <ChartCursorLabel positionY={height - 10} />
+              }
+              onCursorChange={(value: CursorCoordinatesPropType) => {
+                if (typeof value === "number" && onCursorChange) {
+                  const closestTimestamp = dataset.timestamps.reduce(
+                    (prev, curr) =>
+                      Math.abs(curr - value) < Math.abs(prev - value)
+                        ? curr
+                        : prev
+                  );
+
+                  onCursorChange(closestTimestamp);
+                }
+              }}
+            />
+          }
+        >
+          {lines.map(({ line, color }, index) => (
+            <VictoryLine
+              key={`multiple-choice-line-${index}`}
+              data={line}
+              style={{
+                data: { stroke: color },
+              }}
+            />
           ))}
-        </VictoryGroup>
-        <VictoryAxis
-          dependentAxis
-          tickValues={yScale.ticks}
-          tickFormat={yScale.tickFormat}
-          style={{ tickLabels: { padding: 2 } }}
-          label={yLabel}
-          axisLabelComponent={<VictoryLabel dy={-10} />}
-        />
-        <VictoryAxis tickValues={xScale.ticks} tickFormat={xScale.tickFormat} />
-      </VictoryChart>
+          <VictoryAxis
+            dependentAxis
+            tickValues={yScale.ticks}
+            tickFormat={yScale.tickFormat}
+            style={{ tickLabels: { padding: 2 } }}
+            label={yLabel}
+            axisLabelComponent={<VictoryLabel dy={-10} />}
+          />
+          <VictoryAxis
+            tickValues={xScale.ticks}
+            tickFormat={isCursorActive ? () => "" : xScale.tickFormat}
+          />
+        </VictoryChart>
+      )}
+      <div className="mt-3 flex flex-wrap items-center justify-center gap-3 text-xs font-normal">
+        {lines.map(({ label, color }) => (
+          <div key={`option-${label}`}>{label}</div>
+        ))}
+      </div>
     </div>
   );
 };
 
+type ChoiceLine = {
+  line: Line;
+  label: string;
+  color: string;
+};
 type ChartData = BaseChartData & {
-  lines: Line[];
+  lines: ChoiceLine[];
 };
 
 function buildChartData(
@@ -72,15 +172,17 @@ function buildChartData(
 ): ChartData {
   const { timestamps, nr_forecasters, ...choices } = dataset;
 
-  const lines: Line[] = [];
-  for (const choiceValues of Object.values(choices)) {
-    lines.push(
-      timestamps.map((timestamp, timestampIndex) => ({
+  const lines: ChoiceLine[] = [];
+  Object.entries(choices).forEach(([choiceLabel, choiceValues], index) => {
+    lines.push({
+      label: choiceLabel,
+      line: timestamps.map((timestamp, timestampIndex) => ({
         x: timestamp,
         y: choiceValues[timestampIndex],
-      }))
-    );
-  }
+      })),
+      color: COLOR_SCALE[index],
+    });
+  });
 
   const xDomain = generateNumericDomain(timestamps);
 
