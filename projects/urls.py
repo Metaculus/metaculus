a81ac7eb5ac456c9a0fa9089ca20c6b467d2a519
gleb <alexxkur@gmail.com>
2024-05-27 23:23:14 +0200

    Added project endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("projects/topics", views.topics_list_api_view),
     path("projects/categories", views.categories_list_api_view),
     path("projects/tournaments", views.tournaments_list_api_view),
+    path("projects/tags", views.tags_list_api_view),
 ]
