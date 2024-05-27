db171d6811c99e668341e79e75f1a201232ca424
gleb <alexxkur@gmail.com>
2024-05-27 21:34:37 +0200

    Small changes

--- tests/test_questions/test_views.py
+++ tests/test_questions/test_views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 
 from questions.models import Question
 from tests.fixtures import *  # noqa
+from tests.test_projects.fixtures import *  # noqa
 
 
 def test_question_list(anon_client):
@@ -15,19 +16,6 @@ def test_question_list(anon_client):
 def test_question_detail(anon_client):
     url = f"/questions/{Question.objects.first().pk}/"
     response = anon_client.get(url)
-
-    assert response.status_code == status.HTTP_200_OK
-    assert isinstance(response.data, dict)
-
-
-def test_create_question(user1_client):
-    url = "/questions/create/"
-    data = {
-        "title": "New Question",
-        "description": "This is a new question",
-        "type": "binary",
-    }
-    response = user1_client.post(url, data)
 
     assert response.status_code == status.HTTP_200_OK
     assert isinstance(response.data, dict)
@@ -51,3 +39,24 @@ def test_delete_question(user1_client):
     response = user1_client.delete(url)
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+class TestCreateQuestionView:
+    url = "/questions/create/"
+
+    def test_happy_path(self, user1_client, tag1):
+        data = {
+            "title": "New Question",
+            "description": "This is a new question",
+            "type": "binary",
+            "projects": {"tag": [tag1.id]},
+        }
+        response = user1_client.post(self.url, data, format="json")
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert isinstance(response.data, dict)
+
+        # Ensure project relations
+        question = Question.objects.get(pk=response.data["id"])
+
+        assert question.projects.all()
