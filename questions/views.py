2e043784aaf0a5d8935cac1b9e33111c5374bcc0
gleb <alexxkur@gmail.com>
2024-05-27 20:33:09 +0200

    Serialization validation adjustments

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.request import Request
-from rest_framework import status, filters
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from questions.models import Question
 from questions.serializers import QuestionSerializer
-from rest_framework.permissions import AllowAny
 
 
 @api_view(["POST"])
@@ -43,10 +43,10 @@ def question_detail(request: Request, pk):
 @api_view(["POST"])
 def create_question(request):
     serializer = QuestionSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(author=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+
+    serializer.save(author=request.user)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["PUT"])
@@ -56,10 +56,10 @@ def update_question(request, pk):
         return Response(status=status.HTTP_403_FORBIDDEN)
 
     serializer = QuestionSerializer(question, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data)
 
 
 @api_view(["DELETE"])
