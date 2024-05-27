34c71ead23aef507c62f290af63699ea3e365697
gleb <alexxkur@gmail.com>
2024-05-27 23:08:10 +0200

    Added project endpoints

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("projects/topics", views.topics_list_api_view),
+    path("projects/categories", views.categories_list_api_view),
+]
