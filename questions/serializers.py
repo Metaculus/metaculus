6e0bebe2ea1c15f5724fa3ad0b48396d13834be7
gleb <alexxkur@gmail.com>
2024-05-27 20:30:14 +0200

    Added projects serialization

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+
+from projects.serializers import serialize_projects
 from .models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    projects = serializers.SerializerMethodField()
+
     class Meta:
         model = Question
         fields = "__all__"
+
+    def get_projects(self, obj: Question):
+        return serialize_projects(obj.projects.all())
