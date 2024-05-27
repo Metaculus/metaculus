19e95c72f93d5541edc56bb8cef16603ae12790d
gleb <alexxkur@gmail.com>
2024-05-27 23:08:10 +0200

    Added project endpoints

---
+++ projects/views.py
@@ -0,0 +1,38 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from projects.models import Project
+from projects.serializers import TopicSerializer, CategorySerializer
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def topics_list_api_view(request: Request):
+    qs = Project.objects.filter_topic().filter_active().annotate_questions_count()
+
+    data = [
+        {**TopicSerializer(obj).data, "questions_count": obj.questions_count}
+        for obj in qs.all()
+    ]
+
+    return Response(data)
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def categories_list_api_view(request: Request):
+    qs = (
+        Project.objects.filter_category()
+        .filter_active()
+        .annotate_questions_count()
+        .order_by("-questions_count")
+    )
+
+    data = [
+        {**CategorySerializer(obj).data, "questions_count": obj.questions_count}
+        for obj in qs.all()
+    ]
+
+    return Response(data)
