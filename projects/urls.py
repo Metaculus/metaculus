e3f2075c0497a94ac16e62adf1c358de884c5ebf
gleb <alexxkur@gmail.com>
2024-05-27 23:10:49 +0200

    Added project endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("projects/topics", views.topics_list_api_view),
     path("projects/categories", views.categories_list_api_view),
+    path("projects/tournaments", views.tournaments_list_api_view),
 ]
