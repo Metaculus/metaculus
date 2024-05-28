2782e97dd1774dbc74f5cd64d9437786b314b78f
gleb <alexxkur@gmail.com>
2024-05-28 21:39:58 +0200

    Refactored

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,5 +1,6 @@
 from django.db import models
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from projects.models import Project
 from projects.serializers import serialize_projects
@@ -36,20 +37,40 @@ class QuestionFilterSerializer(serializers.Serializer):
         RESOLVED_AT = "resolved_at", "Resolved At"
         CREATED_AT = "created_at", "Created At"
 
-    topic = serializers.PrimaryKeyRelatedField(
-        queryset=Project.objects.filter_topic().filter_active(), required=False
-    )
-    tags = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Project.objects.filter_tags().filter_active(),
-        required=False,
-    )
-    categories = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Project.objects.filter_category().filter_active(),
-        required=False,
-    )
+    topic = serializers.CharField(required=False)
+    tags = serializers.ListField(child=serializers.CharField(), required=False)
+    categories = serializers.ListField(child=serializers.CharField(), required=False)
     answered_by_me = serializers.BooleanField(required=False, allow_null=True)
-    order = serializers.ChoiceField(choices=Order.choices, required=False, allow_null=True)
+    order = serializers.ChoiceField(
+        choices=Order.choices, required=False, allow_null=True
+    )
 
     search = serializers.CharField(required=False, allow_null=True)
+
+    def validate_topic(self, value: str):
+        try:
+            return Project.objects.filter_topic().filter_active().get(slug=value)
+        except Project.DoesNotExist:
+            raise ValidationError("Slug does not exist")
+
+    def validate_tags(self, values: list[str]):
+        tags = Project.objects.filter_tags().filter_active().filter(slug__in=values)
+        slugs = {obj.slug for obj in tags}
+
+        for value in values:
+            if value not in slugs:
+                raise ValidationError(f"Tag {value} does not exist")
+
+        return tags
+
+    def validate_categories(self, values: list[str]):
+        categories = (
+            Project.objects.filter_category().filter_active().filter(slug__in=values)
+        )
+        slugs = {obj.slug for obj in categories}
+
+        for value in values:
+            if value not in slugs:
+                raise ValidationError(f"Category {value} does not exist")
+
+        return categories
