55dbd2e16043a3e11680883b0e94269937f432d9
Nikita <93587872+ncarazon@users.noreply.github.com>
2024-05-28 19:31:11 +0300

    feat: basic header setup

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -3,18 +3,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <Link
-        href={"/questions"}
-        className={"font-bold text-metac-blue-800 hover:opacity-60"}
-      >
-        Go to Questions
-      </Link>
-      <Link
-        href={"/questions/120"}
-        className={"font-bold text-metac-blue-800 hover:opacity-60"}
-      >
-        Go to question 120
-      </Link>
+      <ul>
+        <li>
+          <Link
+            href={"/questions"}
+            className={"font-bold text-metac-blue-800 hover:opacity-60"}
+          >
+            Go to Questions
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/questions/120"}
+            className={"font-bold text-metac-blue-800 hover:opacity-60"}
+          >
+            Go to question 120
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
