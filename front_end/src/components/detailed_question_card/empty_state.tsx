b2923b7b55ede06dc8d89e20876fc7aa681d3082
Nikita <nikita.develops.work@gmail.com>
2024-05-29 18:37:40 +0300

    feat: better empty states handling in detailed chart card

---
+++ front_end/src/components/detailed_question_card/empty_state.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+const DetailsQuestionCardEmptyState: FC = () => {
+  // TODO: implement actual UI and use translated text
+  return <div>Empty data</div>;
+};
+
+export default DetailsQuestionCardEmptyState;
