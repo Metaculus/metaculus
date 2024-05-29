33bfcf148eca463436145e620eebe7f151e38c5c
gleb <alexxkur@gmail.com>
2024-05-29 19:10:43 +0200

    - Added `send_email_with_template` util - Added activation email

---
+++ utils/email.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+
+def send_email_with_template(to: str, subject: str, template_name: str, context=None):
+    convert_to_html_content = render_to_string(
+        template_name=template_name, context=context
+    )
+    plain_message = strip_tags(convert_to_html_content)
+
+    send_mail(
+        subject=subject,
+        message=plain_message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[to],
+        html_message=convert_to_html_content,
+    )
