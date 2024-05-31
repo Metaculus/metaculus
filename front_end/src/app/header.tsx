e44a1be7dd83abfa431cef1e49d021b1027dc544
gleb <alexxkur@gmail.com>
2024-05-31 13:27:35 +0200

    Small Restructuring

--- front_end/src/app/header.tsx
+++ front_end/src/app/header.tsx
@@ -1,12 +1,8 @@
-"use client";
-
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import Link from "next/link";
 import { FC } from "react";
 
-import AuthButton from "@/components/auth";
+import NavUserButton from "@/components/auth";
 import NavLink from "@/components/nav_link";
-import { useUser } from "@/contexts/user_context";
 
 const LINKS = [
   {
@@ -20,8 +16,6 @@ const LINKS = [
 ];
 
 const Header: FC = () => {
-  const { user, setUser } = useUser();
-
   return (
     <header className="ng-scope fixed left-0 top-0 z-50 flex w-full flex-wrap items-stretch justify-between border-b border-metac-blue-200-dark bg-metac-blue-900 text-white">
       <div className="ml-0.5 flex min-h-12 flex-auto">
@@ -47,44 +41,7 @@ const Header: FC = () => {
             </li>
           ))}
           <li className="z-10 lg:flex lg:items-center">
-            {user ? (
-              <Menu>
-                <MenuButton className="flex h-full items-center p-3 no-underline hover:bg-metac-blue-200-dark">
-                  {user.username}
-                </MenuButton>
-                <MenuItems
-                  anchor="bottom"
-                  className="z-50 text-white lg:border lg:border-metac-blue-200-dark lg:bg-metac-blue-900 lg:text-sm"
-                >
-                  <MenuItem>
-                    <a
-                      className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
-                      href="/settings"
-                    >
-                      Profile
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
-                      onClick={() => setUser(null)}
-                    >
-                      Settings
-                    </a>
-                  </MenuItem>
-                  <MenuItem>
-                    <a
-                      className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
-                      href="/auth/signout"
-                    >
-                      Log Out
-                    </a>
-                  </MenuItem>
-                </MenuItems>
-              </Menu>
-            ) : (
-              <AuthButton />
-            )}
+            <NavUserButton />
           </li>
         </ul>
       </div>
--- front_end/src/components/auth/index.tsx
+++ front_end/src/components/auth/index.tsx
@@ -1,22 +1,62 @@
 "use client";
 
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { FC } from "react";
 
 import { useModal } from "@/contexts/modal_context";
+import { useUser } from "@/contexts/user_context";
 
-const AuthButton: FC = () => {
-  const { modalType, setModalType } = useModal();
+const NavUserButton: FC = () => {
+  const { setModalType } = useModal();
+  const { user, setUser } = useUser();
 
   return (
     <>
-      <button
-        className="w-full px-4 py-1.5 text-center hover:bg-metac-blue-400-dark lg:mx-2 lg:rounded-full lg:bg-metac-blue-200 lg:px-2 lg:py-0 lg:text-metac-blue-900 lg:hover:bg-metac-blue-100"
-        onClick={() => setModalType("signin")}
-      >
-        Log In
-      </button>
+      {user ? (
+        <Menu>
+          <MenuButton className="flex h-full items-center p-3 no-underline hover:bg-metac-blue-200-dark">
+            {user.username}
+          </MenuButton>
+          <MenuItems
+            anchor="bottom"
+            className="z-50 text-white lg:border lg:border-metac-blue-200-dark lg:bg-metac-blue-900 lg:text-sm"
+          >
+            <MenuItem>
+              <a
+                className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
+                href="/settings"
+              >
+                Profile
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a
+                className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
+                onClick={() => setUser(null)}
+              >
+                Settings
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a
+                className="flex items-center justify-center whitespace-nowrap px-4 py-1.5 no-underline hover:bg-metac-blue-400-dark lg:items-end lg:justify-end lg:px-6 lg:text-right lg:hover:bg-metac-blue-200-dark"
+                href="/auth/signout"
+              >
+                Log Out
+              </a>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
+      ) : (
+        <button
+          className="w-full px-4 py-1.5 text-center hover:bg-metac-blue-400-dark lg:mx-2 lg:rounded-full lg:bg-metac-blue-200 lg:px-2 lg:py-0 lg:text-metac-blue-900 lg:hover:bg-metac-blue-100"
+          onClick={() => setModalType("signin")}
+        >
+          Log In
+        </button>
+      )}
     </>
   );
 };
 
-export default AuthButton;
+export default NavUserButton;
