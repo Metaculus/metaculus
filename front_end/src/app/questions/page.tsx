8079448042c0e10ba2b93ae7b1b32d584bd0d9e5
Nikita <nikita.develops.work@gmail.com>
2024-05-31 12:25:27 +0300

    feat: topics mobile UI

--- front_end/src/app/questions/page.tsx
+++ front_end/src/app/questions/page.tsx
@@ -15,10 +15,12 @@ export default async function Questions() {
     <main className="mx-auto min-h-min w-full max-w-5xl flex-auto bg-metac-blue-200 p-0 sm:p-2 sm:pt-0 md:p-3 md:pt-0 lg:mt-16 dark:bg-metac-blue-50-dark">
       <div className="gap-3 p-0 sm:flex sm:flex-row sm:gap-4">
         <QuestionTopics topics={topics} />
-        <div className="flex flex-col gap-3">
-          {questions.map((q) => (
-            <QuestionCard key={q.id} question={q} />
-          ))}
+        <div className="min-h-[calc(100vh-300px)] grow overflow-x-hidden p-2 pt-2.5 no-scrollbar sm:p-0 sm:pt-5">
+          <div className="flex flex-col gap-3">
+            {questions.map((q) => (
+              <QuestionCard key={q.id} question={q} />
+            ))}
+          </div>
         </div>
       </div>
     </main>
