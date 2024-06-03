34a5d7b37828422de00fb94c0643fcedf9665cc6
gleb <alexxkur@gmail.com>
2024-06-03 20:36:46 +0200

    Added username change endpoint

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,17 @@ from rest_framework import serializers
 
 from users.models import User
 
+forbidden_usernames = [
+    "anonymous",
+    "moderator",
+    "moderators",
+    "predictor",
+    "predictors",
+    "metaculus",
+    "admin",
+    "admins",
+]
+
 
 class UserPublicSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +24,13 @@ class UserPrivateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = UserPublicSerializer.Meta.fields + ("first_name", "last_name", "email")
+
+
+def validate_username(value: str):
+    if value.lower() in forbidden_usernames:
+        raise serializers.ValidationError({"username": "this username is not allowed"})
+
+    if User.objects.filter(username__iexact=value).exists():
+        raise serializers.ValidationError("The username is already taken")
+
+    return value
