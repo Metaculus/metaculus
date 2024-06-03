6a1fe8517477d1af42284c83f137ab0ab4c7325c
Nikita <nikita.develops.work@gmail.com>
2024-06-03 13:33:36 +0300

    feat: integrate categories and tags endpoints

--- front_end/src/services/projects.ts
+++ front_end/src/services/projects.ts
@@ -1,4 +1,4 @@
-import { Topic } from "@/types/projects";
+import { Category, Tag, Topic } from "@/types/projects";
 import { get } from "@/utils/fetch";
 
 class ProjectsApi {
@@ -7,6 +7,24 @@ class ProjectsApi {
       return await get<Topic[]>("/projects/topics");
     } catch (err) {
       console.error("Error getting topics:", err);
+      return [];
+    }
+  }
+
+  static async getCategories(): Promise<Category[]> {
+    try {
+      return await get<Category[]>("/projects/categories");
+    } catch (err) {
+      console.error("Error getting categories:", err);
+      return [];
+    }
+  }
+
+  static async getTags(): Promise<Tag[]> {
+    try {
+      return await get<Tag[]>("/projects/tags");
+    } catch (err) {
+      console.error("Error getting tags:", err);
       return [];
     }
   }
