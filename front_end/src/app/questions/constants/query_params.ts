01dfad116c80c2436d5648ff362396c6a61d1908
Nikita <nikita.develops.work@gmail.com>
2024-06-03 16:34:29 +0300

    feat: combobox filter presentation + category filter

--- front_end/src/app/questions/constants/query_params.ts
+++ front_end/src/app/questions/constants/query_params.ts
@@ -2,3 +2,4 @@ export const TOPIC_FILTER = "topic";
 export const TEXT_SEARCH_FILTER = "search";
 export const QUESTION_TYPE_FILTER = "forecast_type";
 export const STATUS_FILTER = "status";
+export const CATEGORY_FILTER = "categories";
