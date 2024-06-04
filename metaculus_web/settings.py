cdc3c55f232502f6fdb42b77230dac007e79e3ae
gleb <alexxkur@gmail.com>
2024-06-04 19:50:28 +0200

    Standardize errors

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -71,13 +71,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-
 # Cors configuration
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
 ]
-
 
 ROOT_URLCONF = "metaculus_web.urls"
 
@@ -136,6 +134,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "EXCEPTION_HANDLER": "utils.views.custom_exception_handler",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
 }
