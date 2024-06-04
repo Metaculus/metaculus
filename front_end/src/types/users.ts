62c6b3d6b7b921e6bbd7a97795a39c0494dd1049
Hlib Kononenko <alexxkur@gmail.com>
2024-06-04 12:09:15 +0200

    User profile init (#7)

--- front_end/src/types/users.ts
+++ front_end/src/types/users.ts
@@ -1,5 +1,11 @@
-export type CurrentUser = {
-  id: string;
+export type UserProfile = {
+  id: number;
   username: string;
+  date_joined: string;
+  bio: string;
+  website: string;
+};
+
+export type CurrentUser = UserProfile & {
   email: string;
 };
