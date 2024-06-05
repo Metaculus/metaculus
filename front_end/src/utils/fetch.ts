d89f996433522cdbcbc8ba042cc27918ffa420db
Nikita <nikita.develops.work@gmail.com>
2024-06-05 12:19:37 +0300

    feat: home page hero section

--- front_end/src/utils/fetch.ts
+++ front_end/src/utils/fetch.ts
@@ -6,23 +6,6 @@ import {
   FetchOptions,
 } from "@/types/fetch";
 
-export function encodeQueryParams(params: Record<string, any>): string {
-  const encodedParams = Object.entries(params)
-    .filter(([, value]) => value !== undefined)
-    .flatMap(([key, value]) => {
-      if (Array.isArray(value)) {
-        return value.map(
-          (val) => `${encodeURIComponent(key)}=${encodeURIComponent(val)}`
-        );
-      } else {
-        return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
-      }
-    })
-    .join("&");
-
-  return encodedParams ? `?${encodedParams}` : "";
-}
-
 /**
  * Util for converting Django errors to the standardized way
  */
