f29db55a1cfd7c672b8601459093982fcba70b76
gleb <alexxkur@gmail.com>
2024-06-05 21:01:50 +0200

    Added password reset BE layers

--- authentication/services.py
+++ authentication/services.py
@@ -3,21 +3,15 @@ from rest_framework.exceptions import ValidationError
 
 from users.models import User
 from utils.email import send_email_with_template
-from utils.frontend import build_frontend_account_activation_url
-
-
-def generate_user_activation_link(user: User):
-    """
-    Generates the confirmation link for account activation
-    """
-
-    confirmation_token = default_token_generator.make_token(user)
-
-    return build_frontend_account_activation_url(user.id, confirmation_token)
+from utils.frontend import (
+    build_frontend_account_activation_url,
+    build_frontend_password_reset_url,
+)
 
 
 def send_activation_email(user: User):
-    activation_link = generate_user_activation_link(user)
+    confirmation_token = default_token_generator.make_token(user)
+    activation_link = build_frontend_account_activation_url(user.id, confirmation_token)
 
     # TODO: send email, so printing link for now
     print(activation_link)
@@ -34,13 +28,44 @@ def send_activation_email(user: User):
     )
 
 
-def check_and_activate_user(user: User, token: str):
+def send_password_reset_email(user: User):
+    confirmation_token = default_token_generator.make_token(user)
+    reset_link = build_frontend_password_reset_url(user.id, confirmation_token)
+
+    # TODO: send email, so printing link for now
+    print(reset_link)
+
+    send_email_with_template(
+        user.email,
+        "Metaculus Password Reset Request",
+        "emails/password_reset.html",
+        context={
+            "username": user.username,
+            "reset_link": reset_link,
+        },
+    )
+
+
+def check_and_activate_user(user_id: int, token: str):
     """
     Validates activation token and activates user
     """
 
-    if not default_token_generator.check_token(user, token):
-        raise ValidationError("Invalid activation token")
+    user = User.objects.filter(pk=user_id, is_active=False).first()
+
+    if not user or not default_token_generator.check_token(user, token):
+        raise ValidationError({"token": ["Activation Token is expired or invalid"]})
 
     user.is_active = True
     user.save()
+
+    return user
+
+
+def check_password_reset(user_id: User, token: str):
+    user = User.objects.filter(pk=user_id, is_active=True).first()
+
+    if not user or not default_token_generator.check_token(user, token):
+        raise ValidationError({"token": ["Password Reset Token is expired or invalid"]})
+
+    return user
