c71289e2b7aa8f6ec6be2d7136110fe7bd4b237b
Nikita <nikita.develops.work@gmail.com>
2024-06-06 13:08:14 +0300

    feat: tournaments hero + integrate /tournaments endpoint

---
+++ front_end/src/app/tournaments/components/tournaments_list.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { FC } from "react";
+
+import { Tournament } from "@/types/projects";
+
+type Props = {
+  items: Tournament[];
+};
+
+const TournamentsList: FC<Props> = ({ items }) => {
+  return <></>;
+};
+
+export default TournamentsList;
