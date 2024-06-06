74115e202f3ac1b7726b37ff44b2c47eaef41299
gleb <alexxkur@gmail.com>
2024-06-06 16:40:38 +0200

    Added tournament by slug fetch

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("projects/topics", views.topics_list_api_view),
     path("projects/categories", views.categories_list_api_view),
     path("projects/tournaments", views.tournaments_list_api_view),
+    path("projects/tournaments/<str:slug>", views.tournament_by_slug_api_view),
     path("projects/tags", views.tags_list_api_view),
 ]
