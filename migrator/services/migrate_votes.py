0e7ad612a5c77ef356f515cb7fb42a777c49b373
gleb <alexxkur@gmail.com>
2024-06-07 18:10:38 +0200

    Unified Enum choices

--- migrator/services/migrate_votes.py
+++ migrator/services/migrate_votes.py
@@ -2,7 +2,7 @@ from migrator.utils import paginated_query
 from questions.models import Question, Vote
 
 
-def create_vote(vote_obj, direction: Vote.VoteDirection):
+def create_vote(vote_obj, direction: int):
     return Vote(
         user_id=vote_obj["user_id"],
         question_id=vote_obj["question_id"],
@@ -16,7 +16,7 @@ def migrate_votes():
 
     # Migrating Upvotes
     vote_instances += [
-        create_vote(obj, Vote.VoteDirection.UP)
+        create_vote(obj, 1)
         for obj in paginated_query(
             "SELECT * FROM metac_question_question_votes_up",
         )
@@ -25,7 +25,7 @@ def migrate_votes():
 
     # Migrating Downvotes
     vote_instances += [
-        create_vote(obj, Vote.VoteDirection.DOWN)
+        create_vote(obj, -1)
         for obj in paginated_query(
             "SELECT * FROM metac_question_question_votes_down",
         )
