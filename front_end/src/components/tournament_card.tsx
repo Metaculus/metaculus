45e1b2563c9b23d6a3e0160e4586eaaf24ff8074
George3d6 <george@cerebralab.com>
2024-06-07 02:40:01 +0200

    small fixes

--- front_end/src/components/tournament_card.tsx
+++ front_end/src/components/tournament_card.tsx
@@ -34,7 +34,7 @@ const TournamentCard: FC<Props> = ({
   return (
     <Link
       href={href}
-      className="flex flex-col overflow-hidden rounded-md bg-metac-gray-0 text-sm font-medium text-metac-gray-700 no-underline hover:shadow-lg active:shadow-md dark:bg-metac-gray-0-dark dark:text-metac-gray-700-dark"
+      className="bg-metac-gray-0 text-metac-gray-700 dark:bg-metac-gray-0-dark dark:text-metac-gray-700-dark flex flex-col overflow-hidden rounded-md text-sm font-medium no-underline hover:shadow-lg active:shadow-md"
     >
       <div className="relative h-40 w-full bg-cover bg-center">
         <Image
@@ -56,7 +56,7 @@ const TournamentCard: FC<Props> = ({
       <div className="flex flex-1 flex-col whitespace-break-spaces p-6">
         <h3 className="m-0 flex-1">{name}</h3>
         {!!prizePool && (
-          <div className="mt-2 text-metac-green-800 dark:text-metac-green-800-dark">
+          <div className="text-metac-green-800 dark:text-metac-green-800-dark mt-2">
             <FontAwesomeIcon
               icon={faAward}
               className="ml-1 mr-2 align-middle"
@@ -81,7 +81,7 @@ const TournamentCard: FC<Props> = ({
         )}
       </div>
       {withCount && (
-        <div className="flex flex-col whitespace-break-spaces border-t border-metac-blue-200 px-6 py-3 dark:border-metac-blue-200-dark">
+        <div className="border-metac-blue-200 dark:border-metac-blue-200-dark flex flex-col whitespace-break-spaces border-t px-6 py-3">
           <strong>{t("questionCount", { count: questionsCount })}</strong>
         </div>
       )}
