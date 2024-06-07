9734ab29bba3c8c7afa61c44f867cc23d8f2f6f9
Luke Sabor <lukesabor@gmail.com>
2024-06-07 11:15:45 -0700

    move scaling function to formulas and reorder some imports

--- questions/models.py
+++ questions/models.py
@@ -173,17 +173,3 @@ class Vote(models.Model):
             #    )
             # )
         ]
-
-
-# TODO: find a better place for this
-def scale_continous_forecast_location(
-    question: Question, internal_location: float
-) -> float:
-    if question.zero_point:
-        deriv_ratio = (question.max - question.zero_point) / (
-            question.min - question.zero_point
-        )
-        return question.min + (question.max - question.min) * (
-            deriv_ratio**internal_location - 1
-        ) / (deriv_ratio - 1)
-    return question.min + (question.max - question.min) * internal_location
