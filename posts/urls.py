ce8696eed5a48ee8453cdd4d80f95534574929f8
gleb <alexxkur@gmail.com>
2024-06-10 20:23:39 +0200

    Small fix

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts", views.posts_list_api_view, name="post-list"),
+    path("posts/", views.posts_list_api_view, name="post-list"),
     path("posts/<int:pk>/", views.post_detail, name="post-detail"),
-    path("posts/<int:pk>/vote", views.post_vote_api_view, name="question-detail"),
+    path("posts/<int:pk>/vote/", views.post_vote_api_view, name="question-detail"),
     path("posts/create/", views.post_create_api_view, name="create-question"),
     path("posts/<int:pk>/update/", views.post_update_api_view, name="update-question"),
     path("posts/<int:pk>/delete/", views.post_delete_api_view, name="delete-question"),
