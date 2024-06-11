051d5ff4154ed94f54ffa842fbccd6d6ae8343a4
George3d6 <george@cerebralab.com>
2024-06-11 16:46:13 +0200

    tried and primarily failed to get a nice django admin going

--- projects/admin.py
+++ projects/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.apps import apps
 
 # Register your models here.
-app_models = apps.get_app_config('scoring').get_models()
+app_models = apps.get_app_config('projects').get_models()
 for model in app_models:
     admin.site.register(model)
