ddfcf4068e580b02898aa8d2e1e19e2ea5245d49
George3d6 <george@cerebralab.com>
2024-06-11 12:44:53 +0200

    fixed status and status filter

--- questions/urls.py
+++ questions/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from questions.views import create_forecast_api_view
 
 urlpatterns = [
-    path("questions/<int:pk>/forecast", create_forecast_api_view, name="create-forecast"),
+    path(
+        "questions/<int:pk>/forecast", create_forecast_api_view, name="create-forecast"
+    ),
 ]
