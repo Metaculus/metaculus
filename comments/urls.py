4fd61beec7ac78ccfafb48393e6ba0e05608f130
taci <rudolfordoyne@pm.me>
2024-06-12 10:25:24 -0700

    attach forecasts to comments migration, add comment feed to userprofile

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("comments", views.comments_list_api_view),
+    path("comments", views.comments_list_api_view, name="comment-list"),
 ]
