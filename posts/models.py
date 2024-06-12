80d067ba7f3990f11e25df1328d343dc249089ee
gleb <alexxkur@gmail.com>
2024-06-12 17:28:32 +0200

    Hotfix

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,14 @@ class PostQuerySet(models.QuerySet):
         return self.prefetch_related("projects")
 
     def prefetch_forecasts(self):
-        return self.prefetch_related("question__forecast_set")
+        return self.prefetch_related(
+            "question__forecast_set",
+            # Conditional
+            "conditional__question_yes__forecast_set",
+            "conditional__question_no__forecast_set",
+            # Group Of Questions
+            "group_of_questions__questions__forecast_set",
+        )
 
     def prefetch_questions(self):
         return self.prefetch_related(
