80066415f470458c89590d8fb075782b5b64e140
gleb <alexxkur@gmail.com>
2024-06-14 14:51:26 +0200

    Added autocompletion admin filter for Posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,4 @@
+from admin_auto_filters.filters import AutocompleteFilterFactory
 from django.contrib import admin
 
 from posts.models import Post
@@ -5,4 +6,12 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["author", "approved_by", "question", "projects", "conditional", "group_of_questions"]
+    list_filter = [AutocompleteFilterFactory("Author", "author")]
+    autocomplete_fields = [
+        "author",
+        "approved_by",
+        "question",
+        "projects",
+        "conditional",
+        "group_of_questions",
+    ]
