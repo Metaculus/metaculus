b425dce335e402475b6c47891cc8b8dc6d8edfe8
gleb <alexxkur@gmail.com>
2024-06-14 14:17:04 +0200

    Added NextJS DjangoAdmin rewrite

--- front_end/next.config.mjs
+++ front_end/next.config.mjs
@@ -1,23 +1,42 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
+
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'metaculus-media.s3.amazonaws.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'd3s0w6fek99l5b.cloudfront.net',
-                pathname: '/**',
-            }
-        ],
+  env: {
+    API_BASE_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "metaculus-media.s3.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "d3s0w6fek99l5b.cloudfront.net",
+        pathname: "/**",
+      },
+    ],
+  },
+  rewrites: () => ([
+    // Django Admin rewrites
+    {
+      source: "/admin/:path*",
+      destination: `${API_BASE_URL}/admin/:path*/`,
     },
+    // Assets
+    {
+      source: "/static/admin/:path*",
+      destination: `${API_BASE_URL}/static/admin/:path*`,
+    },
+  ]),
 };
 
 export default withNextIntl(nextConfig);
