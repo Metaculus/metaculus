04fe2815f917f03751cf5c250e208c3a7d7a6f41
Nikita <nikita.develops.work@gmail.com>
2024-06-17 13:53:37 +0300

    feat: render numeric chart in conditional tile

--- front_end/src/components/conditional_tile/conditional_chart.tsx
+++ front_end/src/components/conditional_tile/conditional_chart.tsx
@@ -2,10 +2,12 @@ import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 
+import NumericAreaChart from "@/components/charts/numeric_area_chart";
 import PredictionChip from "@/components/prediction_chip";
 import ProgressBar from "@/components/ui/progress_bar";
 import { PostStatus } from "@/types/post";
 import { QuestionType, QuestionWithForecasts } from "@/types/question";
+import { formatPrediction, getIsForecastEmpty } from "@/utils/forecasts";
 
 type Props = {
   parentResolved: boolean;
@@ -54,6 +56,52 @@ const ConditionalChart: FC<Props> = ({
               status={parentStatus}
               nr_forecasters={question.nr_forecasters}
               prediction={pctCandidate}
+              resolution={question.resolution}
+              size="compact"
+            />
+          )}
+        </>
+      );
+    }
+    case QuestionType.Numeric:
+    case QuestionType.Date: {
+      if (getIsForecastEmpty(question.forecasts)) {
+        return <div className="text-center text-xs">No data yet</div>;
+      }
+
+      const prediction = question.forecasts.values_mean.at(-1);
+      const formattedPrediction = prediction
+        ? formatPrediction(prediction, question.type)
+        : "";
+
+      return (
+        <>
+          <div className="flex gap-2">
+            <div className="flex items-center gap-1 whitespace-nowrap text-xs font-semibold leading-none text-olive-700 dark:text-olive-700-dark">
+              <div>
+                <FontAwesomeIcon icon={faUserGroup} size="sm" />
+              </div>
+              <span>{formattedPrediction}</span>
+            </div>
+            <NumericAreaChart
+              height={40}
+              min={question.min}
+              max={question.max}
+              data={[
+                {
+                  pmf: question.forecasts.latest_pmf,
+                  cdf: question.forecasts.latest_cdf,
+                  color: "green",
+                },
+              ]}
+            />
+          </div>
+          {resolved && (
+            <PredictionChip
+              questionType={question.type}
+              status={parentStatus}
+              nr_forecasters={question.nr_forecasters}
+              prediction={prediction}
               resolution={question.resolution}
               size="compact"
             />
