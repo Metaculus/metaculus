ab0cd40013a1b6317f8a57c6590f3b1c7077d4d8
Hlib Kononenko <alexxkur@gmail.com>
2024-06-17 15:54:20 +0200

    Private questions migration (#35)
    * Migrated private questions
    
    * Hotfix
    
    * Optimized `filter_allowed`
    
    * Hotfix
    
    * Added Posts filter by permissions
    
    * Fixed permissions
    
    * Hotfix

--- migrator/management/commands/migrate_old_db_debug.py
+++ migrator/management/commands/migrate_old_db_debug.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 
 from .migrate_old_db import Command as MigrateCommand
+from ...services.migrate_permissions import migrate_permissions
 from ...services.migrate_questions import migrate_questions
 
 
@@ -13,19 +14,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with connection.cursor() as cursor:
             # Replace with the table you're working with right now
-            cursor.execute("DELETE FROM questions_forecast")
-            cursor.execute("DELETE FROM posts_post_projects")
-            cursor.execute("DELETE FROM posts_vote")
-            cursor.execute("DELETE FROM comments_comment")
-            cursor.execute("DELETE FROM posts_post")
-            cursor.execute("DELETE FROM questions_conditional")
-            cursor.execute("DELETE FROM questions_groupofquestions")
-            cursor.execute("DELETE FROM questions_question")
+            pass
+
+            cursor.execute("DELETE FROM projects_projectuserpermission")
+            cursor.execute("DELETE FROM posts_post_projects USING projects_project "
+                           "WHERE posts_post_projects.project_id = projects_project.id AND projects_project.type = 'personal_list';")
+            cursor.execute("DELETE FROM projects_project WHERE type = 'personal_list'")
 
         print("Starting migration script")
 
         # Function
-        migrate_questions()
+        migrate_permissions()
 
         # Reset sql sequences
         MigrateCommand._reset_sequence()
