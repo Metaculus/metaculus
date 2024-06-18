047d0a5f405503105527bde33d66e4bef747e17e
Nikita <nikita.develops.work@gmail.com>
2024-06-18 17:41:12 +0300

    feat: use resolved_at for sorting group questions

--- front_end/src/components/post_card/group_of_questions_tile/group_numeric_tile.tsx
+++ front_end/src/components/post_card/group_of_questions_tile/group_numeric_tile.tsx
@@ -1,3 +1,4 @@
+import { differenceInMilliseconds } from "date-fns";
 import { FC } from "react";
 
 import FanChart from "@/components/charts/fan_chart";
@@ -43,13 +44,12 @@ function getPredictionQuestion(
   curationStatus: PostStatus
 ) {
   const sortedQuestions = questions
-    .map((q, index) => {
-      const fanName = getFanName(q.title);
-      const dateValue = Date.parse(fanName);
-      const sortValue = isNaN(dateValue) ? index : dateValue;
-      return { ...q, sortValue, fanName };
-    })
-    .sort((a, b) => a.sortValue - b.sortValue);
+    .map((q) => ({
+      ...q,
+      resolvedAt: new Date(q.resolved_at),
+      fanName: getFanName(q.title),
+    }))
+    .sort((a, b) => differenceInMilliseconds(a.resolvedAt, b.resolvedAt));
 
   if (curationStatus === PostStatus.RESOLVED) {
     return sortedQuestions[sortedQuestions.length - 1];
