21e65978b6888658f3ba533b556e0ea06449537a
Nikita <nikita.develops.work@gmail.com>
2024-06-19 17:17:21 +0300

    fix: format prediction for numeric charts

--- front_end/src/app/(main)/(home)/components/question_carousel.tsx
+++ front_end/src/app/(main)/(home)/components/question_carousel.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FC } from "react";
 
 import PostCard from "@/components/post_card";
-import BasicPostCard from "@/components/post_card/basic_post_card";
 import PostsApi from "@/services/posts";
 import { PostWithForecasts } from "@/types/post";
 
@@ -11,13 +10,13 @@ type Props = {
 };
 
 const QuestionCarousel: FC<Props> = async ({ postIds }) => {
-  const posts: PostWithForecasts[] = [];
-  for (let postId of postIds) {
-    const post = await PostsApi.getPost(postId);
-    if (post) {
-      posts.push(post);
-    }
-  }
+  const postsResponse = await Promise.all(
+    postIds.map((postId) => PostsApi.getPost(postId))
+  );
+  const posts = postsResponse.filter(
+    (post) => post !== null
+  ) as PostWithForecasts[];
+
   return (
     <div className="flex flex-row overflow-x-scroll p-4">
       {posts.map((p, idx) => (
