33a801209d814f788e65230ff24fda95470ed7eb
taci <rudolfordoyne@pm.me>
2024-06-19 10:27:35 -0700

    add dropdown menu to comments

--- front_end/src/app/(main)/questions/[id]/page.tsx
+++ front_end/src/app/(main)/questions/[id]/page.tsx
@@ -114,7 +114,9 @@ export default async function IndividualQuestion({
               </div>
             </div>
           </div>
-          {commentsData && <CommentFeed initialComments={commentsData} />}
+          {commentsData && (
+            <CommentFeed initialComments={commentsData} postId={postData.id} />
+          )}
         </div>
         <div className="hidden w-80 shrink-0 border border-transparent bg-gray-0 p-4 text-gray-700 dark:border-blue-200-dark dark:bg-gray-0-dark dark:text-gray-700-dark lg:block">
           <div className="mb-4 flex w-full items-center justify-between gap-2 border-b border-gray-300 pb-4 dark:border-gray-300-dark">
--- front_end/src/components/comment_feed/index.tsx
+++ front_end/src/components/comment_feed/index.tsx
@@ -10,12 +10,22 @@ type Props = {
   //next: any;
   //previous: any;
   initialComments: CommentType[];
+  postId?: number;
+  profileId?: number;
 };
 
-const CommentFeed: FC<Props> = ({ initialComments }) => {
+const CommentFeed: FC<Props> = ({ initialComments, postId, profileId }) => {
   const [numberOfComments, setNumberOfComments] = useState(10);
 
   const comments = initialComments.slice(0, numberOfComments);
+
+  let url = `${window.location.origin}`;
+
+  if (postId) {
+    url += `/questions/${postId}`;
+  } else if (profileId) {
+    url += `/accounts/profile/${profileId}`;
+  }
 
   if (comments.length == 0) return null;
   return (
@@ -24,7 +34,7 @@ const CommentFeed: FC<Props> = ({ initialComments }) => {
         <div key={comment.id}>
           <Hr className="my-4" />
 
-          <Comment comment={comment} />
+          <Comment comment={comment} url={url} />
         </div>
       ))}
     </section>
