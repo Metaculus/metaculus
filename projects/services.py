bcc65c96cd59edf235b1a38571bd6c5775288e50
gleb <alexxkur@gmail.com>
2024-06-21 20:16:24 +0200

    Added project invites

--- projects/services.py
+++ projects/services.py
@@ -1,4 +1,7 @@
-from projects.models import Project
+from django.db import IntegrityError
+from django.db.models import Q
+
+from projects.models import Project, ProjectUserPermission
 from projects.permissions import ObjectPermission
 from users.models import User
 
@@ -40,3 +43,57 @@ def create_private_user_project(user: User):
     )
 
     return obj
+
+
+def get_project_permission_for_user(
+    project: Project, user: User = None
+) -> ObjectPermission | None:
+    """
+    A small wrapper to get the permission of project
+    """
+
+    return (
+        Project.objects.annotate_user_permission(user=user)
+        .values_list("user_permission", flat=True)
+        .get(id=project.id)
+    )
+
+
+def invite_user_to_project(
+    project: Project,
+    user: User,
+    permission: ObjectPermission = ObjectPermission.FORECASTER,
+):
+    """
+    Invites user to the private project
+    """
+
+    try:
+        ProjectUserPermission.objects.create(
+            user=user, project=project, permission=permission
+        )
+    except IntegrityError:
+        # User was already invited
+        return
+
+
+def invite_users_to_project(
+    project: Project,
+    user_identifiers: list[str],
+    permission: ObjectPermission = ObjectPermission.FORECASTER,
+):
+    """
+    Invites users to the project
+    """
+
+    queries = Q()
+    for identifier in user_identifiers:
+        queries |= Q(username__iexact=identifier) | Q(email__iexact=identifier)
+
+    # Fetch the users based on the combined query
+    users = User.objects.filter(queries).distinct()
+
+    print()
+
+    for user in users:
+        invite_user_to_project(project, user, permission=permission)
