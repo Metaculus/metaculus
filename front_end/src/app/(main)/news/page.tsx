8b29225d50ce3c15e876815a05528ed60fb41314
Nikita <nikita.develops.work@gmail.com>
2024-06-21 18:53:41 +0300

    feat: basic notebook editor

---
+++ front_end/src/app/(main)/news/page.tsx
@@ -0,0 +1,36 @@
+import dynamic from "next/dynamic";
+
+const MarkdownEditor = dynamic(() => import("@/components/markdown_editor"), {
+  ssr: false,
+});
+
+const markdown = `
+Hello **world**!
+
+# Heading 1
+
+## Heading 2
+
+- list items 1
+- list item 2
+
+<EmbeddedQuestion id="1" />
+
+<EmbeddedQuestion id="2" />
+`;
+
+export default function TestNewsEditor() {
+  return (
+    <main className="p-4">
+      <h1>Edit mode:</h1>
+      <div className="h-50vh overflow-auto">
+        <MarkdownEditor markdown={markdown} />
+      </div>
+      <hr />
+      <h1>Reader mode:</h1>
+      <div className="h-50vh mx-auto max-w-3xl overflow-auto">
+        <MarkdownEditor markdown={markdown} readOnly />
+      </div>
+    </main>
+  );
+}
