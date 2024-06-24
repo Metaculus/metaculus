094b6f2ccea94d9468b78fa13ae1f496fa6e1b51
gleb <alexxkur@gmail.com>
2024-06-24 15:15:52 +0200

    BE project invitation fixes

---
+++ users/services.py
@@ -0,0 +1,22 @@
+from django.db.models import Q
+from rest_framework.exceptions import ValidationError
+
+from users.models import User
+
+
+def get_users_by_usernames(usernames: list[str]) -> list[User]:
+    if not usernames:
+        return User.objects.none()
+
+    queries = Q()
+    for username in usernames:
+        queries |= Q(username__iexact=username)
+
+    users = User.objects.filter(queries).distinct()
+    fetched_usernames = {u.username for u in users}
+
+    for username in usernames:
+        if username not in fetched_usernames:
+            raise ValidationError(f"User {username} does not exist")
+
+    return users
