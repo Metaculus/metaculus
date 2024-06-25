b6bf258dee76111857d74ffc059f31b4455df7a4
George3d6 <george@cerebralab.com>
2024-06-25 17:45:16 +0200

    stashing

--- front_end/src/components/ui/dropdown_menu.tsx
+++ front_end/src/components/ui/dropdown_menu.tsx
@@ -1,21 +1,22 @@
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Menu } from "@headlessui/react";
+import { Menu, MenuItem, MenuItems, MenuButton } from "@headlessui/react";
 import clsx from "clsx";
 import { Fragment } from "react";
 
 import Button from "./button";
 
-export interface MenuItem {
+export interface MenuItemProps {
   id: string;
   name?: string;
   onClick?: (...args: unknown[]) => unknown;
   link?: string;
   openNewTab?: boolean;
+  show: boolean;
 }
 
 interface DropdownMenuProps extends React.PropsWithChildren {
-  items: MenuItem[];
+  items: MenuItemProps[];
   textAlign?: "left" | "right";
 }
 
@@ -32,50 +33,46 @@ export default function DropdownMenu({
 }: DropdownMenuProps) {
   return (
     <Menu as="div" className="relative text-gray-900 dark:text-gray-900-dark">
-      <Menu.Button as={Fragment}>{children}</Menu.Button>
-      <Menu.Items
+      <MenuButton as={Fragment}>{children}</MenuButton>
+      <MenuItems
         as="div"
         className="absolute right-0 z-50 mt-1 flex origin-top-right flex-col overflow-y-auto rounded border border-gray-500 bg-gray-0 text-sm drop-shadow-lg dark:border-gray-500-dark dark:bg-gray-0-dark"
       >
-        {items.map((item) => (
-          <Menu.Item as={Fragment} key={item.id}>
-            {({ active }) =>
-              item.link ? (
-                <a
-                  target={item.openNewTab ? "_blank" : undefined}
-                  rel="noreferrer"
-                  href={item.link}
-                  className={clsx(
-                    "w-full whitespace-nowrap p-2 no-underline",
-                    {
-                      "bg-gray-200 dark:bg-gray-200-dark": active,
-                    },
-                    { "text-right": textAlign === "right" },
-                    { "text-left": textAlign === "left" }
-                  )}
-                  onClick={item.onClick}
-                >
-                  {item.name}
-                </a>
-              ) : (
-                <button
-                  className={clsx(
-                    "w-full self-stretch whitespace-nowrap p-2",
-                    {
-                      "bg-gray-200 dark:bg-gray-200-dark": active,
-                    },
-                    { "text-right": textAlign === "right" },
-                    { "text-left": textAlign === "left" }
-                  )}
-                  onClick={item.onClick}
-                >
-                  {item.name}
-                </button>
-              )
-            }
-          </Menu.Item>
-        ))}
-      </Menu.Items>
+        {items
+          .filter((x) => x.show)
+          .map((item) => (
+            <MenuItem as={Fragment} key={item.id}>
+              {({}) =>
+                item.link ? (
+                  <a
+                    target={item.openNewTab ? "_blank" : undefined}
+                    rel="noreferrer"
+                    href={item.link}
+                    className={clsx(
+                      "w-full whitespace-nowrap p-2 no-underline hover:bg-gray-200 hover:dark:bg-gray-200-dark",
+                      { "text-right": textAlign === "right" },
+                      { "text-left": textAlign === "left" }
+                    )}
+                    onClick={item.onClick}
+                  >
+                    {item.name}
+                  </a>
+                ) : (
+                  <button
+                    className={clsx(
+                      "w-full self-stretch whitespace-nowrap p-2 hover:bg-gray-200 hover:dark:bg-gray-200-dark",
+                      { "text-right": textAlign === "right" },
+                      { "text-left": textAlign === "left" }
+                    )}
+                    onClick={item.onClick}
+                  >
+                    {item.name}
+                  </button>
+                )
+              }
+            </MenuItem>
+          ))}
+      </MenuItems>
     </Menu>
   );
 }
