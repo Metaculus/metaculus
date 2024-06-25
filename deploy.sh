3d81a0845454511707213f328a7a441f5db795e2
George3d6 <george@cerebralab.com>
2024-06-26 01:27:59 +0200

    various changes

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 set -x;
-source .bashrc;
+source /home/ubuntu/.bashrc;
 
 tmux kill-session -t web_backend;
 tmux kill-session -t web_frontend;
 tmux kill-session -t dramatiq;
 
-# 2. Change directory to 'rewrite'
 cd /home/ubuntu/rewrite;
 
-# 3. Pull the latest from main with overwrite
 git fetch origin;
 git reset --hard origin/main;
 poetry install;
 
-# 4. Install new poetry dependencies and new node dependencies in 'front_end' and run `npm run build`
 cd front_end;
 npm i;
 npm run dev;
 cd ..;
 
-# 5. Run django migrations with poetry
 poetry run python3 manage.py migrate;
 
 export ALPHA_ACCESS_TOKEN="the open source rewrite";
 
-# 7. Start a `web_backend` tmux session and run the Django server
 tmux new-session -d -s web_backend;
 tmux send-keys -t web_backend 'poetry run python3 manage.py runserver' C-m;
 tmux new-session -d -s dramatiq;
 tmux send-keys -t dramatiq 'python manage.py rundramatiq --processes 1 --threads 1' C-m;
 
-# 8. Go to 'front_end' and start the `web_frontend` tmux session and run the Next.js frontend server
 cd front_end;
 tmux new-session -d -s web_frontend;
 tmux send-keys -t web_frontend 'npm run start -- -p 3000' C-m;
