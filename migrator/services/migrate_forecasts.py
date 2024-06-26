d8e85b11d3a1a4d0fcdd901a9604bedf599d1848
Luke Sabor <lukesabor@gmail.com>
2024-06-26 10:23:49 -0700

    add scoring and leaderboard creation to migrate_old_db command

--- migrator/services/migrate_forecasts.py
+++ migrator/services/migrate_forecasts.py
@@ -42,28 +42,36 @@ def create_forecast(
     return new_forecast
 
 
-def migrate_forecasts():
+def migrate_forecasts(qty: int | None = None):
     forecasts = []
     questions = Question.objects.all()
     questions_dict = {x.id: x for x in questions}
     users = User.objects.all()
     users_dict = {x.id: x for x in users}
 
-    # Add `limit 300000` to get a sizeable amount but have the migration be faster
+    query_string = (
+        "SELECT p.*, ps.user_id, ps.question_id, ps.aggregation_method "
+        "FROM metac_question_prediction p "
+        "JOIN metac_question_predictionsequence ps "
+        "ON p.prediction_sequence_id = ps.id "
+        "AND aggregation_method = 'none'"
+    )
+    if qty:
+        # Add `limit 300000` to get a sizeable amount but have the migration be faster
+        query_string += f" limit {qty}"
+
+    forecasts = []
     for i, old_prediction in enumerate(
         # flake
-        paginated_query(
-            "SELECT p.*, ps.user_id, ps.question_id, ps.aggregation_method FROM metac_question_prediction p "
-            "JOIN metac_question_predictionsequence ps "
-            "ON p.prediction_sequence_id = ps.id AND aggregation_method = 'none'"
-        )
-    ):  # limit 300000
-        if (i + 1) % 150000 == 0:
-            print(
-                f"Went through {(i + 1)} predictions and generate {len(forecasts) + 1} forecasts!"
-            )
+        paginated_query(query_string)
+    ):
         forecast = create_forecast(old_prediction, questions_dict, users_dict)
         if forecast is not None:
+            print("Migrating forecast", i + 1, end="                           \r")
             forecasts.append(forecast)
-    print("Bulk inserting forecasts")
-    Forecast.objects.bulk_create(forecasts, batch_size=50_000)
+        if len(forecasts) >= 50_000:
+            print("Migrating forecast", i + 1, "Bulk inserting forecasts...", end="\r")
+            Forecast.objects.bulk_create(forecasts)
+            forecasts = []
+    print("Migrating forecast", i + 1, "Bulk inserting forecasts...")
+    Forecast.objects.bulk_create(forecasts)
