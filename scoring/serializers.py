a344df6a63b8a41690d45a18a7d9501219f66165
Luke Sabor <lukesabor@gmail.com>
2024-06-26 11:16:53 -0700

    improve LeaderboardSerializer

--- scoring/serializers.py
+++ scoring/serializers.py
@@ -14,11 +14,21 @@ class LeaderboardEntrySerializer(serializers.ModelSerializer):
 
 
 class LeaderboardSerializer(serializers.Serializer):
-    entries = LeaderboardEntrySerializer(many=True, source="leaderboardentry")
+    project_id = serializers.IntegerField(source="id")
+    type = serializers.CharField()
+    leaderboard_type = serializers.CharField()
+    name = serializers.CharField()
+    slug = serializers.CharField()
+    entries = LeaderboardEntrySerializer(many=True, source="leaderboard_entries")
+    prize_pool = serializers.IntegerField()
+    start_date = serializers.DateTimeField()
+    close_date = serializers.DateTimeField()
+    is_ongoing = serializers.BooleanField()
 
     class Meta:
         model = Project
         fields = [
+            "project_id",
             "type",
             "leaderboard_type",
             "name",
