35b0484739c35859242fa735c150de74056d3720
ludant <rudolfordoyne@protonmail.com>
2024-06-27 17:46:12 -0500

    add soft-delete comments API path

--- comments/views.py
+++ comments/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import serializers
+from rest_framework import serializers, status
+from django.shortcuts import get_object_or_404
 
 from comments.models import Comment
 from comments.serializers import CommentSerializer
@@ -30,14 +31,27 @@ def comments_list_api_view(request: Request):
         comments = comments.filter(author_id=author_param)
 
     # for testing, show a max of 20 comments
-    # comments = comments.all()[:20]
+    comments = comments.all()[:20]
 
-    comments = [
-        c
-        for c in comments.all()
-        if ObjectPermission.can_view(get_comment_permission_for_user(c, request.user))
-    ]
+    #comments = [
+    #    c
+    #    for c in comments.all()
+    #    if ObjectPermission.can_view(get_comment_permission_for_user(c, request.user))
+    #]
 
-    data = [{**CommentSerializer(obj).data} for obj in comments]
+    data = [{**CommentSerializer(obj).data} for obj in comments.all()]
 
     return Response(data)
+
+
+@api_view(["PUT"])
+@permission_classes([AllowAny])
+def comment_delete_api_view(request: Request, pk: int):
+    comment = get_object_or_404(Comment.objects.all(), pk=pk)
+
+    breakpoint()
+
+    comment.is_soft_deleted = True
+    comment.save()
+
+    return Response({}, status=status.HTTP_200_OK)
