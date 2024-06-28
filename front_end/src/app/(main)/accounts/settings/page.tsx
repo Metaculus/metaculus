0765af6c62b8941ae4466bc38d8d463ac1876d1c
gleb <alexxkur@gmail.com>
2024-06-28 16:50:54 +0200

    Added token reveal settings page

---
+++ front_end/src/app/(main)/accounts/settings/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation";
+
+import ApiAccess from "@/app/(main)/accounts/settings/components/api_access";
+import ProfileApi from "@/services/profile";
+import { getServerSession } from "@/services/session";
+
+export default async function Settings() {
+  const currentUser = await ProfileApi.getMyProfile();
+  const token = getServerSession();
+
+  if (!token || !currentUser) return redirect("/");
+
+  return (
+    <main className="mx-auto min-h-min w-full max-w-3xl flex-auto rounded bg-gray-0 p-0 dark:bg-gray-0-dark sm:p-2 sm:pt-0 md:p-3 lg:mt-4">
+      <ApiAccess token={token} />
+    </main>
+  );
+}
