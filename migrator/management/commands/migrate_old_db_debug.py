2e848313f8be1b99ade2638b6f4adb3ac13f3ab0
George3d6 <george@cerebralab.com>
2024-06-28 17:51:07 +0200

    better notebook conversion

--- migrator/management/commands/migrate_old_db_debug.py
+++ migrator/management/commands/migrate_old_db_debug.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 
+from migrator.services.migrate_questions import migrate_questions__notebook
+
 from ...services.migrate_permissions import migrate_permissions
-from ...utils import reset_sequence
+from ...utils import paginated_query, reset_sequence
 
 
 class Command(BaseCommand):
@@ -11,34 +13,43 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        with connection.cursor() as cursor:
-            # Replace with the table you're working with right now
-            pass
+        old_groups = {}
+        for old_question in paginated_query(
+            """SELECT 
+                            q.*, 
+                            qc.parent_id as condition_id, 
+                            qc.unconditional_question_id as condition_child_id, 
+                            qc.resolution as qc_resolution FROM (
+                                    SELECT
+                                        q.*,
+                                        ARRAY_AGG(o.label) AS option_labels
+                                    FROM
+                                        metac_question_question q
+                                    LEFT JOIN
+                                        metac_question_option o ON q.id = o.question_id
+                                    WHERE  type in ('conditional_group', 'group', 'notebook', 'discussion', 'claim') or group_id is not null
+                                    
+                                    GROUP BY q.id
+                                ) q
+                        LEFT JOIN 
+                            metac_question_conditional qc ON qc.child_id = q.id
+                        -- Ensure parents go first
+                        ORDER BY group_id DESC;
+                                                                """,
+            itersize=10000,
+        ):
+            group_id = old_question["group_id"]
 
-            cursor.execute("DELETE FROM projects_projectuserpermission")
-            cursor.execute(
-                "DELETE FROM posts_post_projects USING projects_project "
-                "WHERE posts_post_projects.project_id = projects_project.id AND projects_project.type = 'personal_project';"
-            )
-            cursor.execute(
-                """
-                UPDATE posts_post
-                SET default_project_id = NULL
-                WHERE default_project_id IN (
-                    SELECT id
-                    FROM projects_project
-                    WHERE type = 'personal_project'
-                );
-                """
-            )
-            cursor.execute(
-                "DELETE FROM projects_project WHERE type = 'personal_project'"
-            )
+            # If root
+            if not group_id:
+                old_groups[old_question["id"]] = {**old_question, "children": []}
+            else:
+                old_groups[group_id]["children"].append(old_question)
 
-        print("Starting migration script")
-
-        # Function
-        migrate_permissions()
+        print("Migrating notebooks")
+        migrate_questions__notebook(list(old_groups.values()))
 
         # Reset sql sequences
         reset_sequence()
+
+
