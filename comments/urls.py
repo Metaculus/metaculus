ed2e278b8e35d93905d49145c64d62a23fc47d41
ludant <rudolfordoyne@protonmail.com>
2024-06-28 16:03:16 -0500

    add createComment endpoint

--- comments/urls.py
+++ comments/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("comments", views.comments_list_api_view, name="comment-list"),
     path("comments/<int:pk>/delete", views.comment_delete_api_view, name="comment-delete"),
+    path("comments/create", views.comment_create_api_view, name="comment-create"),
 ]
