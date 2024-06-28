786530e25f543d1ba2729b5292b5d2514e041d0a
ludant <rudolfordoyne@protonmail.com>
2024-06-28 13:12:06 -0500

    fix softDeleteComment

--- front_end/src/app/(main)/questions/actions.ts
+++ front_end/src/app/(main)/questions/actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
+import CommentsApi from "@/services/comments";
 import PostsApi, { PostsParams } from "@/services/posts";
 import QuestionsApi from "@/services/questions";
 import { FetchError } from "@/types/fetch";
@@ -145,6 +146,18 @@ export async function updateNotebook(
 export async function uploadImage(formData: FormData) {
   try {
     return await PostsApi.uploadImage(formData);
+  } catch (err) {
+    const error = err as FetchError;
+
+    return {
+      errors: error.data,
+    };
+  }
+}
+
+export async function softDeleteComment(commentId: number) {
+  try {
+    return await CommentsApi.softDeleteComment(commentId);
   } catch (err) {
     const error = err as FetchError;
 
