11dcd1613a42a66004e1abd6095d8e3c85a6b415
George3d6 <george@cerebralab.com>
2024-07-02 17:09:19 +0200

    fixing new forms

--- front_end/src/app/(main)/questions/create/group/page.tsx
+++ front_end/src/app/(main)/questions/create/group/page.tsx
@@ -1,9 +1,20 @@
-"use client";
+import { useSearchParams } from "next/navigation";
 
-import QuestionForm from "../../components/question_form";
+import { SearchParams } from "@/types/navigation";
+import { PostWithForecasts } from "@/types/post";
 
-const GroupQuestionCreator: React.FC = ({}) => {
-  return <QuestionForm question_type={"group"}></QuestionForm>;
+import { getPost } from "../../actions";
+import QuestionForm from "../../components/question_form_old";
+
+const GroupQuestionCreator: React.FC<{ searchParams: SearchParams }> = async ({
+  searchParams,
+}) => {
+  const post_id = searchParams["post_id"];
+  let post: PostWithForecasts | null = null;
+  if (post_id) {
+    post = await getPost(Number(post_id));
+  }
+  return <QuestionForm question_type={"group"} post={post}></QuestionForm>;
 };
 
 export default GroupQuestionCreator;
