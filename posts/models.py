1bedcbe7779260873fc994fe007c48f3372fade3
George3d6 <george@cerebralab.com>
2024-07-02 17:09:19 +0200

    fixing new forms

--- posts/models.py
+++ posts/models.py
@@ -214,8 +214,8 @@ class Notebook(TimeStampedModel):
 
     markdown = models.TextField()
     type = models.CharField(max_length=100, choices=NotebookType)
-    news_type = models.CharField(max_length=100, blank=True)
-    image_url = models.URLField(blank=True)
+    news_type = models.CharField(max_length=100, blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
 
 
 class Post(TimeStampedModel):
