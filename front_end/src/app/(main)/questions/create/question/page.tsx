b80ca994bfc4df72591c9e7a5772fb8c8ea44f85
George3d6 <george@cerebralab.com>
2024-07-03 03:12:42 +0200

    new question form

--- front_end/src/app/(main)/questions/create/question/page.tsx
+++ front_end/src/app/(main)/questions/create/question/page.tsx
@@ -1,9 +1,34 @@
-"use client";
+import PostsApi from "@/services/posts";
+import { SearchParams } from "@/types/navigation";
 
-import QuestionForm from "../../components/question_form_old";
+import QuestionForm from "../../components/question_form";
 
-const QuestionCreator: React.FC = ({}) => {
-  return <QuestionForm question_type={undefined}></QuestionForm>;
+const QuestionCreator: React.FC<{ searchParams: SearchParams }> = async ({
+  searchParams,
+}) => {
+  let post = null;
+  if (searchParams["post_id"]) {
+    post = await PostsApi.getPost(Number(searchParams["post_id"]));
+  }
+  if (!post && !searchParams["type"]) {
+    throw new Error("Type is required !");
+  }
+
+  const question_type: string = post
+    ? (post.question?.type as string)
+    : (searchParams["type"] as string);
+  return (
+    <QuestionForm
+      post={post}
+      questionType={question_type}
+      mode={post ? "edit" : "create"}
+      tournament_id={
+        searchParams["tournament_id"]
+          ? Number(searchParams["tournament_id"])
+          : undefined
+      }
+    ></QuestionForm>
+  );
 };
 
 export default QuestionCreator;
--- front_end/src/types/post.ts
+++ front_end/src/types/post.ts
@@ -90,8 +90,8 @@ export type Post<QT = Question> = {
   updated_at: string;
   published_at: string;
   curation_status_updated_at: string | null;
-  aim_to_close_at: string | null;
-  aim_to_resolve_at: string | null;
+  aim_to_close_at: string;
+  aim_to_resolve_at: string;
   vote: PostVote;
   nr_forecasters: number;
   author_username: string;
