21139bcc1c89081cf03c3ad5d44c8ba67b682686
ludant <rudolfordoyne@protonmail.com>
2024-07-03 19:31:03 -0500

    add filtering private comments

--- front_end/src/components/comment_feed/index.tsx
+++ front_end/src/components/comment_feed/index.tsx
@@ -2,6 +2,8 @@
 import { FC, useState } from "react";
 
 import Comment from "@/components/comment_feed/comment";
+import CommentEditor from "@/components/comment_feed/comment_editor";
+import ButtonGroup from "@/components/ui/button_group";
 import Hr from "@/components/ui/hr";
 import { CommentPermissions, CommentType } from "@/types/comment";
 import { Post, ProjectPermissions } from "@/types/post";
@@ -13,11 +15,26 @@ type Props = {
   profile?: UserProfile;
 };
 
+const feedOptions = [
+  {
+    id: "public",
+    label: "public",
+  },
+  {
+    id: "private",
+    label: "private",
+  },
+];
+
 const CommentFeed: FC<Props> = ({ initialComments, post, profile }) => {
   const [numberOfComments, setNumberOfComments] = useState(10);
+  const [feedSection, setFeedSection] = useState("public");
 
-  const comments = initialComments.slice(0, numberOfComments);
-
+  const comments = initialComments
+    .slice(0, numberOfComments)
+    .filter((comment) =>
+      feedSection === "public" ? !comment.is_private : comment.is_private
+    );
   let url = "";
 
   let permissions: CommentPermissions = CommentPermissions.VIEWER;
@@ -33,9 +50,26 @@ const CommentFeed: FC<Props> = ({ initialComments, post, profile }) => {
     url += `/accounts/profile/${profile.id}`;
   }
 
-  if (comments.length === 0) return null;
   return (
     <section>
+      <Hr className="my-4" />
+      <div className="my-4 flex flex-row gap-4">
+        <h2
+          className="mb-1 mt-0 flex scroll-mt-16 items-baseline justify-between break-anywhere"
+          id="comment-section"
+        >
+          Comments
+        </h2>
+        <ButtonGroup
+          value={feedSection}
+          buttons={feedOptions}
+          onChange={(selection) => {
+            setFeedSection(selection);
+          }}
+          variant="tertiary"
+        />
+      </div>
+      {post && <CommentEditor />}
       {comments.map((comment: CommentType) => (
         <div key={comment.id}>
           <Hr className="my-4" />
@@ -43,6 +77,14 @@ const CommentFeed: FC<Props> = ({ initialComments, post, profile }) => {
           <Comment comment={comment} url={url} permissions={permissions} />
         </div>
       ))}
+      {comments.length == 0 && (
+        <>
+          <Hr className="my-4" />
+          <div className="text-center italic text-gray-700 dark:text-gray-700-dark">
+            no comments
+          </div>
+        </>
+      )}
     </section>
   );
 };
