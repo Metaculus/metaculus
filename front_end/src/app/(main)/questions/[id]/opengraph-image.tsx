af7926b7e715e5059167af8c19021769d9d30615
Nikita <nikita.develops.work@gmail.com>
2024-07-05 14:26:31 +0300

    feat: skip beta login for embedding requests

--- front_end/src/app/(main)/questions/[id]/opengraph-image.tsx
+++ front_end/src/app/(main)/questions/[id]/opengraph-image.tsx
@@ -4,7 +4,10 @@ export const alt = "community predictions";
 export const size = { width: 1200, height: 630 };
 
 export default async function Image({ params }: { params: { id: number } }) {
-  return await fetch(
-    `${process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000"}/questions/${params.id}/api/generate-preview`
-  );
+  const origin = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
+  return await fetch(`${origin}/questions/${params.id}/api/generate-preview`, {
+    headers: {
+      "image-preview-request": "true",
+    },
+  });
 }
