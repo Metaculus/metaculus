af7310e56b92891250822d851036772e31fa62f5
Nikita <nikita.develops.work@gmail.com>
2024-07-05 11:40:41 +0300

    fix: update origin in image generation route handlers

--- front_end/src/app/(main)/questions/[id]/opengraph-image.tsx
+++ front_end/src/app/(main)/questions/[id]/opengraph-image.tsx
@@ -1,19 +1,10 @@
-import { headers } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-
 export const runtime = "edge";
 export const contentType = "image/png";
 export const alt = "community predictions";
 export const size = { width: 1200, height: 630 };
 
 export default async function Image({ params }: { params: { id: number } }) {
-  const headersList = headers();
-  const url = headersList.get("x-url");
-  if (!url) {
-    return new NextResponse("Missing x-url header", { status: 400 });
-  }
-
-  const request = new NextRequest(url);
-  const origin = request.nextUrl.origin;
-  return await fetch(`${origin}/questions/${params.id}/api/generate-preview`);
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000"}/questions/${params.id}/api/generate-preview`
+  );
 }
