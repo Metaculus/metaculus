5acbb6f362197dc79e73ae357b24a07a2ab35c1a
Hlib Kononenko <alexxkur@gmail.com>
2024-07-08 21:08:51 +0200

    Extra filters optimization + improvements (#79)
    * BE: added user comment view support + filtering
    
    * Added `PostUserSnapshot.last_forecast_date` + migration
    
    * Fixed tests
    
    * Small fixes
    
    * - Moved questions forecast to a separate service
    - Added last forecast date update methods
    
    * Added comments counter cache update on comments creation
    
    * Small comment adjustments and extra tests

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
@@ -8,9 +6,9 @@ from rest_framework.serializers import DateTimeField
 
 from posts.services import get_post_permission_for_user
 from projects.permissions import ObjectPermission
-from questions.models import Forecast, Question
+from questions.models import Question
 from questions.serializers import validate_question_resolution
-from questions.services import resolve_question
+from questions.services import resolve_question, create_forecast
 
 
 @api_view(["POST"])
@@ -38,28 +36,6 @@ def create_forecast_api_view(request, pk: int):
     permission = get_post_permission_for_user(question.get_post(), user=request.user)
     ObjectPermission.can_forecast(permission, raise_exception=True)
 
-    data = request.data
-    now = datetime.now()
-    prev_forecasts = (
-        Forecast.objects.filter(question=question, author=request.user)
-        .order_by("start_time")
-        .last()
-    )
-    if prev_forecasts:
-        prev_forecasts.end_time = now
-        prev_forecasts.save()
-
-    forecast = Forecast.objects.create(
-        question=question,
-        author=request.user,
-        start_time=now,
-        end_time=None,
-        continuous_cdf=data.get("continuous_cdf", None),
-        probability_yes=data.get("probability_yes", None),
-        probability_yes_per_category=data.get("probability_yes_per_category", None),
-        distribution_components=None,
-        slider_values=data.get("slider_values", None),
-    )
-    forecast.save()
+    create_forecast(question=question, user=request.user, **request.data)
 
     return Response({}, status=status.HTTP_201_CREATED)
