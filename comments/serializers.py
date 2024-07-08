63eaf27fcf213906d4c3e9bae6ec29a9b7d6717d
gleb <alexxkur@gmail.com>
2024-07-08 21:39:33 +0200

    Extra comments fixes

--- comments/serializers.py
+++ comments/serializers.py
@@ -27,16 +27,19 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class CommentWriteSerializer(serializers.ModelSerializer):
     on_post = serializers.IntegerField(required=True)
-    parent = serializers.IntegerField(required=False)
+    parent = serializers.IntegerField(required=False, allow_null=True)
     include_forecast = serializers.BooleanField(required=False)
     is_private = serializers.BooleanField(required=False, default=False)
 
     class Meta:
         model = Comment
-        fields = ("on_post", "parent", "included_forecast", "is_private", "text")
+        fields = ("on_post", "parent", "include_forecast", "is_private", "text")
 
     def validate_on_post(self, value):
         return Post.objects.get(pk=value)
 
     def validate_parent(self, value):
+        if not value:
+            return value
+
         return Comment.objects.get(pk=value)
