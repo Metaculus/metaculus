e83c0d28a71153e527c253f0707768e2d1ed2fcf
Nikita <93587872+ncarazon@users.noreply.github.com>
2024-07-09 17:53:44 +0300

    feat: handle table and math in markdown editor

--- front_end/src/app/(main)/news/testing/page.tsx
+++ front_end/src/app/(main)/news/testing/page.tsx
@@ -11,7 +11,18 @@ const markdown = `
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt dicta ea enim et ex, fugit ipsa iste laboriosam nobis nostrum repellendus sequi sit suscipit ut vel velit vero vitae.
 
-***
+## Math
+
+\\(e=mc^2\\)
+
+\\[e=mc^2\\]
+
+\\begin{align}
+  \\log_2 \\left ( \\frac{p}{0.5} \\right ) &= \\log_2 \\left ( p \\right ) + 1 \\\\
+  \\log_2 \\left ( \\frac{p}{0.5} \\right ) &= \\frac{\\log(p) - \\log(0.5)}{\\log(1) - \\log(0.5)}
+\\end{align}
+
+\\begin{aligned} 2x - 4 &= 6 \\\\ 2x &= 10 \\\\ x &= 5 \\end{aligned}
 
 > quote
 
@@ -19,8 +30,8 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt dicta e
 - list item 2
 
 [Link](https://google.com)
-
 ![image](https://fastly.picsum.photos/id/780/200/300.jpg?hmac=Zmxf0t2fpCbfZrR5NAXA_IKAP_8P6fYe9P440jUTWag "Title")
+
 ![image](https://fastly.picsum.photos/id/780/200/300.jpg?hmac=Zmxf0t2fpCbfZrR5NAXA_IKAP_8P6fYe9P440jUTWag "Title")
 
 ![image1](https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68 "Title")
@@ -28,18 +39,17 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt dicta e
 <EmbeddedQuestion id="1" />
 
 <EmbeddedQuestion id="2" />
-
 `;
 
 export default function TestNewsEditor() {
   return (
     <main className="p-4">
-      <h1>Read mode:</h1>
+      <h1>Write mode:</h1>
       <div className="h-50vh mx-auto max-w-3xl overflow-auto rounded-lg bg-gray-0 dark:bg-gray-100-dark">
         <MarkdownEditor markdown={markdown} mode="write" />
       </div>
       <hr />
-      <h1>Write mode:</h1>
+      <h1>Read mode:</h1>
       <div className="h-50vh mx-auto max-w-6xl overflow-auto bg-gray-0 px-2 dark:bg-gray-100-dark">
         <MarkdownEditor markdown={markdown} mode="read" />
       </div>
