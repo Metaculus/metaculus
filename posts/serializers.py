b90c0c98913ab424b7701236d0b34a2ed6048770
gleb <alexxkur@gmail.com>
2024-07-09 22:09:09 +0200

    Small fixes

--- posts/serializers.py
+++ posts/serializers.py
@@ -41,7 +41,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "title",
-            "url_title",
             "author_id",
             "author_username",
             "projects",
