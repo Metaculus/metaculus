857a226c494df80731dd0c070e8a43150e3f0942
aseckin <atakanseckin@gmail.com>
2024-07-10 01:11:31 +0200

    Styled question creation page

--- front_end/src/app/(main)/questions/create/page.tsx
+++ front_end/src/app/(main)/questions/create/page.tsx
@@ -1,7 +1,11 @@
+// page.tsx
+
 import Link from "next/link";
 import React from "react";
 
 import { SearchParams } from "@/types/navigation";
+
+import QuestionTypePicker from "../components/question_type_picker";
 
 const Creator: React.FC<{ searchParams: SearchParams }> = ({
   searchParams,
@@ -20,143 +24,93 @@ const Creator: React.FC<{ searchParams: SearchParams }> = ({
   };
 
   return (
-    <div className="m-12 flex max-w-[840px] flex-col justify-center bg-white p-4 dark:bg-blue-900">
-      <p className="ml-4 text-3xl">Create New Content</p>
-      <p className="ml-4">
-        Check out our <a href="/question-writing">question writing guide</a> for
-        tips. Good questions are approved faster, and get more predictions.
+    <div className="mb-4 flex max-w-[840px] flex-col justify-center self-center rounded-md bg-white px-4 py-4 pb-5 dark:bg-blue-900 md:m-8 md:mx-auto md:px-8 md:pb-8 lg:m-12 lg:mx-auto">
+      <div className="flex flex-col gap-2 md:gap-3">
+        <h1 className="text-2xl font-medium md:text-3xl">Create New Content</h1>
+        <span className="text-sm text-gray-600 dark:text-gray-300 md:text-base">
+          Check out our <a href="/question-writing">question writing guide</a>{" "}
+          for tips. Good questions are approved faster, and get more
+          predictions.
+        </span>
+        <span className="text-sm text-gray-600 dark:text-gray-300 md:text-base">
+          We have high standards for question quality. We also favor questions
+          on our core topic areas or that we otherwise judge valuable. We may
+          not publish questions that are not a good fit.
+        </span>
+        <hr className="my-1.5 border-blue-400 dark:border-blue-700/50" />
+      </div>
+      <p className="text-lg font-light text-gray-700 dark:text-gray-300">
+        Single Question
       </p>
-      <p className="ml-4">
-        We have high standards for question quality. We also favor questions on
-        our core topic areas or that we otherwise judge valuable. We may not
-        publish questions that are not a good fit.
-      </p>
-      <p className="text-l m-0 ml-4 mt-4 font-light text-gray-600 dark:text-gray-200">
-        Questions
-      </p>
-      <div className="flex w-full flex-wrap md:flex-row md:flex-nowrap">
-        <div className="w-full flex-row">
-          <Link
-            href={createHref("/questions/create/question", { type: "binary" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Binary Question</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “Will it rain today?”
-              </p>
-            </div>
-          </Link>
-          <Link
-            href={createHref("/questions/create/question", { type: "numeric" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Numeric Range</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “How much rain next month?”
-              </p>
-            </div>
-          </Link>
+      <div className="flex w-full flex-wrap gap-3 md:flex-row md:flex-nowrap md:gap-4">
+        <div className="flex w-full flex-col gap-3 md:gap-4">
+          <QuestionTypePicker
+            url={createHref("/questions/create/question", { type: "binary" })}
+            questionType="Binary Question"
+            questionExample="Will it rain today?"
+          />
+          <QuestionTypePicker
+            url={createHref("/questions/create/question", { type: "numeric" })}
+            questionType="Numeric Range"
+            questionExample="How much rain next month?"
+          />
         </div>
-        <div className="w-full flex-row">
-          <Link
-            href={createHref("/questions/create/question", { type: "date" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Date Range</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “When will it rain?”
-              </p>
-            </div>
-          </Link>
-          <Link
-            href={createHref("/questions/create/question", {
+        <div className="flex w-full flex-col gap-3 md:gap-4">
+          <QuestionTypePicker
+            url={createHref("/questions/create/question", { type: "date" })}
+            questionType="Date Range"
+            questionExample="When will it rain?"
+          />
+          <QuestionTypePicker
+            url={createHref("/questions/create/question", {
               type: "multiple_choice",
             })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Multiple Choice</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “Which city will get the most rain?”
-              </p>
-            </div>
-          </Link>
+            questionType="Multiple Choice"
+            questionExample="Which city will get the most rain?"
+          />
         </div>
       </div>
 
-      <p className="text-l m-0 ml-4 mt-4 font-light text-gray-600 dark:text-gray-200">
+      <p className="text-lg  font-light text-gray-600 dark:text-gray-200">
         Question Group
       </p>
-      <div className="flex w-full flex-wrap md:flex-row md:flex-nowrap">
-        <div className="w-full flex-row">
-          <Link
-            href={createHref("/questions/create/group", { subtype: "binary" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Binary Group</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “Will it rain in these cities today?”
-              </p>
-            </div>
-          </Link>
-          <Link
-            href={createHref("/questions/create/group", { subtype: "numeric" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Numeric Group</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “How much will it rain for these cities?”
-              </p>
-            </div>
-          </Link>
+      <div className="flex w-full flex-wrap gap-3 md:flex-row md:flex-nowrap md:gap-4">
+        <div className="flex w-full flex-col gap-3 md:gap-4">
+          <QuestionTypePicker
+            url={createHref("/questions/create/group", { subtype: "binary" })}
+            questionType="Binary Group"
+            questionExample="Will it rain in these cities today?"
+          />
+          <QuestionTypePicker
+            url={createHref("/questions/create/group", { subtype: "numeric" })}
+            questionType="Numeric Group"
+            questionExample="How much will it rain for these cities?"
+          />
         </div>
-        <div className="w-full flex-row">
-          <Link
-            href={createHref("/questions/create/group", { subtype: "date" })}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Date Group</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “When will it rain for the following cities?”
-              </p>
-            </div>
-          </Link>
-          <Link
-            href={createHref("/questions/create/conditional")}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Conditional Pair</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                “If it rains today, will it rain tomorrow?”
-              </p>
-            </div>
-          </Link>
+        <div className="flex w-full flex-col gap-3 md:gap-4">
+          <QuestionTypePicker
+            url={createHref("/questions/create/group", { subtype: "date" })}
+            questionType="Date Group"
+            questionExample="When will it rain for the following cities?"
+          />
+          <QuestionTypePicker
+            url={createHref("/questions/create/conditional")}
+            questionType="Conditional Pair"
+            questionExample="If it rains today, will it rain tomorrow?"
+          />
         </div>
       </div>
 
-      <p className="text-l m-0 ml-4 mt-4 font-light text-gray-600 dark:text-gray-200">
+      <p className="text-lg  font-light text-gray-600 dark:text-gray-200">
         Posts
       </p>
       <div className="flex w-full flex-wrap md:flex-row md:flex-nowrap">
         <div className="w-full flex-row">
-          <Link
-            href={createHref("/questions/create/notebook")}
-            className="no-underline"
-          >
-            <div className="w-100 m-4 rounded-s border p-4 hover:bg-blue-500">
-              <p className="m-0 p-0 text-xl">Notebook</p>
-              <p className="text-m m-0 mt-2 p-0 font-light text-gray-600 dark:text-gray-200">
-                A text-based content that is not a question
-              </p>
-            </div>
-          </Link>
+          <QuestionTypePicker
+            url={createHref("/questions/create/notebook")}
+            questionType="Notebook"
+            questionExample="A text-based content that is not a question"
+          />
         </div>
       </div>
     </div>
