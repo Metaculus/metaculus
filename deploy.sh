5d09e2c2eae7579d9c9cee4b722e419d3ce3d9e2
Hlib Kononenko <alexxkur@gmail.com>
2024-07-11 14:25:26 +0200

    Integrated cron runner (#93)

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ sudo fuser -k 8000/tcp;
 tmux kill-session -t web_backend;
 tmux kill-session -t web_frontend;
 tmux kill-session -t dramatiq;
+tmux kill-session -t cron;
 
 cd /home/ubuntu/rewrite;
 
@@ -30,6 +31,8 @@ tmux new-session -d -s web_backend;
 tmux send-keys -t web_backend 'poetry run gunicorn metaculus_web.wsgi:application --workers 8 --bind 0.0.0.0:8000' C-m;
 tmux new-session -d -s dramatiq;
 tmux send-keys -t dramatiq 'poetry run python3 manage.py rundramatiq --processes 1 --threads 1' C-m;
+tmux new-session -d -s cron;
+tmux send-keys -t cron 'poetry run python3 manage.py cron' C-m;
 
 cd front_end;
 tmux new-session -d -s web_frontend;
