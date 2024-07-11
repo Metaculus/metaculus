00345c409adf927d8f6553d62138e38735383e99
aseckin <atakanseckin@gmail.com>
2024-07-11 14:11:40 +0200

    Category picker styling, added display on focus

--- front_end/src/app/(main)/questions/components/category_picker.tsx
+++ front_end/src/app/(main)/questions/components/category_picker.tsx
@@ -31,7 +31,7 @@ const CategoryPicker: React.FC<{
 
   return (
     <div>
-      <Combobox multiple>
+      <Combobox immediate multiple>
         <div className="relative mt-1">
           <div className="relative w-full cursor-default overflow-hidden rounded border border-gray-500 bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <ComboboxInput
@@ -56,7 +56,7 @@ const CategoryPicker: React.FC<{
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <ComboboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <ComboboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-blue-950 sm:text-sm">
               {filteredCategories.length === 0 && query !== "" ? (
                 <div className="relative cursor-default select-none px-4 py-2 text-gray-700 dark:text-gray-100">
                   Nothing found.
@@ -66,9 +66,9 @@ const CategoryPicker: React.FC<{
                   <ComboboxOption
                     key={category.id}
                     className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      `relative cursor-default select-none ${
                         active
-                          ? "bg-teal-600 text-white"
+                          ? "bg-blue-600 text-white"
                           : "text-gray-900 dark:text-gray-300"
                       }`
                     }
@@ -82,7 +82,7 @@ const CategoryPicker: React.FC<{
                             setFilteredCategories([]);
                             onChange([...categories, category]);
                           }}
-                          className={`block cursor-pointer truncate ${
+                          className={`block cursor-pointer truncate py-2 pl-4 pr-4 ${
                             selected ? "font-medium" : "font-normal"
                           }`}
                         >
@@ -97,21 +97,21 @@ const CategoryPicker: React.FC<{
           </Transition>
         </div>
       </Combobox>
-      <div className="flex flex-col">
+      <div className="mt-2 flex flex-wrap items-start gap-2">
         {categories.map((category: Category, cat_index: number) => {
           return (
             <div
-              className="m-2 w-min min-w-[220px] border p-2 text-xs"
+              className="group flex w-auto cursor-pointer flex-row items-center rounded bg-blue-200 p-2 text-sm dark:bg-blue-700"
               key={cat_index}
+              onClick={(e) => {
+                onChange([
+                  ...categories.slice(0, cat_index),
+                  ...categories.slice(cat_index + 1),
+                ]);
+              }}
             >
               <FontAwesomeIcon
-                onClick={(e) => {
-                  onChange([
-                    ...categories.slice(0, cat_index),
-                    ...categories.slice(cat_index + 1),
-                  ]);
-                }}
-                className="text-red mr-2 cursor-pointer text-red-500"
+                className="mr-2 cursor-pointer text-gray-400 group-hover:text-gray-500 dark:text-blue-500 dark:group-hover:text-gray-200"
                 icon={faX}
               ></FontAwesomeIcon>
               <span>{category.name}</span>
