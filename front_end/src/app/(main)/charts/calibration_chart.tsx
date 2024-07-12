8c4573f00eafafe8226121e4b7963e5041e031c4
George3d6 <george@cerebralab.com>
2024-07-12 02:43:29 +0200

    mock bar chart

--- front_end/src/app/(main)/charts/calibration_chart.tsx
+++ front_end/src/app/(main)/charts/calibration_chart.tsx
@@ -1,39 +1,86 @@
 import React from "react";
-import { VictoryChart, VictoryLine, VictoryArea, VictoryAxis } from "victory";
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryArea,
+  VictoryAxis,
+  VictoryBar,
+  VictoryLabel,
+  VictoryScatter,
+} from "victory";
 
 const CalibrationChart: React.FC<{ data: any }> = ({ data }) => {
   const calibrationData = data;
-
+  console.log(
+    calibrationData.map((d: any, index: number) => {
+      return {
+        x: 1 - 1 / (index + 1),
+        y0: d.y_perfect_ci[0],
+        y: d.y_perfect_ci[1],
+      };
+    })
+  );
   return (
     <VictoryChart domain={{ x: [0, 1], y: [0, 1] }}>
-      {/* Perfect calibration line */}
-      <VictoryLine
-        style={{ data: { stroke: "gray", strokeDasharray: "5,5" } }}
-        data={[
-          { x: 0, y: 0 },
-          { x: 1, y: 1 },
-        ]}
+      <VictoryScatter
+        data={calibrationData.map((d: any, index: number) => {
+          const y =
+            (d.y_real + (index > 0 ? calibrationData[index - 1].y_real : 0)) /
+            2;
+          return {
+            x: (index + 0.5) / calibrationData.length,
+            y0: y - 0.01,
+            y: y,
+            symbol: "diamond",
+          };
+        })}
+        style={{
+          data: {
+            fill: "orange",
+            stroke: "none",
+          },
+        }}
       />
-
+      <VictoryBar
+        barRatio={1.4}
+        data={calibrationData.map((d: any, index: number) => {
+          const y =
+            (d.y_perfect +
+              (index > 0 ? calibrationData[index - 1].y_perfect : 0)) /
+            2;
+          return {
+            x: (index + 0.5) / calibrationData.length,
+            y0: y - 0.01,
+            y: y,
+          };
+        })}
+        style={{ data: { fill: "darkgray", opacity: 1 } }}
+      />
       {/* Confidence interval area */}
-      <VictoryArea
-        data={calibrationData}
-        x={(d) => d.y_perfect}
-        y0={(d) => d.y_perfect_ci[0]}
-        y={(d) => d.y_perfect_ci[1]}
+      <VictoryBar
+        barRatio={1.4}
+        data={calibrationData.map((d: any, index: number) => {
+          return {
+            x: (index + 0.5) / calibrationData.length,
+            y0: d.y_perfect_ci[0],
+            y: d.y_perfect_ci[1],
+          };
+        })}
         style={{ data: { fill: "lightgray", opacity: 0.5 } }}
       />
 
-      {/* Real calibration line */}
-      <VictoryLine
-        data={calibrationData}
-        x={(d) => d.y_perfect}
-        y={(d) => d.y_real}
-        style={{ data: { stroke: "blue" } }}
+      <VictoryAxis
+        tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
+        tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
+        label="Predicted probability"
       />
-
-      <VictoryAxis label="Predicted probability" />
-      <VictoryAxis dependentAxis label="Actual frequency" />
+      <VictoryAxis
+        tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
+        tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
+        dependentAxis
+        label="Actual frequency"
+        axisLabelComponent={<VictoryLabel dy={-12} />}
+      />
     </VictoryChart>
   );
 };
