2b44e137eee569e50fa0cb654a44b952356856c7
Nikita <93587872+ncarazon@users.noreply.github.com>
2024-07-12 15:07:43 +0300

    feat: mobile view for global leaderboard, UI tweaks

---
+++ front_end/src/components/ui/tab_bar/tab_bar.tsx
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+import { ReactNode } from "react";
+
+import Tab from "./tab";
+
+export type TabOption<T> = {
+  value: T;
+  label: string;
+  className?: string;
+  Icon?: ReactNode;
+};
+
+type Props<T> = {
+  options: TabOption<T>[];
+  value: T;
+  onChange: (value: T) => void;
+  className?: string;
+};
+
+const TabBar = <T = string,>({
+  options,
+  value,
+  onChange,
+  className,
+}: Props<T>) => {
+  return (
+    <div className={classNames("flex", className)}>
+      {options.map((option, index) => (
+        <Tab
+          key={`${option.value}`}
+          onClick={() => {
+            onChange(option.value);
+          }}
+          active={option.value === value}
+          className={classNames(
+            "relative grow rounded hover:z-10 focus:z-20",
+            options.length > 1 &&
+              (index === 0
+                ? "rounded-r-none"
+                : index !== options.length - 1
+                  ? "ml-[-1px] rounded-none"
+                  : "ml-[-1px] rounded-l-none")
+          )}
+        >
+          <span className="text-sm font-bold">{option.label}</span>
+        </Tab>
+      ))}
+    </div>
+  );
+};
+
+export default TabBar;
