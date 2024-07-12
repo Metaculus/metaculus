769e096b2293714fbf2b9a0ae55211f10e961626
ludant <rudolfordoyne@protonmail.com>
2024-07-12 15:33:56 -0500

    add included forecasts to comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,6 +3,7 @@ from django.db.models.query import QuerySet
 
 from comments.models import Comment
 from posts.models import Post
+from questions.serializers import ForecastSerializer
 from users.models import User
 from users.serializers import UserCommentSerializer
 
@@ -30,12 +31,12 @@ class CommentSerializer(serializers.ModelSerializer):
 class CommentWriteSerializer(serializers.ModelSerializer):
     on_post = serializers.IntegerField(required=True)
     parent = serializers.IntegerField(required=False, allow_null=True)
-    include_forecast = serializers.BooleanField(required=False)
     is_private = serializers.BooleanField(required=False, default=False)
+    included_forecast = serializers.BooleanField(required=False, default=False)
 
     class Meta:
         model = Comment
-        fields = ("on_post", "parent", "include_forecast", "is_private", "text")
+        fields = ("on_post", "parent", "included_forecast", "is_private", "text")
 
     def validate_on_post(self, value):
         return Post.objects.get(pk=value)
@@ -49,12 +50,15 @@ class CommentWriteSerializer(serializers.ModelSerializer):
 
 def serialize_comment(
     comment: Comment,
-    current_user: User | None = None,
 ) -> dict:
     serialized_data = CommentSerializer(comment).data
 
     # Permissions
     # serialized_data["user_permission"] = post.user_permission
+
+    forecast = comment.included_forecast
+    if forecast is not None:
+        serialized_data["included_forecast"] = ForecastSerializer(forecast).data
 
     # Annotate user's vote
     serialized_data["vote_score"] = comment.vote_score
@@ -69,11 +73,12 @@ def serialize_comment_many(
 ) -> list[dict]:
     qs = Comment.objects.filter(pk__in=[c.pk for c in comments])
 
+    qs = qs.select_related("included_forecast")
     qs = qs.annotate_vote_score()
 
     if current_user and not current_user.is_anonymous:
         qs = qs.annotate_user_vote(current_user)
 
     return [
-        serialize_comment(comment, current_user=current_user) for comment in qs.all()
+        serialize_comment(comment) for comment in qs.all()
     ]
