d17854a4fd4fdda8fb20cf5d61a546159c79e351
George3d6 <george@cerebralab.com>
2024-07-15 13:09:23 +0200

    fixed notebook creation

--- front_end/src/app/(main)/questions/components/group_form.tsx
+++ front_end/src/app/(main)/questions/components/group_form.tsx
@@ -259,7 +259,10 @@ const GroupForm: React.FC<Props> = ({
       </p>
       <form
         onSubmit={async (e) => {
-          e.preventDefault(); // Good for debugging
+          if (!control.getValues("default_project_id")) {
+            control.setValue("default_project_id", siteMain.id);
+          }
+          // e.preventDefault(); // Good for debugging
           await control.handleSubmit(
             async (data) => {
               await submitQuestion(data);
