216fb13c28e46208d59ebd9e7f025bd70a6b731d
Hlib Kononenko <alexxkur@gmail.com>
2024-07-17 19:04:45 +0200

    Added Forecasts db cache (#114)

--- questions/management/commands/build_forecasts.py
+++ questions/management/commands/build_forecasts.py
@@ -1,28 +1,33 @@
+import logging
 import time
 
 from django.core.management.base import BaseCommand
 
 from questions.models import Question
-from questions.services import build_question_forecasts
+from questions.tasks import run_build_question_forecasts
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Composes Question forecasts"
+    help = "Builds forecasts for all questions"
 
     def handle(self, *args, **options):
-        qs = (
-            Question.objects.annotate_forecasts_count()
-            .prefetch_related("forecast_set")
-            .filter(forecasts_count__gt=100)
-        )
+        qs = Question.objects.all().prefetch_related("forecast_set")
         total = qs.count()
         processed = 0
         tm = time.time()
 
-        print(f"Found {total} questions with > forecasts")
+        print(f"Found {total} questions with forecasts")
 
         for question in qs.iterator(chunk_size=100):
-            build_question_forecasts(question)
+
+            try:
+                run_build_question_forecasts(question.id)
+            except:
+                logger.exception(
+                    f"Failed to generate forecast for question {question.id}"
+                )
 
             processed += 1
             if not processed % 100:
