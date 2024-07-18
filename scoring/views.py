67d61ede200c192f1bc698512a35245c208877d8
Luke Sabor <lukesabor@gmail.com>
2024-07-18 16:41:07 -0700

    uncomment is staff, also the previous commit is the entire implementation of question writing global leaderboards

--- scoring/views.py
+++ scoring/views.py
@@ -47,8 +47,8 @@ def global_leaderboard(
     entries = leaderboard.entries.select_related("user").order_by("rank")
     entries = entries.filter(rank__lte=max(3, entries.count() * 0.05))
 
-    # if not user.is_staff:
-    #     entries = entries.filter(excluded=False)
+    if not user.is_staff:
+        entries = entries.filter(excluded=False)
 
     leaderboard_data["entries"] = LeaderboardEntrySerializer(entries, many=True).data
     # add user entry
