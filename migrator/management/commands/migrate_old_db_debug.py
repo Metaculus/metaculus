f7d01f1d40c7a67ede23de7db005e27d4b681f7e
gleb <alexxkur@gmail.com>
2024-07-18 20:55:04 +0200

    hotfix

--- migrator/management/commands/migrate_old_db_debug.py
+++ migrator/management/commands/migrate_old_db_debug.py
@@ -1,19 +1,8 @@
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
-from django.db import connection
 
-from migrator.services.migrate_questions import (
-    migrate_questions,
-    migrate_questions__notebook,
-    migrate_post_user_snapshots,
-    migrate_post_snapshots_forecasts,
-)
-
-from ...services.migrate_permissions import migrate_permissions
-from ...services.post_migrate import (
-    post_migrate_calculate_divergence,
-    post_migrate_movements,
-)
-from ...utils import paginated_query, reset_sequence
+from posts.tasks import run_compute_movement
+from ...utils import reset_sequence
 
 
 class Command(BaseCommand):
@@ -22,8 +11,8 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        post_migrate_movements()
-        # migrate_post_user_snapshots()
-        # migrate_post_snapshots_forecasts()
+        run_compute_movement()
+        call_command("build_forecasts")
+
         # Reset sql sequences
         reset_sequence()
