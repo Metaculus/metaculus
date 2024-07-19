9418aeda12d55af4eaaf34353a3092657041630f
aseckin <atakanseckin@gmail.com>
2024-07-19 16:20:00 +0200

    fix apostrophe

--- front_end/src/app/(main)/charts/calibration_chart.tsx
+++ front_end/src/app/(main)/charts/calibration_chart.tsx
@@ -7,67 +7,81 @@ import {
   VictoryBar,
   VictoryLabel,
   VictoryScatter,
+  VictoryContainer,
 } from "victory";
 
 const CalibrationChart: React.FC<{ data: any }> = ({ data }) => {
   const calibrationData = data;
   return (
-    <VictoryChart domain={{ x: [0, 1], y: [0, 1] }}>
-      <VictoryScatter
-        data={calibrationData.map((d: any, index: number) => {
-          const y = d.user_middle_quartile;
-          return {
-            x: (index + 0.5) / calibrationData.length,
-            y0: y - 0.01,
-            y: y,
-            symbol: "diamond",
-          };
-        })}
-        style={{
-          data: {
-            fill: "orange",
-            stroke: "none",
-          },
-        }}
-      />
-      <VictoryBar
-        barRatio={1.4}
-        data={calibrationData.map((d: any, index: number) => {
-          const y = d.perfect_calibration;
-          return {
-            x: (index + 0.5) / calibrationData.length,
-            y0: y - 0.01,
-            y: y,
-          };
-        })}
-        style={{ data: { fill: "darkgray", opacity: 1 } }}
-      />
-      {/* Confidence interval area */}
-      <VictoryBar
-        barRatio={1.4}
-        data={calibrationData.map((d: any, index: number) => {
-          return {
-            x: (index + 0.5) / calibrationData.length,
-            y0: d.user_lower_quartile,
-            y: d.user_upper_quartile,
-          };
-        })}
-        style={{ data: { fill: "lightgray", opacity: 0.5 } }}
-      />
-
-      <VictoryAxis
-        tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
-        tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
-        label="Predicted probability"
-      />
-      <VictoryAxis
-        tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
-        tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
-        dependentAxis
-        label="Actual frequency"
-        axisLabelComponent={<VictoryLabel dy={-12} />}
-      />
-    </VictoryChart>
+    <div className="mb-5 size-full">
+      <VictoryChart
+        domain={{ x: [0, 1], y: [0, 1] }}
+        containerComponent={<VictoryContainer responsive={true} />}
+        padding={{ top: 24, bottom: 24, left: 35, right: 12 }}
+      >
+        <VictoryAxis
+          tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
+          tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
+          style={{
+            tickLabels: { fontSize: 10, fontWeight: "lighter", opacity: 0.6 },
+            axis: { stroke: "#ECEDEE" },
+            grid: { stroke: "#ECEDEE" },
+          }}
+        />
+        <VictoryAxis
+          tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
+          tickFormat={(t: number) => `${(t * 100).toFixed(0)}%`}
+          dependentAxis
+          axisLabelComponent={<VictoryLabel dy={-12} />}
+          style={{
+            tickLabels: { fontSize: 10, fontWeight: "lighter", opacity: 0.6 },
+            axis: { stroke: "#ECEDEE" },
+            grid: { stroke: "#ECEDEE" },
+          }}
+        />
+        <VictoryScatter
+          data={calibrationData.map((d: any, index: number) => {
+            const y = d.user_middle_quartile;
+            return {
+              x: (index + 0.5) / calibrationData.length,
+              y0: y - 0.01,
+              y: y,
+              symbol: "diamond",
+            };
+          })}
+          style={{
+            data: {
+              fill: "orange",
+              stroke: "none",
+            },
+          }}
+        />
+        <VictoryBar
+          barRatio={1.2}
+          data={calibrationData.map((d: any, index: number) => {
+            const y = d.perfect_calibration;
+            return {
+              x: (index + 0.5) / calibrationData.length,
+              y0: y - 0.01,
+              y: y,
+            };
+          })}
+          style={{ data: { fill: "darkgray", opacity: 1 } }}
+        />
+        {/* Confidence interval area */}
+        <VictoryBar
+          barRatio={1.4}
+          data={calibrationData.map((d: any, index: number) => {
+            return {
+              x: (index + 0.5) / calibrationData.length,
+              y0: d.user_lower_quartile,
+              y: d.user_upper_quartile,
+            };
+          })}
+          style={{ data: { fill: "lightgray", opacity: 0.5 } }}
+        />
+      </VictoryChart>
+    </div>
   );
 };
 
