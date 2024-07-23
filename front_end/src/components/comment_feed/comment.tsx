a90b907ef5ef7301afaba887046ab117198eec4e
ludant <rudolfordoyne@protonmail.com>
2024-07-23 13:25:47 -0500

    add cancel button to comment replies

--- front_end/src/components/comment_feed/comment.tsx
+++ front_end/src/components/comment_feed/comment.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { faChevronRight, faReply } from "@fortawesome/free-solid-svg-icons";
+import {
+  faXmark,
+  faChevronRight,
+  faReply,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { useLocale, useTranslations } from "next-intl";
@@ -277,12 +281,24 @@ const Comment: FC<CommentProps> = ({
                 userVote: comment.user_vote ?? null,
               }}
             />
-            {!onProfile && (
-              <Button onClick={() => setIsReplying(true)} variant="text">
-                <FontAwesomeIcon icon={faReply} />
-                {t("reply")}
-              </Button>
-            )}
+            {!onProfile &&
+              (isReplying ? (
+                <Button
+                  className="ml-auto p-2"
+                  variant="text"
+                  onClick={() => {
+                    setIsReplying(false);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                  Cancel
+                </Button>
+              ) : (
+                <Button onClick={() => setIsReplying(true)} variant="text">
+                  <FontAwesomeIcon icon={faReply} />
+                  {t("reply")}
+                </Button>
+              ))}
           </div>
 
           <DropdownMenu items={menuItems} />
