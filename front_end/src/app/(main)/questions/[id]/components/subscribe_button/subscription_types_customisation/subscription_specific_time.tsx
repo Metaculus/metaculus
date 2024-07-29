f9370a302b492e37252daffddad406748bd18682
gleb <alexxkur@gmail.com>
2024-07-29 13:29:29 +0100

    Subscriptions localization

--- front_end/src/app/(main)/questions/[id]/components/subscribe_button/subscription_types_customisation/subscription_specific_time.tsx
+++ front_end/src/app/(main)/questions/[id]/components/subscribe_button/subscription_types_customisation/subscription_specific_time.tsx
@@ -1,5 +1,5 @@
 import { formatInTimeZone } from "date-fns-tz";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { FC, useMemo } from "react";
 
 import DatetimeUtc from "@/components/ui/datetime_utc";
@@ -10,17 +10,21 @@ import { formatDate } from "@/utils/date_formatters";
 
 import { SubscriptionSectionProps } from "./types";
 
-const RECURRENCE_INTERVAL_OPTIONS: SelectOption<string>[] = [
-  { value: "", label: "Doesn't repeat" },
-  { value: "7 00:00:00", label: "Weekly" },
-  { value: "30 00:00:00", label: "Monthly" },
-  { value: "365 00:00:00", label: "Yearly" },
-];
-
 const SubscriptionSectionSpecificTime: FC<
   SubscriptionSectionProps<PostSubscriptionSpecificTime>
 > = ({ subscription, onChange, post }) => {
+  const t = useTranslations();
   const locale = useLocale();
+
+  const options: SelectOption<string>[] = useMemo(
+    () => [
+      { value: "", label: t("reminderLabelNoRepeat") },
+      { value: "7 00:00:00", label: t("reminderLabelWeekly") },
+      { value: "30 00:00:00", label: t("reminderLabelMonthly") },
+      { value: "365 00:00:00", label: t("reminderLabelAnnually") },
+    ],
+    [t]
+  );
 
   const currentDateTime = useMemo(
     () => formatInTimeZone(new Date(), "UTC", "yyyy-MM-dd'T'HH:mm:ss'Z'"),
@@ -29,7 +33,7 @@ const SubscriptionSectionSpecificTime: FC<
 
   return (
     <div>
-      <p>Notify me on: </p>
+      <p>{t("reminderDateDescription")}: </p>
       <div>
         <DatetimeUtc
           min={currentDateTime}
@@ -40,16 +44,19 @@ const SubscriptionSectionSpecificTime: FC<
         <Select
           defaultValue={subscription.recurrence_interval}
           onChange={(e) => onChange("recurrence_interval", e.target.value)}
-          options={RECURRENCE_INTERVAL_OPTIONS}
+          options={options}
           className="ml-2 border-0"
         />
       </div>
       <p className="mt-4 opacity-70">
-        Reminder: this question opened on{" "}
-        {post.published_at && formatDate(locale, new Date(post.published_at))}{" "}
-        and closes on{" "}
-        {post.scheduled_close_time &&
-          formatDate(locale, new Date(post.scheduled_close_time))}
+        {t("followModalReminderThisQuestionOpened", {
+          published_at:
+            post.published_at &&
+            formatDate(locale, new Date(post.published_at)),
+          scheduled_close_time:
+            post.scheduled_close_time &&
+            formatDate(locale, new Date(post.scheduled_close_time)),
+        })}
       </p>
     </div>
   );
