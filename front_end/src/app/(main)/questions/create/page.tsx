dd759da258e412309c51b5362ab05b2aa97fd2f0
ludant <rudolfordoyne@protonmail.com>
2024-08-07 12:06:52 -0500

    add translations

--- front_end/src/app/(main)/questions/create/page.tsx
+++ front_end/src/app/(main)/questions/create/page.tsx
@@ -1,12 +1,14 @@
+import { getTranslations } from "next-intl/server";
 import React from "react";
 
 import { SearchParams } from "@/types/navigation";
 
 import QuestionTypePicker from "../components/question_type_picker";
 
-const Creator: React.FC<{ searchParams: SearchParams }> = ({
+const Creator: React.FC<{ searchParams: SearchParams }> = async ({
   searchParams,
 }) => {
+  const t = await getTranslations();
   const createHref = (
     path: string,
     extra_params: { [key: string]: string } = {}
@@ -21,92 +23,91 @@ const Creator: React.FC<{ searchParams: SearchParams }> = ({
   };
 
   return (
-    <div className="mb-4 mt-2 flex max-w-4xl flex-col justify-center self-center rounded-none bg-gray-0 px-4 pb-5 pt-4 text-gray-700 dark:bg-gray-0-dark dark:text-gray-700-dark md:m-8 md:mx-auto md:rounded-md md:px-8 md:pb-8 lg:m-12 lg:mx-auto">
+    <div className="mb-4 mt-2 flex max-w-4xl flex-col justify-center self-center rounded-none bg-gray-0 px-4 pb-5 pt-4 text-gray-800 dark:bg-gray-0-dark dark:text-gray-800-dark md:m-8 md:mx-auto md:rounded-md md:px-8 md:pb-8 lg:m-12 lg:mx-auto">
       <div className="text-sm md:text-base">
-        <h1 className="text-2xl font-medium md:text-3xl">Create New Content</h1>
+        <h1 className="text-2xl font-medium capitalize md:text-3xl">
+          {t("createNewContent")}
+        </h1>
         <p>
-          Check out our{" "}
-          <a
-            href="/question-writing"
-            className="text-blue-700 hover:text-blue-800 dark:text-blue-700-dark dark:hover:text-blue-800-dark"
-          >
-            question writing guide
-          </a>{" "}
-          for tips. Good questions are approved faster, and get more
-          predictions.
+          {t.rich("createQuestionDescription1", {
+            link1: (chunks) => (
+              <a
+                href="/question-writing"
+                className="text-blue-800 hover:text-blue-900 dark:text-blue-800-dark dark:hover:text-blue-900-dark"
+              >
+                {chunks}
+              </a>
+            ),
+            link2: (chunks) => (
+              <a
+                href="/questions/956/suggest-questions-to-launch/"
+                className="text-blue-800 hover:text-blue-900 dark:text-blue-800-dark dark:hover:text-blue-900-dark"
+              >
+                {chunks}
+              </a>
+            ),
+          })}
         </p>
-        <p>
-          We have high standards for question quality. We also favor questions
-          on our core topic areas or that we otherwise judge valuable. We may
-          not publish questions that are not a good fit.
-        </p>
+        <p>{t("createQuestionDescription2")}</p>
       </div>
-      <h2 className="mt-0 text-lg font-light">Single Question</h2>
-      <div className="flex w-full flex-wrap gap-3 md:flex-row md:flex-nowrap md:gap-4">
-        <div className="flex w-full flex-col gap-3 md:gap-4">
-          <QuestionTypePicker
-            url={createHref("/questions/create/question", { type: "binary" })}
-            questionType="Binary Question"
-            questionExample="Will it rain today?"
-          />
-          <QuestionTypePicker
-            url={createHref("/questions/create/question", { type: "numeric" })}
-            questionType="Numeric Range"
-            questionExample="How much rain next month?"
-          />
-        </div>
-        <div className="flex w-full flex-col gap-3 md:gap-4">
-          <QuestionTypePicker
-            url={createHref("/questions/create/question", { type: "date" })}
-            questionType="Date Range"
-            questionExample="When will it rain?"
-          />
-          <QuestionTypePicker
-            url={createHref("/questions/create/question", {
-              type: "multiple_choice",
-            })}
-            questionType="Multiple Choice"
-            questionExample="Which city will get the most rain?"
-          />
-        </div>
-      </div>
-
-      <h2 className="text-lg font-light">Question Group</h2>
-      <div className="flex w-full flex-wrap gap-3 md:flex-row md:flex-nowrap md:gap-4">
-        <div className="flex w-full flex-col gap-3 md:gap-4">
-          <QuestionTypePicker
-            url={createHref("/questions/create/group", { subtype: "binary" })}
-            questionType="Binary Group"
-            questionExample="Will it rain in these cities today?"
-          />
-          <QuestionTypePicker
-            url={createHref("/questions/create/group", { subtype: "numeric" })}
-            questionType="Numeric Group"
-            questionExample="How much will it rain for these cities?"
-          />
-        </div>
-        <div className="flex w-full flex-col gap-3 md:gap-4">
-          <QuestionTypePicker
-            url={createHref("/questions/create/group", { subtype: "date" })}
-            questionType="Date Group"
-            questionExample="When will it rain for the following cities?"
-          />
-          <QuestionTypePicker
-            url={createHref("/questions/create/conditional")}
-            questionType="Conditional Pair"
-            questionExample="If it rains today, will it rain tomorrow?"
-          />
-        </div>
-      </div>
-
-      <h2 className="text-lg font-light">Posts</h2>
-      <div className="flex w-full flex-wrap md:flex-row md:flex-nowrap">
+      <h2 className="mt-0 text-lg font-light capitalize">
+        {t("singleQuestion")}
+      </h2>
+      <div className="grid w-full grid-cols-1 gap-3 md:grid-cols-2 md:gap-4">
         <QuestionTypePicker
-          url={createHref("/questions/create/notebook")}
-          questionType="Notebook"
-          questionExample="text-based content that is not a question"
+          url={createHref("/questions/create/question", { type: "binary" })}
+          questionType={t("binaryQuestion")}
+          questionExample={t("binaryQuestionExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/question", { type: "numeric" })}
+          questionType={t("numericRange")}
+          questionExample={t("numericRangeExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/question", { type: "date" })}
+          questionType={t("dateRange")}
+          questionExample={t("dateRangeExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/question", {
+            type: "multiple_choice",
+          })}
+          questionType={t("multipleChoice")}
+          questionExample={t("multipleChoiceExample")}
         />
       </div>
+
+      <h2 className="text-lg font-light capitalize">{t("questionGroup")}</h2>
+      <div className="grid w-full grid-cols-1 gap-3 md:grid-cols-2 md:gap-4">
+        <QuestionTypePicker
+          url={createHref("/questions/create/group", { subtype: "binary" })}
+          questionType={t("binaryGroup")}
+          questionExample={t("binaryGroupExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/group", { subtype: "numeric" })}
+          questionType={t("numericGroup")}
+          questionExample={t("numericGroupExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/group", { subtype: "date" })}
+          questionType={t("dateGroup")}
+          questionExample={t("dateGroupExample")}
+        />
+        <QuestionTypePicker
+          url={createHref("/questions/create/conditional")}
+          questionType={t("conditionalPair")}
+          questionExample={t("conditionalPairExample")}
+        />
+      </div>
+
+      <h2 className="text-lg font-light capitalize">{t("posts")}</h2>
+      <QuestionTypePicker
+        url={createHref("/questions/create/notebook")}
+        questionType={t("notebook")}
+        questionExample={t("notebookExample")}
+      />
     </div>
   );
 };
