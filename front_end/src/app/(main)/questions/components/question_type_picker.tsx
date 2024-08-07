157ab72ee843bc8a912525e45707b3366a9c93ef
ludant <rudolfordoyne@protonmail.com>
2024-08-07 11:15:54 -0500

    standardize styles

--- front_end/src/app/(main)/questions/components/question_type_picker.tsx
+++ front_end/src/app/(main)/questions/components/question_type_picker.tsx
@@ -15,14 +15,15 @@ const QuestionTypePicker: React.FC<QuestionTypePickerProps> = ({
   questionExample,
 }) => {
   return (
-    <Link href={url} className="no-underline">
-      <div className="flex h-full w-full flex-col gap-1 rounded-s border border-blue-400 p-3 hover:bg-blue-300 dark:border-blue-700 hover:dark:bg-blue-700 md:p-4">
-        <div className="text-lg font-medium text-blue-700 dark:text-blue-300 md:text-xl">
-          {questionType}
-        </div>
-        <div className="text-sm font-light text-gray-600 dark:text-gray-400 md:text-lg">
-          {`"${questionExample}"`}
-        </div>
+    <Link
+      href={url}
+      className="flex h-full w-full flex-col gap-1 rounded-s border border-blue-400 p-3 no-underline hover:bg-blue-300 dark:border-blue-400-dark hover:dark:bg-blue-300-dark md:p-4"
+    >
+      <div className="text-lg font-medium capitalize text-blue-700 dark:text-blue-700-dark md:text-xl">
+        {questionType}
+      </div>
+      <div className="text-sm font-light text-gray-600 dark:text-gray-600-dark md:text-lg">
+        {`"${questionExample}"`}
       </div>
     </Link>
   );
