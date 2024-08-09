3c4753b7dee8a4f61b7af7bb9b1d48f412b6308e
Vasile Popescu <elisescu@elisescu.com>
2024-08-09 12:36:28 +0200

    FAB API: add the prediction old question API compatibility

--- metaculus_web/urls.py
+++ metaculus_web/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import include, path
 import comments
 import posts
+import questions
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,6 +33,7 @@ urlpatterns = [
     path("api/", include("scoring.urls")),
     path("api2/", include(comments.urls.old_api)),
     path("api2/", include(posts.urls.old_api)),
+    path("api2/", include(questions.urls.old_api)),
 ]
 
 if settings.DEBUG:
