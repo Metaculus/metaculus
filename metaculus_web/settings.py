c179efb139b02a279ef93f61fdf2e64326cc8ebe
gleb <alexxkur@gmail.com>
2024-08-12 16:27:15 +0100

    Hotfix

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -227,6 +227,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
