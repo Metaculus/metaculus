779ce9a3a94e236e66fb9bd85a6ef28d65368997
gleb <alexxkur@gmail.com>
2024-08-12 20:00:35 +0100

    Fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ npm run build;
 cd ..;
 
 poetry run python3 manage.py migrate;
-poetry run python3 manage.py collectstatic;
+poetry run python3 manage.py collectstatic --noinput;
 
 export ALPHA_ACCESS_TOKEN="the open source rewrite";
 
