f810193d6cfce15d854e5867f3589b6c5bebdc6c
gleb <alexxkur@gmail.com>
2024-08-15 18:11:17 +0100

    Small fix

--- misc/views.py
+++ misc/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import PermissionDenied
@@ -9,10 +8,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from notifications.models import Notification
-from notifications.services import (
-    NotificationPostMilestone, NotificationPostCPChange,
-)
 from .models import ITNArticle
 from .serializers import ContactSerializer
 from .services.itn import remove_article
@@ -49,38 +44,3 @@ def remove_article_api_view(request, pk):
     remove_article(article)
 
     return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(["GET"])
-@permission_classes([AllowAny])
-def post_notifications_check(request):
-    from users.models import User
-
-    user = User.objects.get(pk=104161)
-
-    context = NotificationPostCPChange.get_email_context_group(
-        [
-            Notification(
-                recipient=user,
-                params={
-                    "post": {
-                        "post_id": 349,
-                        "post_title": "Will SpaceX land people on Mars before 2030?",
-                    },
-                    "question_data": [
-                        {
-                            "question": {
-                                "id": 10630,
-                                "title": "Who will be elected US President in 2024? (Joe Biden)",
-                                "type": "binary"
-                            },
-                        }
-                    ],
-                },
-            ),
-        ]
-    )
-
-    template_name = "emails/post_status_change.html"
-
-    return render(request, template_name, context_post_status_change)
