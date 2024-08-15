34e5736ff6ccc295a501ada27ca428f291ec2222
gleb <alexxkur@gmail.com>
2024-08-15 18:11:17 +0100

    Small fix

--- misc/urls.py
+++ misc/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("contact-form", views.contact_api_view),
-    path("email", views.post_notifications_check),
     path(
         "itn-articles/<int:pk>/remove",
         views.remove_article_api_view,
