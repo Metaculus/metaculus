388a7664b2e5bef0738853f7b574c06aa0e4c2c6
George3d6 <george@cerebralab.com>
2024-08-21 22:27:14 +0200

    added example env

---
+++ .env.example.sh
@@ -0,0 +1,27 @@
+# General
+export SECRET_KEY="your developer secret keu"
+export FRONTEND_BASE_URL="https://yourdomain.com"
+export NEXT_PUBLIC_APP_URL="https://yourdomain.com"
+export MAILGUN_API_KEY="your mailgun API key"
+
+# Social Auth
+export SOCIAL_AUTH_FACEBOOK_KEY=
+export SOCIAL_AUTH_FACEBOOK_SECRET=
+export SOCIAL_AUTH_GOOGLE_OAUTH2_KEY=
+export SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=
+
+# S3 configuration
+export AWS_STORAGE_BUCKET_NAME=
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_S3_REGION_NAME=
+
+# Captcha
+export TURNSTILE_SECRET_KEY=
+export NEXT_PUBLIC_TURNSTILE_SITE_KEY=
+
+# OpenAI
+# Required for search, newsmatch, question similarity
+export OPENAI_API_KEY=
+# Required for search
+export SERPER_API_KEY=
