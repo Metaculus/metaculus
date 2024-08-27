a8bd7cbbb0e80a457a34fa98a7a454943f6df523
casens5 <rudolfordoyne@protonmail.com>
2024-08-27 17:25:13 -0500

    add translations

--- front_end/src/app/(main)/questions/[id]/components/forecast_maker/numeric_table.tsx
+++ front_end/src/app/(main)/questions/[id]/components/forecast_maker/numeric_table.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from "react";
 
-import { Bounds, Quartiles, Question } from "@/types/question";
+import {
+  Bounds,
+  Quartiles,
+  QuestionWithNumericForecasts,
+} from "@/types/question";
 import { getDisplayValue } from "@/utils/charts";
+import { useTranslations } from "next-intl";
 
 type Props = {
-  question: Question;
+  question: QuestionWithNumericForecasts;
   userBounds?: Bounds;
   userQuartiles?: Quartiles;
   communityBounds?: Bounds;
@@ -24,6 +29,8 @@ const NumericForecastTable: FC<Props> = ({
   hasUserForecast,
   isDirty,
 }) => {
+  const t = useTranslations();
+
   return (
     <>
       <div className="mb-4 flex justify-between text-center">
@@ -31,54 +38,71 @@ const NumericForecastTable: FC<Props> = ({
         {withUserQuartiles && (
           <>
             <div className="w-full text-orange-800 dark:text-orange-800-dark">
-              My Prediction
+              {t("myPrediction")}
             </div>
           </>
         )}
         <a className="w-full text-olive-700 dark:text-olive-700-dark">
-          Community
+          {t("community")}
         </a>
       </div>
       <div className="mb-4 flex justify-between">
         <div className="w-full text-center">
-          <div className="w-full">mass below lower bound</div>
-          <div className="w-full">lower 25%</div>
-          <div className="w-full">median</div>
-          <div className="w-full">upper 75%</div>
-          <div className="w-full">mass above upper bound</div>
+          {question.open_lower_bound && (
+            <div className="w-full">{"<"}{question.scaling.range_min}</div>
+          )}
+          <div className="w-full">{t("firstQuartile")}</div>
+          <div className="w-full">{t("secondQuartile")}</div>
+          <div className="w-full">{t("thirdQuartile")}</div>
+          {question.open_upper_bound && (
+            <div className="w-full">{">"}{question.scaling.range_max}</div>
+          )}
         </div>
         {withUserQuartiles && (
           <div className="w-full text-center">
             {isDirty || hasUserForecast ? (
               <>
-                <div>
-                  {userBounds && (userBounds.belowLower * 100).toFixed(1)}%
-                </div>
+                {question.open_lower_bound && (
+                  <div>
+                    {userBounds && (userBounds.belowLower * 100).toFixed(1)}%
+                  </div>
+                )}
                 <div>{getDisplayValue(userQuartiles?.lower25, question)}</div>
                 <div>{getDisplayValue(userQuartiles?.median, question)}</div>
                 <div>{getDisplayValue(userQuartiles?.upper75, question)}</div>
-                <div>{(userBounds!.aboveUpper * 100).toFixed(1)}%</div>
+                {question.open_upper_bound && (
+                  <div>{(userBounds!.aboveUpper * 100).toFixed(1)}%</div>
+                )}
               </>
             ) : (
               <>
-                {[...Array(5)].map((_, i) => {
+                {question.open_lower_bound && <div>...</div>}
+                {[...Array(3)].map((_, i) => {
                   return <div key={i}>...</div>;
                 })}
+                {question.open_lower_bound && <div>...</div>}
               </>
             )}
           </div>
         )}
 
         <div className="w-full text-center">
-          <div>
-            {communityBounds && (communityBounds.belowLower * 100).toFixed(1)}%
-          </div>
+          {question.open_lower_bound && (
+            <div>
+              {communityBounds && (communityBounds.belowLower * 100).toFixed(1)}
+              %
+            </div>
+          )}
           <div>{getDisplayValue(communityQuartiles?.lower25, question)}</div>
           <div>{getDisplayValue(communityQuartiles?.median, question)}</div>
           <div>{getDisplayValue(communityQuartiles?.upper75, question)}</div>
-          <div>
-            {communityBounds && (communityBounds!.aboveUpper * 100).toFixed(1)}%
-          </div>
+          {question.open_upper_bound && (
+            <div>
+              {communityBounds &&
+                (communityBounds!.aboveUpper * 100).toFixed(1)}
+              %
+            </div>
+          )}
         </div>
       </div>
     </>
