21d5b14f63f83bea511123ad0459da37e28dbd51
Vasile Popescu <elisescu@elisescu.com>
2024-08-28 23:41:10 +0200

    Optimize the Dockerfile and various other improvements

--- scripts/prod/startapp.sh
+++ scripts/prod/startapp.sh
@@ -1,11 +1,21 @@
-#! /bin/bash
+#!/bin/bash
+set -e
+set -o pipefail
+
+cleanup() {
+    echo "Stopping the processes"
+    for process in "gunicorn"; do
+        PID=$(ps -eo pid,comm,args | awk -v process=$process '$0 ~ process && $0 !~ /awk/ {print $1}')
+        [ -n "$PID" ] && kill -s SIGTERM $PID
+    done
+}
+
+trap cleanup EXIT
+trap "exit" INT TERM ERR
 
 cd /app/
 source venv/bin/activate
 
-# PORT is passed by Heroku, and should be the one where nextjs lists on
-gunicorn metaculus_web.wsgi:application --workers 8 --bind 0.0.0.0:8000 &
-
-
-cd front_end
-NEXT_PUBLIC_APP_URL=http://localhost:$PORT && npm run start
+export NEXT_PUBLIC_APP_URL="http://localhost:$PORT"
+(gunicorn metaculus_web.wsgi:application --bind 0.0.0.0:8000 2>&1 | sed 's/^/[Backend]: /') &
+(cd front_end && npm run start 2>&1 | sed 's/^/[Frontend]: /')
