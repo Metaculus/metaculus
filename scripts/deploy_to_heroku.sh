e35d655b1a08da563b68aeccbdc4cafaf285b8ba
Vasile Popescu <elisescu@elisescu.com>
2024-08-28 12:33:37 +0200

    Add a Github action to deploy to dev

--- scripts/deploy_to_heroku.sh
+++ scripts/deploy_to_heroku.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
+set -x
+
+[ -z "$HEROKU_APP" ] && { echo "Error: HEROKU_APP env varible is not set."; exit 1; }
 
 # Push container
-heroku container:push release  --arg ENTRY_SCRIPT_ARG="scripts/prod/release.sh"
-heroku container:push web  --arg ENTRY_SCRIPT_ARG="scripts/prod/startapp.sh"
-heroku container:push dramatiq_worker  --arg ENTRY_SCRIPT_ARG="scripts/prod/run_dramatiq.sh"
-heroku container:push django_cron  --arg ENTRY_SCRIPT_ARG="scripts/prod/django_cron.sh"
+heroku container:push release --arg ENTRY_SCRIPT_ARG="scripts/prod/release.sh" -a $HEROKU_APP
+heroku container:push web --arg ENTRY_SCRIPT_ARG="scripts/prod/startapp.sh" -a $HEROKU_APP
+heroku container:push dramatiq_worker --arg ENTRY_SCRIPT_ARG="scripts/prod/run_dramatiq.sh" -a $HEROKU_APP
+heroku container:push django_cron --arg ENTRY_SCRIPT_ARG="scripts/prod/django_cron.sh" -a $HEROKU_APP
 
 # Release them
-heroku container:release release
-heroku container:release web
-heroku container:release dramatiq_worker
-heroku container:release django_cron
+heroku container:release release -a $HEROKU_APP
+heroku container:release web -a $HEROKU_APP
+heroku container:release dramatiq_worker -a $HEROKU_APP
+heroku container:release django_cron -a $HEROKU_APP
