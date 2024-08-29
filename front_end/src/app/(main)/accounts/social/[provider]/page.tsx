8458e3d153bdf8ad1d58c75c23ddad54b00f9cce
gleb <alexxkur@gmail.com>
2024-08-29 18:03:00 +0200

    Small fixes

--- front_end/src/app/(main)/accounts/social/[provider]/page.tsx
+++ front_end/src/app/(main)/accounts/social/[provider]/page.tsx
@@ -1,20 +1,37 @@
-import ExchangeSocialAuthCode from "@/app/(main)/accounts/social/[provider]/components/exchange_social_auth_code";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+import { exchangeSocialOauthCode } from "@/app/(main)/accounts/social/[provider]/actions";
+import GlobalErrorBoundary from "@/components/global_error_boundary";
+import LoadingIndicator from "@/components/ui/loading_indicator";
 import { SocialProviderType } from "@/types/auth";
 import { SearchParams } from "@/types/navigation";
 
-export default async function SocialAuth({
+export default function SocialAuth({
   params: { provider },
   searchParams,
 }: {
   params: { provider: SocialProviderType };
   searchParams: SearchParams;
 }) {
+  const [error, setError] = useState();
+  const router = useRouter();
+
+  useEffect(() => {
+    exchangeSocialOauthCode(provider, searchParams.code as string)
+      .then(() => router.push("/"))
+      .catch(setError);
+  }, [provider, searchParams.code, router]);
+
+  if (error) {
+    return <GlobalErrorBoundary error={error} reset={() => router.push("/")} />;
+  }
+
   return (
     <main className="mx-auto my-12 flex min-h-min w-full max-w-5xl flex-col gap-4 px-3 lg:px-0">
-      <ExchangeSocialAuthCode
-        code={searchParams.code as string}
-        provider={provider}
-      />
+      <LoadingIndicator className="mx-auto h-8 w-24 text-gray-600 dark:text-gray-600-dark" />
     </main>
   );
 }
