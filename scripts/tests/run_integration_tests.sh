238fb8e76c44350016ad276c8999668963933e57
Vasile Popescu <elisescu@elisescu.com>
2024-08-30 09:52:09 +0200

    Run a ./manage.py collectstatic in the integration tests

--- scripts/tests/run_integration_tests.sh
+++ scripts/tests/run_integration_tests.sh
@@ -17,6 +17,7 @@ export DATABASE_URL=postgres://postgres:postgres@localhost:5432/test_metaculus
 
 # Start the frontend and backend processes in the background,
 # and use sed to add a prefix to each of their stdout and stderr printed lines
+poetry run  ./manage.py collectstatic --noinput
 (poetry run gunicorn metaculus_web.wsgi:application --log-level=debug --bind 0.0.0.0:8000 2>&1 | sed 's/^/[Backend]: /') &
 (poetry run ./manage.py rundramatiq --processes 1 --threads 1 2>&1 | sed 's/^/[Dramatiq]: /') &
 (npm run --prefix front_end start 2>&1 | sed 's/^/[Frontend]: /') &
