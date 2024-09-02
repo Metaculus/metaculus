34ce6f51454b3cb8d50e11dbf2f4e28659bed094
Vasile Popescu <elisescu@elisescu.com>
2024-09-02 17:41:18 +0200

    Remove the STATICFILES_DIRS from our settings
    We don't have any custom static assets that should be collected, so no
    need for this setting

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -244,7 +244,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
