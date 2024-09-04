a2b737d5a8b82cb00650f1620f90937b330513c8
Vasile Popescu <elisescu@elisescu.com>
2024-09-04 09:11:21 +0200

    Proxy /api2/* and /api/* requests to the backend

--- front_end/next.config.mjs
+++ front_end/next.config.mjs
@@ -8,6 +8,7 @@ const API_BASE_URL =
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   env: {
     API_BASE_URL,
     APP_URL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
@@ -50,6 +51,14 @@ const nextConfig = {
     {
       source: "/admin/:path*",
       destination: `${API_BASE_URL}/admin/:path*/`,
+    },
+    {
+      source: "/api2/:path*/",
+      destination: `${API_BASE_URL}/api2/:path*/`,
+    },
+    {
+      source: "/api/:path*/",
+      destination: `${API_BASE_URL}/api/:path*/`,
     },
     // Assets
     {
