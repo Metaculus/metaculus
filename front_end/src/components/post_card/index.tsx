8b9a9a8cc042e257ad3f714ac87efdc365ee4e50
casens5 <rudolfordoyne@protonmail.com>
2024-09-12 10:32:30 -0500

    simplify post cards and spacing

--- front_end/src/components/post_card/index.tsx
+++ front_end/src/components/post_card/index.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
 
-import ConditionalTile from "@/components/conditional_tile";
-import NotebookTile from "@/components/post_card/notebook_tile";
-import { PostWithForecasts } from "@/types/post";
-
 import BasicPostCard from "./basic_post_card";
 import PostCardErrorBoundary from "./error_boundary";
 import GroupOfQuestionsTile from "./group_of_questions_tile";
 import QuestionChartTile from "./question_chart_tile";
+
+import ConditionalTile from "@/components/conditional_tile";
+import NotebookTile from "@/components/post_card/notebook_tile";
+import { PostWithForecasts } from "@/types/post";
 
 type Props = {
   post: PostWithForecasts;
@@ -22,29 +22,27 @@ const PostCard: FC<Props> = ({ post }) => {
         borderVariant={post.notebook ? "highlighted" : "regular"}
         borderColor={post.notebook ? "purple" : "blue"}
       >
-        <div className="mb-0.5 pt-1.5">
-          {!!post?.question && (
-            <QuestionChartTile
-              question={post?.question}
-              authorUsername={post.author_username}
-              curationStatus={post.status}
-            />
-          )}
-          {!!post.group_of_questions && (
-            <GroupOfQuestionsTile
-              questions={post.group_of_questions.questions}
-              curationStatus={post.status}
-            />
-          )}
-          {!!post.conditional && (
-            <ConditionalTile
-              postTitle={post.title}
-              conditional={post.conditional}
-              curationStatus={post.status}
-            />
-          )}
-          {!!post.notebook && <NotebookTile notebook={post.notebook} />}
-        </div>
+        {!!post?.question && (
+          <QuestionChartTile
+            question={post?.question}
+            authorUsername={post.author_username}
+            curationStatus={post.status}
+          />
+        )}
+        {!!post.group_of_questions && (
+          <GroupOfQuestionsTile
+            questions={post.group_of_questions.questions}
+            curationStatus={post.status}
+          />
+        )}
+        {!!post.conditional && (
+          <ConditionalTile
+            postTitle={post.title}
+            conditional={post.conditional}
+            curationStatus={post.status}
+          />
+        )}
+        {!!post.notebook && <NotebookTile notebook={post.notebook} />}
       </BasicPostCard>
     </PostCardErrorBoundary>
   );
