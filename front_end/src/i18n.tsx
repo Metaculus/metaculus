17f851bbff2a1b5da72f421deccdf46d0b2f6e60
casens5 <rudolfordoyne@protonmail.com>
2024-09-25 11:39:45 -0500

    fix more

--- front_end/src/i18n.tsx
+++ front_end/src/i18n.tsx
@@ -1,12 +1,12 @@
 import { cookies } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
-import { getBrowserLocale } from "./utils/translation";
+//import { getBrowserLocale } from "./utils/translation";
 
 export default getRequestConfig(async () => {
   const locale = cookies().get("NEXT_LOCALE")
     ? cookies().get("NEXT_LOCALE")?.value
-    : (await getBrowserLocale()) || "en";
+    : "en";
   let messages;
 
   try {
