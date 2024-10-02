39d10232ab1818f0eb52e0929b9236c69dbe95cc
Nikita <93587872+ncarazon@users.noreply.github.com>
2024-10-02 14:49:06 +0300

    feat: cookies banner

--- front_end/src/app/providers.tsx
+++ front_end/src/app/providers.tsx
@@ -2,12 +2,18 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
+import { getAnalyticsCookieConsentGiven } from "@/app/(main)/components/cookies_banner";
+
 export function CSPostHogProvider({ children }: { children: any }) {
-  if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       // set to 'always' to create profiles for anonymous users as well
       person_profiles: "identified_only",
+      persistence:
+        getAnalyticsCookieConsentGiven() === "yes"
+          ? "localStorage+cookie"
+          : "memory",
     });
   }
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
