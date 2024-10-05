74116259421e1b494e3c86b6634a1f820ebbe7a6
Hlib <hlib@metaculus.com>
2024-10-05 11:51:36 +0100

    Added concurrency limiter to forecasting async jobs (#786)
    * Added concurrency limiter to forecasting async jobs
    
    * Added docstring

---
+++ utils/dramatiq.py
@@ -0,0 +1,45 @@
+import functools
+from typing import Callable
+
+from django.conf import settings
+from dramatiq.rate_limits import ConcurrentRateLimiter
+from dramatiq.rate_limits.backends import RedisBackend
+
+
+def get_redis_backend():
+    return RedisBackend(**settings.DRAMATIQ_RATE_LIMITER_BACKEND_OPTIONS)
+
+
+def concurrency_retries(max_retries=20):
+    """
+    We want our calculation task be accomplished consequently no matter
+    how much time it might take.
+    """
+
+    def f(retries_so_far, exception):
+        return isinstance(exception, Exception) or retries_so_far < max_retries
+
+    return f
+
+
+def task_concurrent_limit(key: Callable | str, **limiter_kwargs):
+    """
+    This decorator applies ConcurrentRateLimiter to dramatiq actor
+    End ensures max concurrent executions of the same actor
+    """
+
+    def f(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            mutex_key = key if isinstance(key, str) else key(*args, **kwargs)
+            mutex = ConcurrentRateLimiter(
+                get_redis_backend(), mutex_key, **limiter_kwargs
+            )
+
+            with mutex.acquire() as acquired:
+                if acquired:
+                    return func(*args, **kwargs)
+
+        return wrapper
+
+    return f
