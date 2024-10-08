0efd7007abd0a4514b5b37b110bc3ef907405efd
Nikita <nikita.develops.work@gmail.com>
2024-10-08 17:17:33 +0300

    fix: disable sentry replay integration

--- front_end/sentry.client.config.ts
+++ front_end/sentry.client.config.ts
@@ -3,8 +3,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
   tracesSampleRate: 1,
-  integrations: [Sentry.replayIntegration()],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
+  // integrations: [Sentry.replayIntegration()],
+  // replaysSessionSampleRate: 0.1,
+  // replaysOnErrorSampleRate: 1.0,
   ignoreErrors: ["NEXT_REDIRECT", "NEXT_NOT_FOUND"],
 });
