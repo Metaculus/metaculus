624b4a65f4862da4a6395883b2b5e4896c60c091
Luke Sabor <32885230+lsabor@users.noreply.github.com>
2024-10-10 14:55:00 -0700

    Fix/674 continuous conditionals timeline (#950)
    switch from fan graph to group timeline for continuous conditionals

--- front_end/src/components/conditional_timeline.tsx
+++ front_end/src/components/conditional_timeline.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 
 import BinaryGroupChart from "@/app/(main)/questions/[id]/components/detailed_group_card/binary_group_chart";
-import NumericGroupChart from "@/app/(main)/questions/[id]/components/detailed_group_card/numeric_group_chart";
 import { PostConditional } from "@/types/post";
 import { QuestionType, QuestionWithNumericForecasts } from "@/types/question";
 import { getGroupQuestionsTimestamps } from "@/utils/charts";
+import ContinuousGroupTimeline from "@/app/(main)/questions/[id]/components/continuous_group_timeline";
 
 type Props = {
   conditional: PostConditional<QuestionWithNumericForecasts>;
@@ -14,10 +14,10 @@ type Props = {
 const ConditionalTimeline: FC<Props> = ({ conditional, isClosed }) => {
   const groupType = conditional.question_no.type;
   const questions = [conditional.question_yes, conditional.question_no];
+  const timestamps = getGroupQuestionsTimestamps(questions);
 
   switch (groupType) {
     case QuestionType.Binary: {
-      const timestamps = getGroupQuestionsTimestamps(questions);
       return (
         <BinaryGroupChart
           questions={questions}
@@ -28,7 +28,20 @@ const ConditionalTimeline: FC<Props> = ({ conditional, isClosed }) => {
     }
     case QuestionType.Numeric:
     case QuestionType.Date:
-      return <NumericGroupChart questions={questions} />;
+      return (
+        <ContinuousGroupTimeline
+          actualCloseTime={
+            conditional.condition_child.actual_close_time
+              ? new Date(
+                  conditional.condition_child.actual_close_time
+                ).getTime()
+              : null
+          }
+          questions={questions}
+          timestamps={timestamps}
+          isClosed={isClosed}
+        />
+      );
   }
 };
 
