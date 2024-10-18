790b135c832d96b437340c634097db17932d0d65
lsabor <lukesabor@gmail.com>
2024-10-18 14:55:07 -0700

    flake8 for migration

--- scoring/migrations/0004_alter_leaderboard_end_time_and_more.py
+++ scoring/migrations/0004_alter_leaderboard_end_time_and_more.py
@@ -6,33 +6,56 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scoring', '0003_alter_leaderboard_name_alter_leaderboard_project_and_more'),
+        ("scoring", "0003_alter_leaderboard_name_alter_leaderboard_project_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='leaderboard',
-            name='end_time',
+            model_name="leaderboard",
+            name="end_time",
             field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='leaderboard',
-            name='finalize_time',
+            model_name="leaderboard",
+            name="finalize_time",
             field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='leaderboard',
-            name='score_type',
-            field=models.CharField(choices=[('relative_legacy_tournament', 'Relative Legacy Tournament'), ('peer_global', 'Peer Global'), ('peer_global_legacy', 'Peer Global Legacy'), ('peer_tournament', 'Peer Tournament'), ('spot_peer_tournament', 'Spot Peer Tournament'), ('baseline_global', 'Baseline Global'), ('comment_insight', 'Comment Insight'), ('question_writing', 'Question Writing'), ('manual', 'Manual')], max_length=200),
+            model_name="leaderboard",
+            name="score_type",
+            field=models.CharField(
+                choices=[
+                    ("relative_legacy_tournament", "Relative Legacy Tournament"),
+                    ("peer_global", "Peer Global"),
+                    ("peer_global_legacy", "Peer Global Legacy"),
+                    ("peer_tournament", "Peer Tournament"),
+                    ("spot_peer_tournament", "Spot Peer Tournament"),
+                    ("baseline_global", "Baseline Global"),
+                    ("comment_insight", "Comment Insight"),
+                    ("question_writing", "Question Writing"),
+                    ("manual", "Manual"),
+                ],
+                max_length=200,
+            ),
         ),
         migrations.AlterField(
-            model_name='leaderboard',
-            name='start_time',
+            model_name="leaderboard",
+            name="start_time",
             field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='score',
-            name='score_type',
-            field=models.CharField(choices=[('relative_legacy', 'Relative Legacy'), ('peer', 'Peer'), ('baseline', 'Baseline'), ('spot_peer', 'Spot Peer'), ('spot_baseline', 'Spot Baseline'), ('manual', 'Manual')], max_length=200),
+            model_name="score",
+            name="score_type",
+            field=models.CharField(
+                choices=[
+                    ("relative_legacy", "Relative Legacy"),
+                    ("peer", "Peer"),
+                    ("baseline", "Baseline"),
+                    ("spot_peer", "Spot Peer"),
+                    ("spot_baseline", "Spot Baseline"),
+                    ("manual", "Manual"),
+                ],
+                max_length=200,
+            ),
         ),
     ]
