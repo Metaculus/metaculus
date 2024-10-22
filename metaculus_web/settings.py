d838fe605f15ae0f008b1c141fba630584ae7cf6
Hlib <hlib@metaculus.com>
2024-10-22 15:23:28 +0200

    Improved admin Array field for multiple-choice questions (#1096)

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "anymail",
     "django_dramatiq",
     "admin_auto_filters",
+    "django_better_admin_arrayfield",
     # TODO: disable in prod
     # first-party:
     "migrator",
