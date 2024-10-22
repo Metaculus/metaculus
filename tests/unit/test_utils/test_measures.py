4dadd3a8f5b2500e6bffc0ffe2705333e95d268a
lsabor <lukesabor@gmail.com>
2024-10-22 11:00:45 -0700

    add unit tests for measures.py

--- tests/unit/test_utils/test_measures.py
+++ tests/unit/test_utils/test_measures.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pytest
+from unittest.mock import Mock
 
-from utils.the_math.measures import weighted_percentile_2d
+from questions.models import Question
+from utils.the_math.measures import (
+    weighted_percentile_2d,
+    percent_point_function,
+    prediction_difference_for_sorting,
+    prediction_difference_for_display,
+    decimal_h_index,
+)
 
 
 @pytest.mark.parametrize(
@@ -67,3 +75,293 @@ def test_weighted_percentile_2d(values, weights, percentiles, expected_result):
     if weights is None and [percentiles] == [50.0]:  # should behave like np.median
         numpy_medians = np.median(values, axis=0)
         np.testing.assert_allclose(result, [numpy_medians])
+
+
+@pytest.mark.parametrize(
+    "cdf, percentiles, expected_result",
+    [
+        ([0.1, 0.5, 0.9], 50, [0.5]),
+        ([0.1, 0.5, 0.9], [10, 50, 90], [0.0, 0.5, 1.0]),
+        (np.linspace(0, 0.5, 101), 40, [0.8]),
+    ],
+)
+def test_percent_point_function(cdf, percentiles, expected_result):
+    result = percent_point_function(cdf, percentiles)
+    np.testing.assert_allclose(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "p1, p2, question, expected_result",
+    [
+        (
+            [0.5, 0.5],
+            [0.5, 0.5],
+            Question(type="binary"),
+            0.0,
+        ),
+        (
+            [0.5, 0.5],
+            [0.6, 0.4],
+            Question(type="binary"),
+            sum([-0.1 * np.log2(0.5 / 0.6), 0.1 * np.log2(0.5 / 0.4)]),  # 0.05849625
+        ),
+        (
+            [0.5, 0.5],
+            [0.5, 0.5],
+            Question(type="multiple_choice"),
+            0.0,
+        ),
+        (
+            [0.5, 0.5],
+            [0.6, 0.4],
+            Question(type="multiple_choice"),
+            0.05849625,
+        ),
+        (
+            [0.1, 0.2, 0.3, 0.4],
+            [0.1, 0.2, 0.3, 0.4],
+            Question(type="multiple_choice"),
+            0.0,
+        ),
+        (
+            [0.1, 0.2, 0.3, 0.4],
+            [0.4, 0.3, 0.2, 0.1],
+            Question(type="multiple_choice"),
+            sum(
+                [
+                    (0.1 - 0.4) * np.log2(0.1 / 0.4),
+                    (0.2 - 0.3) * np.log2(0.2 / 0.3),
+                    (0.3 - 0.2) * np.log2(0.3 / 0.2),
+                    (0.4 - 0.1) * np.log2(0.4 / 0.1),
+                ]
+            ),  # 1.3169925
+        ),
+        (
+            [0.01, 0.5, 0.99],
+            [0.01, 0.5, 0.99],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            0.0,
+        ),
+        (
+            [0.01, 0.02, 0.03],
+            [0.97, 0.98, 0.99],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            10.9793888,
+        ),
+        (
+            [0.01, 0.02, 0.03],
+            [0.97, 0.98, 0.99],
+            Question(
+                type="numeric",
+                range_min=99,
+                range_max=100,
+                zero_point=None,
+            ),
+            10.9793888,
+        ),
+        (
+            [0.01, 0.02, 0.03],
+            [0.97, 0.98, 0.99],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=100,
+                zero_point=None,
+            ),
+            10.9793888,
+        ),
+        (
+            [0.01, 0.02, 0.98, 0.99],
+            [0.48, 0.49, 0.51, 0.52],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            2.7616993,
+        ),
+        (
+            [0.0, 0.48, 0.49, 0.51, 0.52, 1.0],
+            [0.0, 0.01, 0.49, 0.51, 0.99, 1.0],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            1.2246093,
+        ),
+    ],
+)
+def test_prediction_difference_for_sorting(p1, p2, question, expected_result):
+    result = prediction_difference_for_sorting(p1, p2, question)
+    assert np.isclose(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "p1, p2, question, expected_result",
+    [
+        (
+            [0.5, 0.5],
+            [0.5, 0.5],
+            Question(type="binary"),
+            [
+                (0.0, 1 / 1),
+            ],
+        ),
+        (
+            [0.5, 0.5],
+            [0.6, 0.4],
+            Question(type="binary"),
+            [
+                (-0.1, (2 / 3) / 1),
+            ],
+        ),
+        (
+            [0.5, 0.5],
+            [0.5, 0.5],
+            Question(type="multiple_choice"),
+            [
+                (0.0, 1 / 1),
+                (0.0, 1 / 1),
+            ],
+        ),
+        (
+            [0.5, 0.5],
+            [0.6, 0.4],
+            Question(type="multiple_choice"),
+            [
+                (0.1, (3 / 2) / 1),
+                (-0.1, (2 / 3) / 1),
+            ],
+        ),
+        (
+            [0.1, 0.2, 0.3, 0.4],
+            [0.1, 0.2, 0.3, 0.4],
+            Question(type="multiple_choice"),
+            [
+                (0.0, (1 / 9) / (1 / 9)),
+                (0.0, (2 / 8) / (2 / 8)),
+                (0.0, (3 / 7) / (3 / 7)),
+                (0.0, (4 / 6) / (4 / 6)),
+            ],
+        ),
+        (
+            [0.1, 0.2, 0.3, 0.4],
+            [0.4, 0.3, 0.2, 0.1],
+            Question(type="multiple_choice"),
+            [
+                (0.3, (4 / 6) / (1 / 9)),
+                (0.1, (3 / 7) / (2 / 8)),
+                (-0.1, (2 / 8) / (3 / 7)),
+                (-0.3, (1 / 9) / (4 / 6)),
+            ],
+        ),
+        (
+            [0.0, 0.5, 1.0],
+            [0.0, 0.5, 1.0],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            [
+                (0.0, 0.0),
+            ],
+        ),
+        (
+            [0.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            [
+                (1.0, 1.0),
+            ],
+        ),
+        (
+            [0.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0],
+            Question(
+                type="numeric",
+                range_min=99,
+                range_max=100,
+                zero_point=None,
+            ),
+            [
+                (1.0, 1.0),
+            ],
+        ),
+        (
+            [0.0, 0.0, 0.0],
+            [1.0, 1.0, 1.0],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=100,
+                zero_point=None,
+            ),
+            [
+                (100.0, 100.0),
+            ],
+        ),
+        (
+            [0.0, 0.0, 1.0, 1.0],
+            [0.5, 0.5, 0.5, 0.5],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            [
+                (0.5, 0.0),
+            ],
+        ),
+        (
+            [0.0, 0.5, 0.5, 0.5, 0.5, 1.0],
+            [0.0, 0.0, 0.5, 0.5, 1.0, 1.0],
+            Question(
+                type="numeric",
+                range_min=0,
+                range_max=1,
+                zero_point=None,
+            ),
+            [
+                (0.2, 0.0),
+            ],
+        ),
+    ],
+)
+def test_prediction_difference_for_display(p1, p2, question, expected_result):
+    result = prediction_difference_for_display(p1, p2, question)
+    for res, exp in zip(result, expected_result):
+        assert np.allclose(res, exp, equal_nan=True)
+
+
+@pytest.mark.parametrize(
+    "scores, expected_result",
+    [
+        ([2, 2], 2.0 + round(0 / 4, 2)),
+        ([2, 2, 1], 2 + round(1 / 5, 2)),
+        ([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 5 + round(10 / 11, 2)),
+    ],
+)
+def test_decimal_h_index(scores, expected_result):
+    result = decimal_h_index(scores)
+    assert np.isclose(result, expected_result)
