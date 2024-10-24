d040d68e542ff54c6bd6f8eb19cea95aa922ad23
Hlib <hlib@metaculus.com>
2024-10-24 23:47:30 +0200

    Added gunicorn access logs (#1136)

--- scripts/prod/startapp.sh
+++ scripts/prod/startapp.sh
@@ -19,5 +19,5 @@ source venv/bin/activate
 export NEXT_PUBLIC_APP_URL="http://localhost:$PORT"
 export UV_THREADPOOL_SIZE=6
 export NODE_OPTIONS="--max-old-space-size=2048"
-(gunicorn metaculus_web.wsgi:application --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS --threads 4 --timeout 25 2>&1 | sed 's/^/[Backend]: /') &
+(gunicorn metaculus_web.wsgi:application --bind 0.0.0.0:8000 --access-logfile - --workers $GUNICORN_WORKERS --threads 4 --timeout 25 2>&1 | sed 's/^/[Backend]: /') &
 (cd front_end && npm run start 2>&1 | sed 's/^/[Frontend]: /')
