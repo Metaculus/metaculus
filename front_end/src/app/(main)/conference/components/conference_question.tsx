d275d49f6cf3252d3f4bbfc4f801afc31b0e57c2
Connor McCormick <connor@lotspot.co>
2024-10-28 20:01:55 -0700

    Ugly implementation of conference question

--- front_end/src/app/(main)/conference/components/conference_question.tsx
+++ front_end/src/app/(main)/conference/components/conference_question.tsx
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 import { getPost } from "@/app/(main)/conference/api-actions";
 import { PostWithForecasts } from "@/types/post";
 
+
+import BinarySlider, {
+  BINARY_FORECAST_PRECISION,
+} from "@/app/(main)/questions/[id]/components/forecast_maker/binary_slider";
+
 const ConferenceQuestion = ({ questionId }: { questionId: number }) => {
   const [question, setQuestion] = useState<PostWithForecasts | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [prediction, setPrediction] = useState<number | null>(null);
+  const [communityForecast, setCommunityForecast] = useState<number>(0.5);
 
   useEffect(() => {
     const fetchQuestion = async () => {
@@ -40,6 +47,17 @@ const ConferenceQuestion = ({ questionId }: { questionId: number }) => {
   return (
     <div className="text-center">
       <h2 className="mb-8 text-3xl font-bold">{question.title}</h2>
+      <BinarySlider
+        forecast={prediction}
+        onChange={(value) => {
+          setPrediction(value);
+        }}
+        isDirty={true}
+        communityForecast={communityForecast}
+        onBecomeDirty={() => {}}
+        disabled={false}
+        // helperDisplay={true}
+      />
     </div>
   );
 };
