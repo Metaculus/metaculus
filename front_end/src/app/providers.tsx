9b85758af0638a3709fcaf65391c146fa21a7509
Nikita <nikita.develops.work@gmail.com>
2024-11-05 17:10:05 +0200

    feat(communities): enable discover tab on the main feed based on feature flag

--- front_end/src/app/providers.tsx
+++ front_end/src/app/providers.tsx
@@ -7,19 +7,20 @@ import { getAnalyticsCookieConsentGiven } from "@/app/(main)/components/cookies_
 
 export function CSPostHogProvider({ children }: { children: any }) {
   useEffect(() => {
-    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        // set to 'always' to create profiles for anonymous users as well
-        person_profiles: "identified_only",
-        // Disable automatic pageview capture, as we capture manually
-        capture_pageview: false,
-        persistence:
-          getAnalyticsCookieConsentGiven() === "yes"
-            ? "localStorage+cookie"
-            : "memory",
-      });
-    }
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "/ingest",
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST
+        ? undefined
+        : "https://us.posthog.com",
+      // set to 'always' to create profiles for anonymous users as well
+      person_profiles: "identified_only",
+      // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false,
+      persistence:
+        getAnalyticsCookieConsentGiven() === "yes"
+          ? "localStorage+cookie"
+          : "memory",
+    });
   }, []);
 
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
