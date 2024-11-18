d132b6712a049917c48f883a695c0efb0b601dcd
Nikita <nikita.develops.work@gmail.com>
2024-11-18 14:28:05 +0200

    fix(thecurve): refactor question info

--- front_end/src/app/(thecurve)/components/curve_question.tsx
+++ front_end/src/app/(thecurve)/components/curve_question.tsx
@@ -1,22 +1,74 @@
-import { FC } from "react";
+"use client";
 
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+import { useTranslations } from "next-intl";
+import { FC, useEffect, useRef, useState } from "react";
+
+import Button from "@/components/ui/button";
 import { PostWithForecasts } from "@/types/post";
 
 import CurveQuestionDetails from "./question_details/curve_question_details";
 
 type Props = {
   post: PostWithForecasts;
+  expandLabel?: string;
+  collapseLabel?: string;
+  className?: string;
 };
 
-const CurveQuestion: FC<Props> = ({ post }) => {
+const HEADER_HEIGHT = 48;
+const CurveQuestion: FC<Props> = ({
+  post,
+  expandLabel: _expandLabel,
+  collapseLabel: _collapseLabel,
+}) => {
+  const t = useTranslations();
+  const expandLabel = _expandLabel ?? t("details");
+  const collapseLabel = _collapseLabel ?? t("collapse");
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [maxDetailsHeight, setMaxDetailsHeight] = useState<number>(500);
+  const [isExpanded, setIsExpanded] = useState(false);
+  useEffect(() => {
+    if (isExpanded && wrapperRef.current) {
+      const wrapperHeight = wrapperRef.current.offsetHeight;
+      const windowHeight = window.innerHeight;
+      setMaxDetailsHeight(windowHeight - wrapperHeight - HEADER_HEIGHT);
+    }
+  }, [isExpanded]);
   return (
-    <div className="w-full">
-      <div className="relative max-h-[calc(100vh-48px)] w-full overflow-y-auto bg-blue-800 p-5 pb-3 md:rounded-t">
+    <div className="relative w-full" ref={wrapperRef}>
+      <div className="max-h-[calc(100vh-48px)] w-full overflow-y-auto bg-blue-800 p-5 pb-3 md:rounded-t">
         <h1 className="m-0 text-2xl font-medium leading-8 text-gray-100">
           {post.title}
         </h1>
-        <CurveQuestionDetails question={post} />
+
+        <Button
+          variant="text"
+          className={classNames(
+            "z-10 mt-2 !justify-start !p-0 !font-normal !text-blue-500 dark:!text-blue-500"
+          )}
+          onClick={() => setIsExpanded((prev) => !prev)}
+        >
+          <FontAwesomeIcon
+            icon={isExpanded ? faChevronUp : faChevronDown}
+            className="m-0"
+            width={10}
+            height={10}
+          />
+
+          {isExpanded ? collapseLabel : expandLabel}
+        </Button>
       </div>
+      {isExpanded && (
+        <div
+          className="absolute top-full z-50 overflow-y-scroll bg-blue-800 p-5 pt-0 lg:!max-h-[510px]"
+          style={{ maxHeight: `${maxDetailsHeight}px` }}
+        >
+          <CurveQuestionDetails question={post} />
+        </div>
+      )}
     </div>
   );
 };
--- front_end/src/app/(thecurve)/components/question_details/curve_question_details.tsx
+++ front_end/src/app/(thecurve)/components/question_details/curve_question_details.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { useTranslations } from "next-intl";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 import MarkdownEditor from "@/components/markdown_editor";
-import Button from "@/components/ui/button";
 import { PostWithForecasts } from "@/types/post";
 
 import "./styles.css";
@@ -16,69 +13,39 @@ type Props = {
   question: PostWithForecasts;
   expandLabel?: string;
   collapseLabel?: string;
-  className?: string;
 };
 
 const CurveQuestionDetails: FC<Props> = ({
   question,
   expandLabel: _expandLabel,
   collapseLabel: _collapseLabel,
-  className,
 }) => {
   const t = useTranslations();
-  const expandLabel = _expandLabel ?? t("details");
-  const collapseLabel = _collapseLabel ?? t("collapse");
-
-  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className={classNames("relative flex flex-col")}>
-      {isExpanded && (
-        <div>
-          {!!question.group_of_questions?.resolution_criteria && (
-            <>
-              <h3 className="m-0 mt-3 text-lg font-normal leading-5 text-blue-500 dark:text-blue-500">
-                {t("resolutionCriteria")}
-              </h3>
-              <MarkdownEditor
-                markdown={question.group_of_questions?.resolution_criteria}
-                contentEditableClassName="thecurve"
-              />
-            </>
-          )}
-          {!!question.group_of_questions?.fine_print && (
-            <>
-              <h3 className="m-0 mt-3 text-lg font-normal leading-5 text-blue-500 dark:text-blue-500">
-                {t("finePrint")}
-              </h3>
-              <MarkdownEditor
-                markdown={question.group_of_questions?.fine_print}
-                contentEditableClassName="thecurve"
-              />
-            </>
-          )}
-        </div>
+    <div className={classNames("flex flex-col")}>
+      {!!question.group_of_questions?.resolution_criteria && (
+        <>
+          <h3 className="m-0 mt-3 text-lg font-normal leading-5 text-blue-500 dark:text-blue-500">
+            {t("resolutionCriteria")}
+          </h3>
+          <MarkdownEditor
+            markdown={question.group_of_questions?.resolution_criteria}
+            contentEditableClassName="thecurve"
+          />
+        </>
       )}
-
-      {isExpanded && (
-        <div className="absolute bottom-0 left-0 h-10 w-full bg-gradient-to-t to-transparent"></div>
+      {!!question.group_of_questions?.fine_print && (
+        <>
+          <h3 className="m-0 mt-3 text-lg font-normal leading-5 text-blue-500 dark:text-blue-500">
+            {t("finePrint")}
+          </h3>
+          <MarkdownEditor
+            markdown={question.group_of_questions?.fine_print}
+            contentEditableClassName="thecurve"
+          />
+        </>
       )}
-      <Button
-        variant="text"
-        className={classNames(
-          "z-10 mt-2 !justify-start !p-0 !font-normal !text-blue-500 dark:!text-blue-500"
-        )}
-        onClick={() => setIsExpanded((prev) => !prev)}
-      >
-        <FontAwesomeIcon
-          icon={isExpanded ? faChevronUp : faChevronDown}
-          className="m-0"
-          width={10}
-          height={10}
-        />
-
-        {isExpanded ? collapseLabel : expandLabel}
-      </Button>
     </div>
   );
 };
