a9df1ca0703858ea96c549feea4ff305f3e32e91
Nikita <93587872+ncarazon@users.noreply.github.com>
2024-11-20 14:10:46 +0200

    feat(markdown-editor): populate user mention items with local mentions info

--- front_end/src/components/comment_feed/index.tsx
+++ front_end/src/components/comment_feed/index.tsx
@@ -5,12 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useState } from "react";
 
 import { getComments, markPostAsRead } from "@/app/(main)/questions/actions";
 import { useContentTranslatedBannerProvider } from "@/app/providers";
 import Comment from "@/components/comment_feed/comment";
 import CommentEditor from "@/components/comment_feed/comment_editor";
+import { DefaultUserMentionsContextProvider } from "@/components/markdown_editor/plugins/mentions/components/default_mentions_context";
+import { MentionItem } from "@/components/markdown_editor/plugins/mentions/types";
 import ButtonGroup, { GroupButton } from "@/components/ui/button_group";
 import DropdownMenu, { MenuItemProps } from "@/components/ui/dropdown_menu";
 import LoadingIndicator from "@/components/ui/loading_indicator";
@@ -132,6 +134,11 @@ const CommentFeed: FC<Props> = ({
   const [offset, setOffset] = useState<number>(0);
   const postId = postData?.id;
   const includeUserForecast = shouldIncludeForecast(postData);
+
+  const commentAuthorMentionItems: MentionItem[] = useMemo(
+    () => extractUniqueAuthors(comments),
+    [comments]
+  );
 
   const { setBannerisVisible } = useContentTranslatedBannerProvider();
 
@@ -292,134 +299,162 @@ const CommentFeed: FC<Props> = ({
   );
 
   return (
-    <section
-      id={id}
-      className={classNames(
-        "max-w-full text-gray-900 dark:text-gray-900-dark",
-        {
-          "mt-6 w-full px-0 md:px-3": inNotebook,
-        },
-        {
-          "w-[48rem] border-transparent bg-gray-0 px-3 py-2 after:mt-6 after:block after:w-full after:content-[''] dark:border-blue-200-dark dark:bg-gray-0-dark xs:px-4 lg:border":
-            !inNotebook,
-        }
-      )}
+    <DefaultUserMentionsContextProvider
+      defaultUserMentions={commentAuthorMentionItems}
     >
-      <div className="mb-4 mt-2 flex flex-col items-start gap-2">
-        <div className="flex w-full flex-row justify-between gap-4 md:gap-3">
-          <h2
-            className="m-0 flex scroll-mt-16 items-baseline justify-between capitalize break-anywhere"
-            id="comments"
-          >
-            {t("comments")}
-          </h2>
-          {!profileId && user && (
-            <ButtonGroup
-              value={feedFilters.is_private ? "private" : "public"}
-              buttons={feedOptions}
-              onChange={(section) => {
-                handleFilterChange("is_private", section === "private");
-              }}
-              variant="tertiary"
-            />
-          )}
-        </div>
-      </div>
-      {postId && (
-        <CommentEditor
-          shouldIncludeForecast={includeUserForecast}
-          postId={postId}
-          onSubmit={
-            //TODO: revisit after BE changes
-            (newComment) =>
-              setComments((prevComments) => [newComment, ...prevComments])
+      <section
+        id={id}
+        className={classNames(
+          "max-w-full text-gray-900 dark:text-gray-900-dark",
+          {
+            "mt-6 w-full px-0 md:px-3": inNotebook,
+          },
+          {
+            "w-[48rem] border-transparent bg-gray-0 px-3 py-2 after:mt-6 after:block after:w-full after:content-[''] dark:border-blue-200-dark dark:bg-gray-0-dark xs:px-4 lg:border":
+              !inNotebook,
           }
-          isPrivateFeed={feedFilters.is_private}
-        />
-      )}
-
-      <div className="mb-1 mt-3 flex flex-row items-center justify-start gap-1">
-        <span className="text-sm text-gray-600 dark:text-gray-600-dark">
-          {totalCount ? `${totalCount} ` : ""}
-          {t("commentsWithCount", { count: totalCount })}
-          {postData?.last_viewed_at && (
-            <>
-              {getUnreadCount(comments) > 0 && (
-                <span className="ml-1 font-bold text-purple-700 dark:text-purple-700-dark">
-                  ({getUnreadCount(comments)} {t("unread")})
-                </span>
-              )}
-            </>
-          )}
-        </span>
-        <DropdownMenu items={menuItems} itemClassName={"capitalize"}>
-          <Button variant="text" className="capitalize">
-            {menuItems.find((item) => item.id === feedFilters.sort)?.name ??
-              "sort"}
-            <FontAwesomeIcon icon={faChevronDown} />
-          </Button>
-        </DropdownMenu>
-      </div>
-      {comments.map((comment: CommentType) => (
-        <div
-          key={comment.id}
-          className={classNames(
-            "my-1.5 rounded-md border px-1.5 py-1 md:px-2.5 md:py-1.5",
-            {
-              "border-blue-400 dark:border-blue-400-dark": !(
-                postData?.last_viewed_at &&
-                new Date(postData.last_viewed_at) < new Date(comment.created_at)
-              ),
-              "border-purple-500 bg-purple-100/50 dark:border-purple-500-dark/60 dark:bg-purple-100-dark/50":
-                postData?.last_viewed_at &&
-                new Date(postData.last_viewed_at) <
-                  new Date(comment.created_at),
-            }
-          )}
-        >
-          {profileId && comment.on_post_data && (
-            <h3 className="mb-2 text-lg font-semibold">
-              <Link
-                href={`/questions/${comment.on_post_data.id}#comment-${comment.id}`}
-                className="text-blue-700 no-underline hover:text-blue-800 dark:text-blue-600-dark hover:dark:text-blue-300"
-              >
-                {comment.on_post_data.title}
-              </Link>
-            </h3>
-          )}
-          <Comment
-            onProfile={!!profileId}
-            comment={comment}
-            treeDepth={0}
-            /* replies should always be sorted from oldest to newest */
-            sort={"created_at" as SortOption}
-            postData={postData}
-            lastViewedAt={postData?.last_viewed_at}
-          />
-        </div>
-      ))}
-      {comments.length === 0 && !isLoading && (
-        <>
-          <hr className="my-4" />
-          <div className="text-center italic text-gray-700 dark:text-gray-700-dark">
-            {t("noComments")}
+        )}
+      >
+        <div className="mb-4 mt-2 flex flex-col items-start gap-2">
+          <div className="flex w-full flex-row justify-between gap-4 md:gap-3">
+            <h2
+              className="m-0 flex scroll-mt-16 items-baseline justify-between capitalize break-anywhere"
+              id="comments"
+            >
+              {t("comments")}
+            </h2>
+            {!profileId && user && (
+              <ButtonGroup
+                value={feedFilters.is_private ? "private" : "public"}
+                buttons={feedOptions}
+                onChange={(section) => {
+                  handleFilterChange("is_private", section === "private");
+                }}
+                variant="tertiary"
+              />
+            )}
           </div>
-        </>
-      )}
-      {isLoading && <LoadingIndicator className="mx-auto my-8 w-24" />}
-      {!isLoading && <FormErrorMessage errors={error?.digest} />}
-      {offset !== -1 && (
-        <div className="flex items-center justify-center pt-4">
-          <Button
-            onClick={() => fetchComments(true, { ...feedFilters, offset })}
-            disabled={isLoading}
-          >
-            {t("loadMoreComments")}
-          </Button>
         </div>
-      )}
-    </section>
+        {postId && (
+          <CommentEditor
+            shouldIncludeForecast={includeUserForecast}
+            postId={postId}
+            onSubmit={
+              //TODO: revisit after BE changes
+              (newComment) =>
+                setComments((prevComments) => [newComment, ...prevComments])
+            }
+            isPrivateFeed={feedFilters.is_private}
+          />
+        )}
+
+        <div className="mb-1 mt-3 flex flex-row items-center justify-start gap-1">
+          <span className="text-sm text-gray-600 dark:text-gray-600-dark">
+            {totalCount ? `${totalCount} ` : ""}
+            {t("commentsWithCount", { count: totalCount })}
+            {postData?.last_viewed_at && (
+              <>
+                {getUnreadCount(comments) > 0 && (
+                  <span className="ml-1 font-bold text-purple-700 dark:text-purple-700-dark">
+                    ({getUnreadCount(comments)} {t("unread")})
+                  </span>
+                )}
+              </>
+            )}
+          </span>
+          <DropdownMenu items={menuItems} itemClassName={"capitalize"}>
+            <Button variant="text" className="capitalize">
+              {menuItems.find((item) => item.id === feedFilters.sort)?.name ??
+                "sort"}
+              <FontAwesomeIcon icon={faChevronDown} />
+            </Button>
+          </DropdownMenu>
+        </div>
+        {comments.map((comment: CommentType) => (
+          <div
+            key={comment.id}
+            className={classNames(
+              "my-1.5 rounded-md border px-1.5 py-1 md:px-2.5 md:py-1.5",
+              {
+                "border-blue-400 dark:border-blue-400-dark": !(
+                  postData?.last_viewed_at &&
+                  new Date(postData.last_viewed_at) <
+                    new Date(comment.created_at)
+                ),
+                "border-purple-500 bg-purple-100/50 dark:border-purple-500-dark/60 dark:bg-purple-100-dark/50":
+                  postData?.last_viewed_at &&
+                  new Date(postData.last_viewed_at) <
+                    new Date(comment.created_at),
+              }
+            )}
+          >
+            {profileId && comment.on_post_data && (
+              <h3 className="mb-2 text-lg font-semibold">
+                <Link
+                  href={`/questions/${comment.on_post_data.id}#comment-${comment.id}`}
+                  className="text-blue-700 no-underline hover:text-blue-800 dark:text-blue-600-dark hover:dark:text-blue-300"
+                >
+                  {comment.on_post_data.title}
+                </Link>
+              </h3>
+            )}
+            <Comment
+              onProfile={!!profileId}
+              comment={comment}
+              treeDepth={0}
+              /* replies should always be sorted from oldest to newest */
+              sort={"created_at" as SortOption}
+              postData={postData}
+              lastViewedAt={postData?.last_viewed_at}
+            />
+          </div>
+        ))}
+        {comments.length === 0 && !isLoading && (
+          <>
+            <hr className="my-4" />
+            <div className="text-center italic text-gray-700 dark:text-gray-700-dark">
+              {t("noComments")}
+            </div>
+          </>
+        )}
+        {isLoading && <LoadingIndicator className="mx-auto my-8 w-24" />}
+        {!isLoading && <FormErrorMessage errors={error?.digest} />}
+        {offset !== -1 && (
+          <div className="flex items-center justify-center pt-4">
+            <Button
+              onClick={() => fetchComments(true, { ...feedFilters, offset })}
+              disabled={isLoading}
+            >
+              {t("loadMoreComments")}
+            </Button>
+          </div>
+        )}
+      </section>
+    </DefaultUserMentionsContextProvider>
   );
 };
+
+function extractUniqueAuthors(comments: CommentType[]): MentionItem[] {
+  const authorMap = new Map<number, string>();
+
+  const traverseComments = (commentList: CommentType[]) => {
+    for (const comment of commentList) {
+      if (!authorMap.has(comment.author.id)) {
+        authorMap.set(comment.author.id, comment.author.username);
+      }
+
+      if (comment.children.length) {
+        traverseComments(comment.children);
+      }
+    }
+  };
+
+  traverseComments(comments);
+
+  return Array.from(authorMap.entries()).map(([userId, value]) => ({
+    userId,
+    value,
+  }));
+}
 
 export default CommentFeed;
