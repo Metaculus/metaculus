900dcadd4a79744aed7d05de53540e0be587c280
Vasile Popescu <elisescu@elisescu.com>
2024-12-02 22:00:19 +0100

    Update the BW page to the latest design

--- front_end/src/app/(campaigns-registration)/projM123/page.tsx
+++ front_end/src/app/(campaigns-registration)/projM123/page.tsx
@@ -1,44 +1,65 @@
 import ProfileApi from "@/services/profile";
 import Header from "./components/header";
-import { AlreadyRegisteredCard, ChoicesCards } from "./components/cards";
+import { SucessfullyRegistered } from "./components/cards";
 import { redirect } from "next/navigation";
-import { Heading1, Heading2 } from "./components/typography";
+import { ChoicesButtons, HeroSection } from "./components/hero-section";
+import { CAMPAIGN_KEY, CAMPAIGN_URL_BASE_PATH } from "./constants";
 
 export const metadata = {
-  title: "projM123",
-  description: "Project M123",
+  title: "Bridgewater x Metaculus",
+  description:
+    "Register to forecast, explore opportunities with Bridgewater Associates, and compete for $25,000 in prizes!",
 };
 
 export default async function Page() {
   const user = await ProfileApi.getMyProfile();
 
-  if (user && user.registered_campaign_keys.includes("projM123")) {
+  if (user && user.registered_campaign_keys.includes(CAMPAIGN_KEY)) {
     return (
       <>
-        <Header />
-        <main className="flex flex-grow justify-center">
-          <div className="mt-16 flex size-full flex-col items-center">
-            <Heading1>ProjectM123</Heading1>
-            <AlreadyRegisteredCard />
-          </div>
-        </main>
+        <>
+          <Header />
+          <main className="flex flex-grow justify-center">
+            <HeroSection className="m-5 w-full max-w-[896px] pb-10">
+              <div className="flex w-full flex-col items-center px-5">
+                <SucessfullyRegistered />
+              </div>
+            </HeroSection>
+          </main>
+        </>
       </>
     );
   } else if (user) {
-    redirect("register");
-    return null;
+    redirect(`${CAMPAIGN_URL_BASE_PATH}/register`);
   }
 
   return (
     <>
       <Header />
-      <main className="flex flex-grow justify-center">
-        <div className="mt-16 flex size-full flex-col items-center">
-          <Heading1>ProjectM123</Heading1>
-          <Heading2>Register for project M123</Heading2>
-          <div className="mt-6">
-            <ChoicesCards />
+      <main className="flex flex-col items-center justify-center p-3 sm:p-5 ">
+        <HeroSection className="m-5 w-full max-w-[896px]">
+          <div className="w-full px-5">
+            <ChoicesButtons />
           </div>
+        </HeroSection>
+
+        <div className="mx-8 text-blue-800 sm:mx-12 md:max-w-[670px]">
+          <p>
+            For the second year, Metaculus is teaming up with Bridgewater - a
+            premier asset management firm - for a unique forecasting
+            competition. No experience necessary. Just register today, and
+            starting February 3rd, make your predictions on a wide range of
+            topics!
+          </p>
+          <p>
+            The most accurate forecasters will be eligible for $25,000 in prizes
+            and potential opportunities with Bridgewater.
+          </p>
+          <p>
+            Register for the contest today and be notified as soon as questions
+            open. The earlier you forecast, the better your odds to beat the
+            competition and win cash prizes!
+          </p>
         </div>
       </main>
     </>
