f7e7fb7412827191b4b268e53c4cb798134065fa
Vasile Popescu <elisescu@elisescu.com>
2024-12-03 17:29:30 +0100

    Restructure the BW registration new and old pages

--- front_end/src/app/(campaigns-registration)/bridgewater/components/cards.tsx
+++ front_end/src/app/(campaigns-registration)/bridgewater/components/cards.tsx
@@ -12,7 +12,7 @@ export const SucessfullyRegistered = () => {
   const { setCurrentModal } = useModal();
 
   return (
-    <div className="flex w-full flex-col items-center gap-7 bg-blue-200 p-8 sm:w-[415px]">
+    <div className="flex w-full flex-col items-center gap-7 bg-blue-200 dark:bg-blue-200-dark p-8 sm:w-[415px]">
       <p className="my-0 text-base text-gray-900 dark:text-gray-900-dark">
         You are registered!
       </p>
@@ -41,7 +41,7 @@ export const SucessfullyRegistered = () => {
 export const SuccessAndVerifyEmail = () => {
   return (
     <HeroSection className="m-5 w-full max-w-[896px] pb-10">
-      <div className="flex w-full flex-col items-center gap-7 bg-blue-200 p-8 sm:w-[415px]">
+      <div className="flex w-full flex-col items-center gap-7 bg-blue-200 dark:bg-blue-200-dark p-8 sm:w-[415px]">
         <p className="my-0 text-center text-base text-gray-900 dark:text-gray-900-dark">
           You have successfully registered! Confirm your email now.
         </p>
