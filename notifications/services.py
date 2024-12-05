cb9a924f8e276aaae202fbfff5d517f0e3c86b3f
hlbmtc <hlib@metaculus.com>
2024-12-05 14:54:03 +0100

    Adjusted settings

--- notifications/services.py
+++ notifications/services.py
@@ -688,4 +688,5 @@ def send_comment_mention_notification(recipient, comment: Comment, mention: str)
             },
         },
         use_async=False,
+        from_email=settings.EMAIL_NOTIFICATIONS_USER,
     )
