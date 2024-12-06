b5f4c024c9d80a1bcb872d4694f3603c04bdd328
Nikita <nikita.develops.work@gmail.com>
2024-12-06 18:10:03 +0200

    fix(scroll-hook): code cleanup

--- front_end/src/hooks/use_scroll_to.ts
+++ front_end/src/hooks/use_scroll_to.ts
@@ -4,11 +4,11 @@ import { useCallback } from "react";
 
 const headerOffset = 48;
 const extraOffset = 12;
-export const COMMENT_SCROLL_OFFSET = headerOffset + extraOffset;
+const SCROLL_OFFSET = headerOffset + extraOffset;
 
 const useScrollTo = () => {
   return useCallback((top: number, behavior: ScrollBehavior = "smooth") => {
-    const y = top + window.scrollY - COMMENT_SCROLL_OFFSET;
+    const y = top + window.scrollY - SCROLL_OFFSET;
 
     window.scrollTo({ top: y, behavior });
   }, []);
