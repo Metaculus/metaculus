f468369a2ef3cfbe9b31ab9b75dfd7ea178a2ff5
Hlib <hlib@metaculus.com>
2024-12-09 20:01:17 +0100

    Post edition embedding indexing (#1635)
    * Added post embedding reindexing on text change
    
    * Fixed home + navbar search to sort by relevance
    
    * Wrapped `run_post_indexing` to log errors

--- tests/unit/test_posts/test_services/test_common.py
+++ tests/unit/test_posts/test_services/test_common.py
@@ -1,8 +1,12 @@
-from posts.services.common import get_posts_staff_users
+from django.utils import timezone
+
+from posts.services.common import get_posts_staff_users, update_post
 from projects.permissions import ObjectPermission
+from questions.models import Question
 from tests.unit.fixtures import *  # noqa
 from tests.unit.test_posts.factories import factory_post
 from tests.unit.test_projects.factories import factory_project
+from tests.unit.test_questions.factories import create_question
 
 
 def test_get_posts_staff_users(user1, user2):
@@ -42,3 +46,32 @@ def test_get_posts_staff_users(user1, user2):
     assert data[post_2][user2.id] == ObjectPermission.CURATOR
 
     assert data[post_3] == {}
+
+
+def test_update_post__run_post_indexing(user1, user2, mocker):
+    mock_run_post_indexing = mocker.patch("posts.tasks.run_post_indexing.send")
+
+    post_1 = factory_post(
+        author=user1,
+        title="Post",
+        question=create_question(
+            title="Binary Question", question_type=Question.QuestionType.BINARY
+        ),
+    )
+
+    # No text updated
+    update_post(post_1, scheduled_close_time=timezone.now())
+    mock_run_post_indexing.assert_not_called()
+
+    # Updated text - reindexing
+    update_post(post_1, title="Updated Post")
+    mock_run_post_indexing.assert_called_once()
+
+    # No text updated
+    mock_run_post_indexing.reset_mock()
+    update_post(post_1, scheduled_close_time=timezone.now())
+    mock_run_post_indexing.assert_not_called()
+
+    # Updated question text - reindexing
+    update_post(post_1, question={"title": "Updated Binary Question"})
+    mock_run_post_indexing.assert_called_once()
