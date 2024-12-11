4ce7a2dea8972ecb1c680015755ad632284b0f37
Vasile Popescu <elisescu@elisescu.com>
2024-12-11 22:02:00 +0100

    Minor copy update for the notice-at-collection bw doc

--- front_end/src/app/(campaigns-registration)/bridgewater/notice-at-collection/page.tsx
+++ front_end/src/app/(campaigns-registration)/bridgewater/notice-at-collection/page.tsx
@@ -123,7 +123,7 @@ export default function NoticeAtCollection() {
                 <td className="px-4 py-6 align-top">
                   Residency is used to verify eligibility for this tournament.
                 </td>
-                <td className="px-4 py-6 align-top">Yes</td>
+                <td className="px-4 py-6 align-top">Residency information may be shared with Bridgewater</td>
               </tr>
 
               <tr>
