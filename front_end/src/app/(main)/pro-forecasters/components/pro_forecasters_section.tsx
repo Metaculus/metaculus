f4895ca35495e22dccbccafedaf2b94417507c3e
Nikita <nikita.develops.work@gmail.com>
2024-12-12 11:25:31 +0200

    fix(pro-forecasters): improve image quality for pro-forecasters and fix text copy

--- front_end/src/app/(main)/pro-forecasters/components/pro_forecasters_section.tsx
+++ front_end/src/app/(main)/pro-forecasters/components/pro_forecasters_section.tsx
@@ -18,6 +18,7 @@ const ProForecasterCard: FC<ProForecaster> = ({
         src={image}
         alt={name}
         className="size-[72px] shrink-0 rounded-2xl object-cover md:size-[180px]"
+        quality={100}
       />
 
       <div className="-mt-1 flex flex-col gap-2">
