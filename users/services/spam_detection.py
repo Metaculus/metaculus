26d50422000de70e5cb42e9ba0f7cbd5b2847063
lsabor <lukesabor@gmail.com>
2024-12-26 11:11:55 -0800

    add spam detection run on command from admin panel, and refactor some basic logic in spam_detection.py

--- users/services/spam_detection.py
+++ users/services/spam_detection.py
@@ -11,6 +11,7 @@ from users.models import User
 from utils.openai import generate_text_async
 from misc.tasks import send_email_async
 import time
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,8 +22,21 @@ def check_profile_update_for_spam(
     days_since_joined_threshold = 7
     request_data = cast(dict, valid_serializer.validated_data)
 
-    bio: str | None = request_data.get("bio")
-    website: str | None = request_data.get("website")
+    if days_since_joined > days_since_joined_threshold:
+        idenficated_as_spam = False
+        reasoning = (
+            "The user has been a member for more than "
+            f"{days_since_joined_threshold} days"
+        )
+    else:
+        idenficated_as_spam, reasoning = check_data_for_spam(user, **request_data)
+
+    return idenficated_as_spam, reasoning
+
+
+def check_data_for_spam(user: User, **args):
+    bio: str | None = args.get("bio")
+    website: str | None = args.get("website")
     if bio and website:
         bio_plus_website = f"{bio}\n\nWebsite: {website}"
     elif not bio and website:
@@ -42,9 +56,6 @@ def check_profile_update_for_spam(
     elif len(bio_plus_website) < 10:
         idenficated_as_spam = False
         reasoning = "Bio is too short to be spam"
-    elif days_since_joined > days_since_joined_threshold:
-        idenficated_as_spam = False
-        reasoning = f"The user has been a member for more than {days_since_joined_threshold} days"
     elif len(bio_plus_website) > 17500:
         idenficated_as_spam = True
         reasoning = "Bio is more than 17500 characters"
@@ -58,9 +69,11 @@ def check_profile_update_for_spam(
 
     if idenficated_as_spam:
         logger.info(
-            f"User: {user.username} ID: {user.id} was soft deleted for spam bio: {bio_plus_website[:100]}... "
+            f"User: {user.username} ID: {user.id} was soft deleted "
+            f"for spam bio: {bio_plus_website[:100]}... "
             f"The reason was: {reasoning[:100]}... "
-            f"It took {duration:.2f} seconds to check. gpt_was_used: {gpt_was_used}"
+            f"It took {duration:.2f} seconds to check. "
+            f"gpt_was_used: {gpt_was_used}"
         )
     return idenficated_as_spam, reasoning
 
@@ -104,7 +117,7 @@ async def ask_gpt_to_check_profile_for_spam(
             system_prompt=system_prompt,
             prompt=prompt,
             temperature=0,
-            timeout=7
+            timeout=7,
         )
         is_spam = "TRUE" in gpt_response
     except Exception as e:
