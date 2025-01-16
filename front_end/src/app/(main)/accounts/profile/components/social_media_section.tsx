43a1f5eead40761c902dad5457041dbe4251a5de
Vasile Popescu <elisescu@elisescu.com>
2025-01-16 18:23:25 +0100

    Add the new UI for the User profile

--- front_end/src/app/(main)/accounts/profile/components/social_media_section.tsx
+++ front_end/src/app/(main)/accounts/profile/components/social_media_section.tsx
@@ -13,40 +13,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { FC } from "react";
 
-import { FormError, Input } from "@/components/ui/form_field";
 import { UserProfile } from "@/types/users";
 
-const SocialMediaSection: FC<{
-  user: UserProfile;
-  editMode: boolean;
-  register: any;
-  state: any;
-}> = ({ user, editMode, register, state }) => {
-  const socialMedia = [
-    [faEarth, user.website, "website"],
-    [faTwitter, user.twitter, "twitter"],
-    [faLinkedin, user.linkedin, "linkedin"],
-    [faFacebook, user.facebook, "facebook"],
-    [faGithub, user.github, "github"],
-    [faChartLine, user.good_judgement_open, "good_judgement_open"],
-    [faChartLine, user.kalshi, "kalshi"],
-    [faChartLine, user.manifold, "manifold"],
-    [faChartLine, user.infer, "infer"],
-    [faChartLine, user.hypermind, "hypermind"],
-  ];
+export const getSocialMediaArray = (
+  user: UserProfile
+): [IconDefinition, string | undefined, string][] => [
+  [faEarth, user.website, "website"],
+  [faTwitter, user.twitter, "twitter"],
+  [faLinkedin, user.linkedin, "linkedin"],
+  [faFacebook, user.facebook, "facebook"],
+  [faGithub, user.github, "github"],
+  [faChartLine, user.good_judgement_open, "good_judgement_open"],
+  [faChartLine, user.kalshi, "kalshi"],
+  [faChartLine, user.manifold, "manifold"],
+  [faChartLine, user.infer, "infer"],
+  [faChartLine, user.hypermind, "hypermind"],
+];
 
+export const hasUserSocialMediaLink = (user: UserProfile) => {
+  return getSocialMediaArray(user).filter(([, link]) => !!link).length > 0;
+};
+
+const SocialMediaFragment: FC<{
+  user: UserProfile;
+}> = ({ user }) => {
+  const socialMedia = getSocialMediaArray(user).filter(([, url]) => !!url);
   return (
-    <div
-      className={
-        "space-x-1 space-y-1" + (editMode ? " flex flex-col" : " flex flex-row")
-      }
-    >
-      {socialMedia.map((x, index) => {
-        const icon: IconDefinition = x[0] as IconDefinition;
-        const link: string | undefined = x[1] as string;
-        const label: string = x[2] as string;
+    <>
+      {socialMedia.map(([icon, link, label]) => {
         return (
-          <div key={index} className="flex flex-col">
+          <div key={label} className="flex flex-col">
             {link && (
               <Link href={link} target="_blank" rel="ugc">
                 <FontAwesomeIcon
@@ -56,23 +52,11 @@ const SocialMediaSection: FC<{
                 ></FontAwesomeIcon>
               </Link>
             )}
-            {editMode && (
-              <div className="flex flex-col">
-                <span>{label}</span>
-                <Input
-                  className="w-6/12 rounded border border-gray-700 px-3 py-2 text-sm placeholder:italic dark:border-gray-700-dark	"
-                  placeholder="http://www.example.com"
-                  defaultValue={link ? link : ""}
-                  {...register(label)}
-                />
-                <FormError errors={state?.errors} name={label} />
-              </div>
-            )}
           </div>
         );
       })}
-    </div>
+    </>
   );
 };
 
-export default SocialMediaSection;
+export default SocialMediaFragment;
