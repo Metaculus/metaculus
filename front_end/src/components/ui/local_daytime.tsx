42c1389b8ea579347e5e1d20ff220ccab49f6795
Patrick Molgaard <patrick@ataraxia.systems>
2025-01-20 18:24:43 +0000

    LocalDaytime: make this an instance of relative-time
    I've tried to follow the spirit of the current behaviour/design here; it
    only displays an actual relative time if the relative time is +- 1 day
    from now.
    
    So if you have a question closing tomorrow, it'll say:
    
    closes: Jan 21, 2025
    
    but if you have a question closing today it'll say:
    
    closes: XX hours YY mins

--- front_end/src/components/ui/local_daytime.tsx
+++ front_end/src/components/ui/local_daytime.tsx
@@ -1,5 +1,6 @@
 import { useLocale } from "next-intl";
 import { FC } from "react";
+import "@github/relative-time-element";
 
 import { formatDate } from "@/utils/date_formatters";
 
@@ -11,7 +12,18 @@ const LocalDaytime: FC<Props> = ({ date }) => {
   const locale = useLocale();
   const localValue = date ? formatDate(locale, new Date(date)) : "";
 
-  return <span>{localValue}</span>;
+  return (
+    <relative-time
+      datetime={date}
+      format="relative"
+      prefix=""
+      threshold="P1D"
+      year="numeric"
+      lang={locale}
+    >
+      {localValue}
+    </relative-time>
+  );
 };
 
 export default LocalDaytime;
