a2e29e9b36ff74356e7e771f2e7323a94b045bbe
Patrick Molgaard <patrick@ataraxia.systems>
2025-01-20 18:10:34 +0000

    refactor CpRevealTime
    - use relative-time component for consistency with comment timestamps
    - this means we can also drop the option to take a formatter in the
      LocalDaytime component and simplify that

--- front_end/src/components/ui/local_daytime.tsx
+++ front_end/src/components/ui/local_daytime.tsx
@@ -5,16 +5,11 @@ import { formatDate } from "@/utils/date_formatters";
 
 type Props = {
   date?: string;
-  formatFn?: (date: Date, locale: string) => string;
 };
 
-const LocalDaytime: FC<Props> = ({ date, formatFn }) => {
+const LocalDaytime: FC<Props> = ({ date }) => {
   const locale = useLocale();
-  const localValue = date
-    ? formatFn
-      ? formatFn(new Date(date), locale)
-      : formatDate(locale, new Date(date))
-    : "";
+  const localValue = date ? formatDate(locale, new Date(date)) : "";
 
   return <span>{localValue}</span>;
 };
