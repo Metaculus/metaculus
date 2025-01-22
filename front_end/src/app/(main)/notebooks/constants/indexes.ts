d879e2d78702c397cae8e7e99d01790e55f3da4c
Nikita <nikita.develops.work@gmail.com>
2025-01-22 16:32:36 +0200

    feat(notebook): update AGI Readiness Index questions

--- front_end/src/app/(main)/notebooks/constants/indexes.ts
+++ front_end/src/app/(main)/notebooks/constants/indexes.ts
@@ -4,15 +4,13 @@ export const AGI_INDEX_SLUG = "agi-readiness-index";
 
 export const NOTEBOOK_INDEXES: NotebookIndex = {
   [AGI_INDEX_SLUG]: [
-    { questionId: 3820, weight: 12 },
-    { questionId: 28002, weight: 12 },
-    { questionId: 27995, weight: 10 },
-    { questionId: 27898, weight: 9 },
-    { questionId: 27797, weight: 9 },
-    { questionId: 27642, weight: 11 },
-    { questionId: 28163, weight: -8 },
-    { questionId: 27998, weight: 6 },
-    { questionId: 27881, weight: 2.5 },
-    { questionId: 26961, weight: -1.5 },
+    { questionId: 31688, weight: 12 },
+    { questionId: 31689, weight: 12 },
+    { questionId: 31707, weight: 10 },
+    { questionId: 31709, weight: 9 },
+    // { questionId: 27797, weight: 9 }, // currently missing on prod
+    { questionId: 31710, weight: 11 },
+    { questionId: 31711, weight: -8 },
+    { questionId: 31712, weight: 6 },
   ],
 };
