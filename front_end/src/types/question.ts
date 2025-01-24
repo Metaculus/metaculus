9b5a758a979044eeac8f308fe1b55c259fd6c6b5
lsabor <lukesabor@gmail.com>
2025-01-24 13:59:23 -0800

    add MC histogram support

--- front_end/src/types/question.ts
+++ front_end/src/types/question.ts
@@ -137,7 +137,7 @@ export type AggregateForecast = Forecast & {
   method: AggregationMethod;
   forecaster_count: number;
   means: number[] | null;
-  histogram: number[] | null;
+  histogram: number[][] | null;
   forecast_values: number[] | null;
 };
 
