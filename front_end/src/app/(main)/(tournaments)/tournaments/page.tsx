1227fed0266c0c5ca6b7236d1bbebc7d52f9782c
lsabor <lukesabor@gmail.com>
2025-02-05 15:16:55 -0800

    remove text from projects page

--- front_end/src/app/(main)/(tournaments)/tournaments/page.tsx
+++ front_end/src/app/(main)/(tournaments)/tournaments/page.tsx
@@ -20,27 +20,35 @@ export default async function Tournaments() {
   const tournaments = await ProjectsApi.getTournaments();
   const { activeTournaments, archivedTournaments, questionSeries } =
     extractTournamentLists(tournaments);
+
+  const minimalUI =
+    (process.env.MINIMAL_UI || "false").toLowerCase() === "true";
+
   return (
     <main className="mx-auto mb-24 mt-16 w-full max-w-7xl flex-1 px-4 text-blue-700 dark:text-blue-700-dark sm:mt-28 sm:px-8 md:px-12 lg:px-16">
-      <div>
-        <h1 className="mb-12 mt-0 text-4xl sm:text-5xl">{t("tournaments")}</h1>
-        <p className="my-4 text-lg">{t("tournamentsHero1")}</p>
-        <p className="my-4 text-lg">{t("tournamentsHero2")}</p>
-        <p className="my-4 text-lg">
-          {t.rich("tournamentsHero3", {
-            scores: (chunks) => (
-              <Link href="/help/scores-faq/#tournament-scores">{chunks}</Link>
-            ),
-          })}
-        </p>
-        <p className="my-4 text-lg">
-          {t.rich("tournamentsHero4", {
-            email: (chunks) => (
-              <a href="mailto:hello@metaculus.com">{chunks}</a>
-            ),
-          })}
-        </p>
-      </div>
+      {!minimalUI && (
+        <div>
+          <h1 className="mb-12 mt-0 text-4xl sm:text-5xl">
+            {t("tournaments")}
+          </h1>
+          <p className="my-4 text-lg">{t("tournamentsHero1")}</p>
+          <p className="my-4 text-lg">{t("tournamentsHero2")}</p>
+          <p className="my-4 text-lg">
+            {t.rich("tournamentsHero3", {
+              scores: (chunks) => (
+                <Link href="/help/scores-faq/#tournament-scores">{chunks}</Link>
+              ),
+            })}
+          </p>
+          <p className="my-4 text-lg">
+            {t.rich("tournamentsHero4", {
+              email: (chunks) => (
+                <a href="mailto:hello@metaculus.com">{chunks}</a>
+              ),
+            })}
+          </p>
+        </div>
+      )}
 
       <TournamentFilters />
 
