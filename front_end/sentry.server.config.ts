ce178a905430aaac601d410c88a80b927e32d640
hlbmtc <hlib@metaculus.com>
2025-02-13 19:52:31 +0700

    Fixed frontend sentry env vars

--- front_end/sentry.server.config.ts
+++ front_end/sentry.server.config.ts
@@ -1,8 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
 
-if (!!process.env.PUBLIC_SENTRY_DSN) {
+if (!!process.env.PUBLIC_FRONTEND_SENTRY_DSN) {
   Sentry.init({
-    dsn: process.env.PUBLIC_SENTRY_DSN,
+    environment: process.env.METACULUS_ENV,
+    dsn: process.env.PUBLIC_FRONTEND_SENTRY_DSN,
     tracesSampleRate: 0.1,
     ignoreErrors: ["NEXT_REDIRECT", "NEXT_NOT_FOUND"],
   });
