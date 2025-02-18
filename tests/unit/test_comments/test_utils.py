78c04b24c1500dbaef59ba9593f57f34111ded04
hlbmtc <hlib@metaculus.com>
2025-02-18 23:50:16 +0700

    Fixed linter

--- tests/unit/test_comments/test_utils.py
+++ tests/unit/test_comments/test_utils.py
@@ -1,3 +1,4 @@
+import pytest
 import re
 
 from comments.utils import (
@@ -10,6 +11,7 @@ from projects.permissions import ObjectPermission
 from tests.unit.fixtures import *  # noqa
 from tests.unit.test_comments.factories import factory_comment
 from tests.unit.test_projects.factories import factory_project
+from tests.unit.test_posts.factories import factory_post
 from tests.unit.test_questions.conftest import *  # noqa
 from tests.unit.test_questions.factories import factory_forecast
 from tests.unit.test_users.factories import factory_user
