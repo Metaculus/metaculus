7a04241e16c6cda37c1fb19248b045a237424644
Nikita <93587872+ncarazon@users.noreply.github.com>
2025-03-12 21:34:55 +0200

    feat(consumer-view): binary tiles (#2370)

--- front_end/src/components/post_card/consumer_post_card/index.tsx
+++ front_end/src/components/post_card/consumer_post_card/index.tsx
@@ -1,4 +1,3 @@
-import { isNil } from "lodash";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
@@ -9,6 +8,8 @@ import { getPostLink } from "@/utils/navigation";
 import {
   getGroupForecastAvailability,
   getQuestionForecastAvailability,
+  isGroupOfQuestionsPost,
+  isQuestionPost,
 } from "@/utils/questions";
 
 import ConsumerKeyFactor from "./key_factor";
@@ -28,9 +29,9 @@ const ConsumerPostCard: FC<Props> = ({ post }) => {
   return (
     <Link
       href={getPostLink(post)}
-      className={cn(
+      className={
         "flex flex-col items-center rounded border border-blue-400 bg-gray-0 p-6 no-underline @container dark:border-blue-400-dark dark:bg-gray-0-dark"
-      )}
+      }
     >
       <div className="flex w-full flex-col items-center justify-between gap-4 @[500px]:flex-row @[500px]:gap-2">
         <div className="flex flex-col gap-4 @[500px]:gap-2">
@@ -72,11 +73,10 @@ const ConsumerPostCard: FC<Props> = ({ post }) => {
 };
 
 function getPostForecastAvailability(post: PostWithForecasts) {
-  const { question, group_of_questions } = post;
-  if (!isNil(question)) {
-    return getQuestionForecastAvailability(question);
-  } else if (!isNil(group_of_questions)) {
-    return getGroupForecastAvailability(group_of_questions.questions);
+  if (isQuestionPost(post)) {
+    return getQuestionForecastAvailability(post.question);
+  } else if (isGroupOfQuestionsPost(post)) {
+    return getGroupForecastAvailability(post.group_of_questions.questions);
   }
   return null;
 }
--- front_end/src/components/post_card/consumer_post_card/prediction_info.tsx
+++ front_end/src/components/post_card/consumer_post_card/prediction_info.tsx
@@ -1,14 +1,14 @@
-import { intlFormatDistance } from "date-fns";
 import { isNil } from "lodash";
-import { useLocale, useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 import { FC } from "react";
 
-import { PostWithForecasts, QuestionStatus } from "@/types/post";
+import { PostWithForecasts } from "@/types/post";
 import { ForecastAvailability, QuestionType } from "@/types/question";
-import "@github/relative-time-element";
-import { getDisplayValue } from "@/utils/charts";
-import cn from "@/utils/cn";
 import { formatResolution, isSuccessfullyResolved } from "@/utils/questions";
+
+import QuestionForecastChip from "./question_forecast_chip";
+import QuestionResolutionChip from "./question_resolution_chip";
+import UpcomingCP from "./upcoming_cp";
 
 type Props = {
   post: PostWithForecasts;
@@ -17,7 +17,6 @@ type Props = {
 
 const ConsumerPredictionInfo: FC<Props> = ({ post, forecastAvailability }) => {
   const { question, group_of_questions } = post;
-  const t = useTranslations();
   const locale = useLocale();
 
   // CP empty
@@ -26,22 +25,7 @@ const ConsumerPredictionInfo: FC<Props> = ({ post, forecastAvailability }) => {
   }
   // CP hidden
   if (!isNil(forecastAvailability?.cpRevealsOn)) {
-    return (
-      <div className="flex min-w-[200px] max-w-[200px] flex-col items-center gap-0">
-        <span className="text-xs font-normal leading-4 text-purple-700 dark:text-purple-700-dark">
-          {t("forecastRevealed")}{" "}
-        </span>
-        <relative-time
-          datetime={forecastAvailability.cpRevealsOn}
-          lang={locale}
-          className="text-base font-medium leading-6 text-purple-800 dark:text-purple-800-dark"
-        >
-          {intlFormatDistance(forecastAvailability.cpRevealsOn, new Date(), {
-            locale,
-          })}
-        </relative-time>
-      </div>
-    );
+    return <UpcomingCP cpRevealsOn={forecastAvailability.cpRevealsOn} />;
   }
 
   // TODO: implement view for group and MC questions
@@ -59,70 +43,15 @@ const ConsumerPredictionInfo: FC<Props> = ({ post, forecastAvailability }) => {
       );
       const successfullResolution = isSuccessfullyResolved(question.resolution);
       return (
-        <div className="flex min-w-[200px] max-w-[200px] justify-center">
-          <div
-            className={cn(
-              "flex w-fit flex-col items-center rounded bg-purple-100 px-4 py-2 dark:bg-purple-100-dark",
-              {
-                "bg-gray-300 dark:bg-gray-300-dark": !successfullResolution,
-              }
-            )}
-          >
-            {successfullResolution && (
-              <span className="text-xs font-medium uppercase leading-4 text-purple-600 dark:text-purple-600-dark">
-                {t("resolved")}
-              </span>
-            )}
-            <span
-              className={cn(
-                "text-base font-medium leading-6 text-purple-800 dark:text-purple-800-dark",
-                {
-                  "text-gray-700 dark:text-gray-700-dark":
-                    !successfullResolution,
-                }
-              )}
-            >
-              {formatedResolution}
-            </span>
-          </div>
-        </div>
+        <QuestionResolutionChip
+          formatedResolution={formatedResolution}
+          successfullResolution={successfullResolution}
+        />
       );
     }
 
     // Open/Closed
-    const isClosed = question.status === QuestionStatus.CLOSED;
-    const latest = question.aggregations.recency_weighted.latest;
-    const communityPredictionDisplayValue = latest
-      ? getDisplayValue({
-          value: latest.centers?.[0],
-          questionType: question.type,
-          scaling: question.scaling,
-        })
-      : null;
-
-    return (
-      <div className="flex min-w-[200px] max-w-[200px] justify-center">
-        <div
-          className={cn(
-            "flex w-fit flex-col items-center rounded border-2 border-blue-400 bg-transparent px-5 py-2 dark:border-blue-400-dark dark:bg-transparent",
-            {
-              "border-gray-300 dark:border-gray-300-dark": isClosed,
-            }
-          )}
-        >
-          <span
-            className={cn(
-              "text-lg font-bold leading-7 text-blue-700 dark:text-blue-700-dark",
-              {
-                "text-gray-600 dark:text-gray-600-dark": isClosed,
-              }
-            )}
-          >
-            {communityPredictionDisplayValue}
-          </span>
-        </div>
-      </div>
-    );
+    return <QuestionForecastChip question={question} />;
   }
   return null;
 };
