5cb9fedfab84608112f1977d6eee703e33786a0d
aseckin <atakanseckin@gmail.com>
2025-03-31 12:39:44 +0200

    Landing page updates for AIB Q2

--- front_end/src/app/(main)/aib/components/hero-q1.tsx
+++ front_end/src/app/(main)/aib/components/hero-q1.tsx
@@ -1,10 +1,10 @@
 import { useTranslations } from "next-intl";
 
-function Hero() {
+function HeroQ1() {
   const t = useTranslations();
 
   return (
-    <div className="flex w-full grow flex-col items-center justify-center gap-0 rounded bg-white p-4 dark:bg-blue-100-dark md:p-6 lg:gap-2 lg:p-8 min-[1920px]:gap-3 min-[1920px]:p-16">
+    <div className="flex w-full grow flex-col items-center justify-center gap-1 rounded bg-white p-4 dark:bg-blue-100-dark md:p-6 lg:gap-2 lg:p-8 min-[1920px]:gap-3 min-[1920px]:p-16">
       <h1 className="m-0 self-start text-balance text-left text-3xl font-bold leading-snug text-blue-600 dark:text-blue-600-dark md:text-3xl md:leading-tight lg:text-5xl lg:leading-tight min-[1920px]:text-6xl min-[1920px]:leading-normal">
         <span className="text-blue-800 dark:text-blue-800-dark">
           {t("FABHeroTitle")}
@@ -12,16 +12,15 @@ function Hero() {
         <span className="font-light text-blue-600 dark:text-blue-600-dark">
           {t("FABHeroSubtitle")}{" "}
           <span className="font-light text-blue-700 dark:text-blue-700-dark">
-            Q2{" "}
-            <span className="font-light opacity-50 md:font-thin">(2025)</span>
+            Q1 <span className="font-thin opacity-50">(2025)</span>
           </span>
         </span>
       </h1>
-      <p className="mb-0 mt-2 text-base font-normal leading-normal text-blue-600 dark:text-blue-600-dark sm:leading-normal md:mt-2 md:text-xl md:font-light md:leading-normal lg:text-2xl lg:leading-normal xl:leading-normal min-[1920px]:text-3xl min-[1920px]:leading-normal ">
+      <p className="mb-0 mt-2 text-lg font-light leading-tight text-blue-600 dark:text-blue-600-dark md:mt-2 md:text-xl md:leading-snug lg:text-2xl min-[1920px]:text-3xl min-[1920px]:leading-normal">
         {t("FABHeroDesc")}
       </p>
     </div>
   );
 }
 
-export default Hero;
+export default HeroQ1;
