c2971871a0ea222af12830986ec8dc65290e38b2
hlbmtc <hlib@metaculus.com>
2025-04-07 19:35:36 +0200

    Unified sentry config

--- front_end/sentry.server.config.ts
+++ front_end/sentry.server.config.ts
@@ -1,10 +1,3 @@
-import * as Sentry from "@sentry/nextjs";
+import { initSentry } from "@/utils/sentry";
 
-if (!!process.env.PUBLIC_FRONTEND_SENTRY_DSN) {
-  Sentry.init({
-    environment: process.env.METACULUS_ENV,
-    dsn: process.env.PUBLIC_FRONTEND_SENTRY_DSN,
-    tracesSampleRate: 0.1,
-    ignoreErrors: ["NEXT_REDIRECT", "NEXT_NOT_FOUND"],
-  });
-}
+initSentry();
---
+++ front_end/src/utils/sentry.ts
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/nextjs";
+
+import { getPublicSettings } from "@/utils/public_settings.server";
+
+const { PUBLIC_FRONTEND_SENTRY_DSN } = getPublicSettings();
+
+export function initSentry() {
+  if (!!PUBLIC_FRONTEND_SENTRY_DSN) {
+    Sentry.init({
+      environment: process.env.METACULUS_ENV,
+      dsn: PUBLIC_FRONTEND_SENTRY_DSN,
+      tracesSampleRate: 0.075,
+      integrations: [Sentry.replayIntegration()],
+      replaysSessionSampleRate: 0.075,
+      replaysOnErrorSampleRate: 1.0,
+      ignoreErrors: ["NEXT_REDIRECT", "NEXT_NOT_FOUND"],
+    });
+  }
+}
