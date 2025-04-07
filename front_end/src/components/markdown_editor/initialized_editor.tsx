c36d4c814b8da447c413f9fbdfd3b776cb428ce7
Nikita <nikita.develops.work@gmail.com>
2025-04-07 13:35:29 +0300

    fix(mentions): menu is overlapped with page content

--- front_end/src/components/markdown_editor/initialized_editor.tsx
+++ front_end/src/components/markdown_editor/initialized_editor.tsx
@@ -28,6 +28,7 @@ import {
   toolbarPlugin,
   UndoRedo,
 } from "@mdxeditor/editor";
+import { BeautifulMentionsTheme } from "lexical-beautiful-mentions";
 import React, {
   FC,
   ForwardedRef,
@@ -60,6 +61,11 @@ import { mentionsPlugin } from "./plugins/mentions";
 import { SourceModeTitle } from "./source_mode_title";
 
 type EditorMode = "write" | "read";
+
+const beautifulMentionsTheme: BeautifulMentionsTheme = {
+  "@": "block rounded",
+  "@Focused": "ring-2 ring-offset-1 ring-blue-500 dark:ring-blue-500-dark",
+};
 
 const jsxComponentDescriptors: JsxComponentDescriptor[] = [
   embeddedQuestionDescriptor,
@@ -265,6 +271,9 @@ const InitializedMarkdownEditor: FC<
         ...(editorDiffSourcePlugin ? [editorDiffSourcePlugin] : []),
         ...(editorToolbarPlugin ? [editorToolbarPlugin] : []),
       ]}
+      lexicalTheme={{
+        beautifulMentions: beautifulMentionsTheme,
+      }}
     />
   );
 };
