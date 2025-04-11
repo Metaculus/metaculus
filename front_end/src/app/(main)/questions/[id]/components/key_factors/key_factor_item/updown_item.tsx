f85b7f8b4631cd37ac323b3c97522767d86792fc
Nikita <nikita.develops.work@gmail.com>
2025-04-11 19:21:08 +0300

    WIP: 2 step survey layout

--- front_end/src/app/(main)/questions/[id]/components/key_factors/key_factor_item/updown_item.tsx
+++ front_end/src/app/(main)/questions/[id]/components/key_factors/key_factor_item/updown_item.tsx
@@ -17,7 +17,7 @@ type Props = {
 };
 
 export const UpdownKeyFactorItem: FC<Props> = ({
-  keyFactor: { text, id, votes_score, user_vote },
+  keyFactor: { text, id, votes_score, user_votes },
   linkToComment = true,
   linkAnchor,
 }) => {
@@ -26,7 +26,7 @@ export const UpdownKeyFactorItem: FC<Props> = ({
   return (
     <div
       className={cn(
-        "relative flex items-center gap-3 rounded border border-transparent bg-blue-200 p-3 hover:border-blue-500 dark:bg-blue-200-dark dark:hover:border-blue-500-dark [&>.target]:hover:visible",
+        "relative flex flex-col items-center justify-center gap-3 rounded border border-transparent bg-blue-200 p-3 hover:border-blue-500 dark:bg-blue-200-dark dark:hover:border-blue-500-dark xs:flex-row [&>.target]:hover:visible",
         { "bg-gray-0 dark:bg-gray-0-dark": linkToComment }
       )}
     >
@@ -36,7 +36,9 @@ export const UpdownKeyFactorItem: FC<Props> = ({
         voteData={{
           keyFactorId: id,
           votesScore: votes_score,
-          userVote: user_vote ?? null,
+          userVote:
+            user_votes.findLast((vote) => vote.vote_type === "a_updown") ??
+            null,
         }}
       />
       <div className="decoration-blue-500 underline-offset-4 dark:decoration-blue-500-dark">
@@ -55,7 +57,7 @@ export const UpdownKeyFactorItem: FC<Props> = ({
           }
           sendGAEvent("event", "KeyFactorClick", { event_label: "fromList" });
         }}
-        className="target invisible ml-auto text-blue-600 dark:text-blue-600"
+        className=" target absolute right-2 top-2 ml-0 mr-auto flex items-center rounded-full p-1 text-blue-600 hover:bg-blue-400 hover:text-blue-700 dark:text-blue-600 dark:hover:bg-blue-400-dark xs:invisible xs:relative xs:right-0 xs:top-0"
       >
         <FontAwesomeIcon
           icon={faArrowUpRightFromSquare}
