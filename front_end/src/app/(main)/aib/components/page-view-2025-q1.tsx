4cb5e16ea64009dee9747459395c82d45fd721e6
aseckin <atakanseckin@gmail.com>
2025-05-01 08:09:46 +0200

    Update links in between AIB pages

--- front_end/src/app/(main)/aib/components/page-view-2025-q1.tsx
+++ front_end/src/app/(main)/aib/components/page-view-2025-q1.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,15 +18,15 @@ const AiBenchmarkingTournamentPage: FC<{ token: string | null }> = () => {
 
   return (
     <div className="mx-auto h-auto w-full flex-auto items-stretch bg-gradient-to-tl from-blue-300/30 via-blue-100/30 to-blue-400/30 px-4 py-4 text-blue-700 dark:bg-blue-800 dark:from-blue-600/50 dark:via-blue-800/30 dark:to-blue-500/30 dark:text-blue-700-dark">
-      <Link href="/aib" className="mb-3 flex w-full no-underline">
-        <div className="flex w-full cursor-pointer flex-col items-center justify-center gap-2 text-balance rounded-md bg-purple-200 p-3 text-center text-base text-purple-800 transition-colors hover:bg-purple-300 dark:bg-purple-200-dark dark:text-purple-800-dark dark:hover:bg-purple-300-dark md:flex-row md:gap-4 md:p-6 md:text-xl">
-          2025&apos;s Q2 tournament is underway!{" "}
+      <Link href="/aib/2024/q4" className="mb-3 flex w-full no-underline">
+        <div className="flex flex w-full cursor-pointer flex-col items-center items-center justify-center justify-center gap-2 text-balance rounded-md bg-purple-200 p-3 text-center text-base text-purple-800 transition-colors hover:bg-purple-300 dark:bg-purple-200-dark dark:text-purple-800-dark dark:hover:bg-purple-300-dark md:flex-row md:gap-4 md:p-6 md:text-xl">
           <FontAwesomeIcon
-            icon={faArrowRight}
+            icon={faArrowLeft}
             className="text-lg md:text-lg min-[1920px]:text-xl"
-          />
+          />{" "}
+          Check out final rankings for Q4 - 2024
         </div>
-      </Link>
+      </Link>{" "}
       <div className="flex size-full flex-col items-center gap-3">
         <div className="flex w-full flex-col gap-3 md:flex-row">
           <div className="flex w-full flex-col gap-3 md:w-1/3">
