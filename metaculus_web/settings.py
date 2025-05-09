d421910787d514e3771a2565cda7eb01c1ae45ff
Hlib <hlib@metaculus.com>
2025-05-09 17:53:27 +0100

    Auth middleware tweaks (#2731)
    * - Added `FallbackTokenAuthentication` auth method to use optional `Token` prefix
    - Added `authenticate_request` util for middlewares
    - Made `PUBLIC_AUTHENTICATION_REQUIRED=true` error pages to render Browsable API pages
    
    * Small fix
    
    * Small fix

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -159,7 +159,7 @@ if not IS_TEST_ENV:
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "authentication.auth.FallbackTokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
