37dc56a1040f88c33ad4d29e7e32a11ec9a07dfb
Vasile Popescu <elisescu@elisescu.com>
2025-05-13 09:42:21 +0200

    Fix translations issue with empty strings

--- comments/translation.py
+++ comments/translation.py
@@ -1,14 +1,18 @@
 from modeltranslation.translator import TranslationOptions, translator
 
+from metaculus_web.settings import TRANSLATIONS_FALLBACK_UNDEFINED
+
 from .models import Comment, KeyFactor
 
 
 class CommentTranslationOptions(TranslationOptions):
     fields = ("text",)
+    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 class KeyFactorTranslationOptions(TranslationOptions):
     fields = ("text",)
+    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 translator.register(Comment, CommentTranslationOptions)
--- questions/translation.py
+++ questions/translation.py
@@ -1,13 +1,16 @@
 from modeltranslation.translator import TranslationOptions, translator
 from .models import Question, GroupOfQuestions
+from metaculus_web.settings import TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 class QuestionTranslationOptions(TranslationOptions):
     fields = ("description", "resolution_criteria", "fine_print", "label", "title")
+    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 class GroupOfQuestionsTranslationOptions(TranslationOptions):
     fields = ("description", "resolution_criteria", "fine_print")
+    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 translator.register(Question, QuestionTranslationOptions)
