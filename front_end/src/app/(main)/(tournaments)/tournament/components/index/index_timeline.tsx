a4946249fd46daa54125feb3eab5b23971bde6d4
Nikita <93587872+ncarazon@users.noreply.github.com>
2025-05-26 12:50:52 +0300

    feat(charts): new timeline UI for binary question chart (#2751)

--- front_end/src/app/(main)/(tournaments)/tournament/components/index/index_timeline.tsx
+++ front_end/src/app/(main)/(tournaments)/tournament/components/index/index_timeline.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useTranslations } from "next-intl";
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { DomainTuple } from "victory";
 
 import NewNumericChart from "@/components/charts/new_numeric_chart";
+import { METAC_COLORS } from "@/constants/colors";
 import { Area, Line } from "@/types/charts";
 import { TimelineChartZoomOption } from "@/types/charts";
 import { Tournament } from "@/types/projects";
@@ -21,15 +22,19 @@ type Props = {
 
 const IndexTimeline: FC<Props> = ({ tournament, height = 170 }) => {
   const t = useTranslations();
+  const buildChartData = useCallback(
+    (width: number, zoom: TimelineChartZoomOption) =>
+      buildIndexChartData({ tournament, width, zoom }),
+    [tournament]
+  );
 
   return (
     <NewNumericChart
-      buildChartData={(width, zoom) =>
-        buildIndexChartData({ tournament, width, zoom })
-      }
+      buildChartData={buildChartData}
+      withZoomPicker={true}
       height={height}
       chartTitle={t("indexTimeline")}
-      tickFontSize={10}
+      colorOverride={METAC_COLORS.blue["600"]}
     />
   );
 };
