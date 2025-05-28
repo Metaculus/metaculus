7c7e31eb54ab1980bde62e3661f763bb0f348141
Nikita <93587872+ncarazon@users.noreply.github.com>
2025-05-28 19:10:21 +0300

    Download Data Modal (#2464)
    feat(question): download question data modal
    set up back end email data task and view
    
    Co-authored-by: lsabor <lukesabor@gmail.com>

--- front_end/src/types/utils.ts
+++ front_end/src/types/utils.ts
@@ -3,3 +3,16 @@ export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
+export type DataParams = {
+  post_id?: number;
+  question_id?: number;
+  sub_question?: number;
+  aggregation_methods?: string[];
+  minimize?: boolean;
+  user_ids?: number[];
+  include_comments?: boolean;
+  include_scores?: boolean;
+  include_bots?: boolean;
+  include_user_data?: boolean;
+  anonymized?: boolean;
+};
