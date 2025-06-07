cde60d6d56e9ada51f04d158f6acca30d003413b
lsabor <lukesabor@gmail.com>
2025-06-07 10:30:20 -0700

    add additional aggregations

--- questions/types.py
+++ questions/types.py
@@ -6,3 +6,13 @@ class AggregationMethod(models.TextChoices):
     UNWEIGHTED = "unweighted"
     SINGLE_AGGREGATION = "single_aggregation"
     METACULUS_PREDICTION = "metaculus_prediction"
+    MEDALISTS = "medalists"
+    EXPERIENCED_USERS_25_RESOLVED = "experienced_users_25_resolved"
+    IGNORANCE = "ignorance"
+    RECENCY_WEIGHTED_LOG_ODDS = "recency_weighted_log_odds"
+    RECENCY_WEIGHTED_MEAN_NO_OUTLIERS = "recency_weighted_mean_no_outliers"
+    RECENCY_WEIGHTED_MEDALISTS = "recency_weighted_medalists"
+    RECENCY_WEIGHTED_EXPERIENCED_USERS_25_RESOLVED = (
+        "recency_weighted_experienced_users_25_resolved"
+    )
+    RECENCY_WEIGHTED_LOG_ODDS_NO_OUTLIERS = "recency_weighted_log_odds_no_outliers"
