af0d1ba5741d327d89aed81e1862e35836881741
Vasile Popescu <elisescu@elisescu.com>
2025-06-10 13:31:47 +0200

    Update the copy from expiration to withdrawal

--- front_end/src/components/forecast_maker/forecast_maker_question/forecast_maker_binary.tsx
+++ front_end/src/components/forecast_maker/forecast_maker_question/forecast_maker_binary.tsx
@@ -220,10 +220,10 @@ const ForecastMakerBinary: FC<Props> = ({
               )}
             >
               {previousForecastExpiration.isExpired
-                ? t("predictionExpiredText", {
+                ? t("predictionWithdrawnText", {
                     time: previousForecastExpiration.string,
                   })
-                : t("predictionWillExpireInText", {
+                : t("predictionWillBeWithdrawInText", {
                     time: previousForecastExpiration.string,
                   })}
             </span>
