a51147fb056051448cefefe558533d33a0e05e3a
Nikita <nikita.develops.work@gmail.com>
2025-06-30 14:11:37 +0300

    fix(logs): remove logs for debugging

--- front_end/src/app/(main)/questions/components/project_picker_input.tsx
+++ front_end/src/app/(main)/questions/components/project_picker_input.tsx
@@ -57,8 +57,7 @@ const ProjectPickerInput: React.FC<{
       )
     );
   }, [query]);
-  console.log("initialProjects", initialProjects);
-  console.log("selectedProject", selectedProject);
+
   // Show selector only if user has at least 2 projects
   // With admin/curator permissions
   if (initialProjects.length < 2) {
@@ -70,11 +69,8 @@ const ProjectPickerInput: React.FC<{
       <Combobox
         immediate
         onChange={(project: TournamentPreview) => {
-          console.log("===START OF THE onChange HANDLER===");
-          console.log("project", project);
           onChange(project);
           setSelectedProject(project);
-          console.log("===END OF THE onChange HANDLER===");
         }}
       >
         <div className="relative mt-1">
@@ -119,15 +115,12 @@ const ProjectPickerInput: React.FC<{
                     }
                     value={project}
                     onClick={() => {
-                      console.log("===START OF THE onCick HANDLER===");
-                      console.log("project", project);
                       setQuery("");
                       setFilteredProjects(initialProjects);
                       // we need timeout to blur the combobox after the selection is made
                       setTimeout(() => {
                         (document.activeElement as HTMLElement)?.blur();
                       }, 0);
-                      console.log("===END OF THE onCick HANDLER===");
                     }}
                   >
                     {({}) => (
