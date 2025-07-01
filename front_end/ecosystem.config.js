ab2939d5efb220d43b4476cfa90cf597a3bfbbee
hlbmtc <hlib@metaculus.com>
2025-07-01 17:45:43 +0200

    override port

--- front_end/ecosystem.config.js
+++ front_end/ecosystem.config.js
@@ -8,6 +8,9 @@ module.exports = {
       exec_mode: "cluster",
       interpreter_args: `--max-old-space-size=${process.env.NODE_HEAP_SIZE ?? 1024}`,
       max_memory_restart: "2280M",
+      env: {
+        PORT: 3000,
+      },
     },
   ],
 };
