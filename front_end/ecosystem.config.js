e5a91f00e184f2d796bf19ac52e5438403b00e95
hlbmtc <hlib@metaculus.com>
2025-07-01 17:24:25 +0200

    Adjusted PM2 config

---
+++ front_end/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "./",
+      instances: process.env.NODE_INSTANCES ?? 1,
+      exec_mode: "cluster",
+      interpreter_args: `--max-old-space-size=${process.env.NODE_HEAP_SIZE ?? 1024}`,
+      max_memory_restart: "2280M",
+    },
+  ],
+};
