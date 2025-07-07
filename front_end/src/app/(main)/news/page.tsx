939c44bcaaa55c6462cccf593c00a7f6e3662356
Vasile Popescu <elisescu@elisescu.com>
2025-07-07 16:26:23 +0200

    Several changes to poke at the issue

--- front_end/src/app/(main)/news/page.tsx
+++ front_end/src/app/(main)/news/page.tsx
@@ -21,48 +21,17 @@ export const metadata = {
 export default async function NewsFeed(props: {
   searchParams: Promise<SearchParams>;
 }) {
-  const searchParams = await props.searchParams;
-  const t = await getTranslations();
-  const newsCategories = await ServerProjectsApi.getNewsCategories();
-  const filters = {
-    [POST_NEWS_TYPE_FILTER]: newsCategories.map((obj) => obj.slug),
-    ...generateFiltersFromSearchParams(searchParams),
-    curation_status: "approved",
-    order_by: QuestionOrder.PublishTimeDesc,
-  };
+  return <div>Empty News Feed, no Suspense</div>;
 
-  const newsCategoryId = filters["news_type"]
-    ? newsCategories.find((obj) => obj.slug === filters["news_type"])?.id
-    : null;
-
+  // If I comment this below, the page loads fine.
   return (
-    <main className="mx-auto mb-auto w-full max-w-3xl px-2 pb-4">
-      <h1 className="mb-6 mt-12 text-center text-5xl font-bold text-blue-800 dark:text-blue-800-dark">
-        Metaculus{" "}
-        <span className="text-blue-700 dark:text-blue-700-dark">News</span>
-      </h1>
-      <NewsFilters categories={newsCategories} />
-      {newsCategoryId && (
-        <div className="flex w-full flex-col items-center">
-          <Button
-            variant="tertiary"
-            size="md"
-            href={`/questions/create/notebook?news_category_id=${newsCategoryId}`}
-          >
-            {t("createArticle")}
-          </Button>
-        </div>
-      )}
-      <div className="min-h-[calc(100vh-300px)] grow overflow-x-hidden p-2 pt-2.5 no-scrollbar sm:p-0 sm:pt-5">
-        <Suspense
-          key={JSON.stringify(searchParams)}
-          fallback={
-            <LoadingIndicator className="mx-auto h-8 w-24 text-gray-600 dark:text-gray-600-dark" />
-          }
-        >
-          <AwaitedPostsFeed filters={filters} type="news" />
-        </Suspense>
-      </div>
-    </main>
+    <Suspense
+      key="news-feed"
+      fallback={
+        <LoadingIndicator className="mx-auto h-8 w-24 text-gray-600 dark:text-gray-600-dark" />
+      }
+    >
+      <AwaitedPostsFeed filters={{}} type="news" />
+    </Suspense>
   );
 }
