43caa14ac4ae5792860a27d95a26d750ce8df019
Alex Costea <alex.costea@metaculus.com>
2025-07-14 16:02:29 -0400

    DisplayCoherenceLink display question title

--- front_end/src/app/(main)/questions/components/coherence_links/display_coherence_link.tsx
+++ front_end/src/app/(main)/questions/components/coherence_links/display_coherence_link.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import Button from "@/components/ui/button";
+import ClientPostsApi from "@/services/api/posts/posts.client";
 import { CoherenceLink } from "@/types/coherence";
 import { Post } from "@/types/post";
+import { Question } from "@/types/question";
 
 type Props = {
   link: CoherenceLink;
@@ -12,6 +14,16 @@ type Props = {
 export const DisplayCoherenceLink: FC<Props> = ({ link, post }) => {
   const isFirstQuestion = link.question1 === post.question?.id;
   const otherQuestionID = isFirstQuestion ? link.question2 : link.question1;
+  const [otherQuestion, setOtherQuestion] = useState<Question | null>(null);
+
+  useEffect(() => {
+    ClientPostsApi.getQuestion(otherQuestionID).then((question) =>
+      setOtherQuestion(question)
+    );
+  }, [otherQuestionID]);
+
+  if (!otherQuestion) return null;
+
   return (
     <>
       <div>
@@ -20,7 +32,7 @@ export const DisplayCoherenceLink: FC<Props> = ({ link, post }) => {
             This question has a {link.strength} {link.direction} {link.type}{" "}
             impact on{" "}
             <Link href={`/questions/${otherQuestionID}`} target="_blank">
-              <b>{otherQuestionID}</b>
+              <b>{otherQuestion.title}</b>
             </Link>
             .
           </div>
@@ -28,7 +40,7 @@ export const DisplayCoherenceLink: FC<Props> = ({ link, post }) => {
           <div>
             The question{" "}
             <Link href={`/questions/${otherQuestionID}`} target="_blank">
-              <b>{otherQuestionID}</b>
+              <b>{otherQuestion.title}</b>
             </Link>{" "}
             has a {link.strength} {link.direction} {link.type} impact on this
             question.
