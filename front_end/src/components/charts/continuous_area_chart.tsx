237157a9ec20dace4bad6b625c882b5ec99900cc
Luke Sabor <32885230+lsabor@users.noreply.github.com>
2025-07-19 15:29:44 -0700

    rework tickmarks (#2934)
    overhaul to tickmark selection

--- front_end/src/components/charts/continuous_area_chart.tsx
+++ front_end/src/components/charts/continuous_area_chart.tsx
@@ -602,9 +602,12 @@ const ContinuousAreaChart: FC<Props> = ({
               tickLabels: {
                 textAnchor: ({ index, ticks }) =>
                   // We want first and last labels be aligned against area boundaries
-                  index === 0
+                  // except for discrete questions, whose first and last ticks are not
+                  // at the edges of the chart
+                  index === 0 && question.type !== QuestionType.Discrete
                     ? "start"
-                    : index === ticks.length - 1
+                    : index === ticks.length - 1 &&
+                        question.type !== QuestionType.Discrete
                       ? "end"
                       : "middle",
               },
