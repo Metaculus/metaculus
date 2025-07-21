4be994c9f226e676f1557d1a816cba0f5892edea
Nikita <nikita.develops.work@gmail.com>
2025-07-21 21:36:56 +0300

    feat(consumer-view): continuous distribution chart updates

--- front_end/src/components/charts/numeric_chart.tsx
+++ front_end/src/components/charts/numeric_chart.tsx
@@ -57,6 +57,7 @@ import {
 import cn from "@/utils/core/cn";
 
 import ForecastAvailabilityChartOverflow from "../post_card/chart_overflow";
+import ChartValueBox from "./primitives/chart_value_box";
 
 type ChartData = {
   line: Line;
@@ -552,7 +553,7 @@ const NumericChart: FC<Props> = ({
                           isEmbedded={isEmbedded}
                         />
                       ) : (
-                        <CursorValue
+                        <ChartValueBox
                           isCursorActive={
                             shouldAdjustCursorLabel || isCursorActive
                           }
@@ -585,86 +586,6 @@ const NumericChart: FC<Props> = ({
         </FloatingPortal>
       )}
     </>
-  );
-};
-
-const CursorValue: FC<{
-  x?: number;
-  y?: number;
-  datum?: any;
-  isCursorActive: boolean;
-  chartWidth: number;
-  rightPadding: number;
-  colorOverride?: ThemeColor;
-  getCursorValue?: (value: number) => string;
-  resolution?: Resolution | null;
-}> = (props) => {
-  const { getThemeColor } = useAppTheme();
-  const {
-    x,
-    y,
-    datum,
-    isCursorActive,
-    chartWidth,
-    rightPadding,
-    colorOverride,
-    getCursorValue,
-    resolution,
-  } = props;
-  const TEXT_PADDING = 4;
-  const CHIP_OFFSET = !isNil(resolution) ? 8 : 0;
-
-  const [textWidth, setTextWidth] = useState(0);
-  const textRef = useRef<SVGTextElement>(null);
-  useEffect(() => {
-    if (textRef.current) {
-      setTextWidth(textRef.current.getBBox().width + TEXT_PADDING);
-    }
-  }, [datum?.y]);
-
-  if (isNil(x) || isNil(y)) {
-    return null;
-  }
-
-  const adjustedX = isCursorActive
-    ? x
-    : chartWidth - rightPadding + textWidth / 2 + CHIP_OFFSET;
-  const chipHeight = 16;
-  const chipFontSize = 12;
-
-  return (
-    <g>
-      <rect
-        x={adjustedX - textWidth / 2}
-        y={y - chipHeight / 2}
-        width={textWidth}
-        height={chipHeight}
-        fill={
-          isNil(colorOverride)
-            ? getThemeColor(METAC_COLORS.olive["600"])
-            : getThemeColor(colorOverride)
-        }
-        stroke="transparent"
-        rx={2}
-        ry={2}
-      />
-      <text
-        ref={textRef}
-        x={adjustedX}
-        y={y + chipFontSize / 10} // fix vertical alignment
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fill={getThemeColor(METAC_COLORS.gray["0"])}
-        fontWeight="bold"
-        fontSize={chipFontSize}
-      >
-        {!!resolution && !isCursorActive
-          ? resolution
-          : getCursorValue
-            ? getCursorValue(datum.y)
-            : datum.y.toFixed(1)}
-      </text>
-    </g>
   );
 };
 
---
+++ front_end/src/components/charts/primitives/chart_value_box.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { METAC_COLORS } from "@/constants/colors";
+import useAppTheme from "@/hooks/use_app_theme";
+import { Resolution } from "@/types/post";
+import { ThemeColor } from "@/types/theme";
+import { isNil } from "lodash";
+import { FC, useEffect, useRef, useState } from "react";
+
+const ChartValueBox: FC<{
+  x?: number | null;
+  y?: number | null;
+  datum?: any;
+  isCursorActive: boolean;
+  chartWidth: number;
+  rightPadding: number;
+  colorOverride?: ThemeColor;
+  getCursorValue?: (value: number) => string;
+  resolution?: Resolution | null;
+  isDistributionChip?: boolean;
+}> = (props) => {
+  const { getThemeColor } = useAppTheme();
+  const {
+    x,
+    y,
+    datum,
+    isCursorActive,
+    chartWidth,
+    rightPadding,
+    colorOverride,
+    getCursorValue,
+    resolution,
+    isDistributionChip,
+  } = props;
+  const TEXT_PADDING = 4;
+  const CHIP_OFFSET = !isNil(resolution) ? 8 : 0;
+
+  const [textWidth, setTextWidth] = useState(0);
+  const textRef = useRef<SVGTextElement>(null);
+  useEffect(() => {
+    if (textRef.current) {
+      setTextWidth(textRef.current.getBBox().width + TEXT_PADDING);
+    }
+  }, [datum?.y]);
+
+  if (isNil(x) || isNil(y)) {
+    return null;
+  }
+
+  const adjustedX =
+    isCursorActive || isDistributionChip
+      ? x
+      : chartWidth - rightPadding + textWidth / 2 + CHIP_OFFSET;
+  const chipHeight = 16;
+  const chipFontSize = 12;
+
+  return (
+    <g>
+      <rect
+        x={adjustedX - textWidth / 2}
+        y={y - chipHeight / 2 - (isDistributionChip ? chipHeight : 0)}
+        width={textWidth}
+        height={chipHeight}
+        fill={
+          isNil(colorOverride)
+            ? getThemeColor(METAC_COLORS.olive["600"])
+            : getThemeColor(colorOverride)
+        }
+        stroke="transparent"
+        rx={2}
+        ry={2}
+      />
+      <text
+        ref={textRef}
+        x={adjustedX}
+        y={y + chipFontSize / 10 - (isDistributionChip ? chipHeight : 0)} // fix vertical alignment
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fill={getThemeColor(METAC_COLORS.gray["0"])}
+        fontWeight="bold"
+        fontSize={chipFontSize}
+      >
+        {!!resolution && !isCursorActive
+          ? resolution
+          : getCursorValue
+            ? getCursorValue(datum.y)
+            : datum.y.toFixed(1)}
+      </text>
+    </g>
+  );
+};
+
+export default ChartValueBox;
