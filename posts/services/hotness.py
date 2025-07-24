6e923dc97e008cb9489e5cf5f3791f3e591b1af4
Luke Sabor <32885230+lsabor@users.noreply.github.com>
2025-07-23 19:10:54 -0700

    Revert "Tweaks to "In The News" hotness feed (#3165)"
    This reverts commit 29ccfdacc00440846358ea30db6dd15bb46d566d.

--- posts/services/hotness.py
+++ posts/services/hotness.py
@@ -60,15 +60,13 @@ def _compute_hotness_approval_score(post: Post) -> float:
 
 
 def _compute_hotness_relevant_news(post: Post) -> float:
-    qs = PostArticle.objects.filter(post=post)
+    related_article = PostArticle.objects.filter(post=post).order_by("distance").first()
 
-    return sum(
-        [
-            decay(
-                20 * max(0, 0.5 - related_article.distance), related_article.created_at
-            )
-            for related_article in qs
-        ]
+    if not related_article:
+        return 0
+
+    return decay(
+        20 * max(0, 0.5 - related_article.distance), related_article.created_at
     )
 
 
