914b0c6d4057e7262379a63c9d7eba3f874a5c46
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-07-29 23:28:51 -0600

    Changed description fields for aib page

--- front_end/src/app/(main)/aib/components/description.tsx
+++ front_end/src/app/(main)/aib/components/description.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 function Description() {
@@ -9,7 +10,11 @@ function Description() {
       <ul className="w-full list-none space-y-2 divide-y divide-blue-400 text-sm font-normal leading-relaxed text-blue-700 dark:divide-blue-400-dark/40 dark:text-blue-700-dark md:space-y-3 md:text-base md:text-lg md:font-light lg:text-lg lg:leading-normal xl:text-xl min-[1920px]:space-y-4 min-[1920px]:text-3xl">
         <li>{t.rich("FABdesc1", { bold })}</li>
         <li className="pt-3 min-[1920px]:pt-5">
-          {t.rich("FABdesc2", { bold })}
+          {t.rich("FABdesc2", {
+            link: (chunks) => (
+              <Link href="https://www.loom.com/share/fc3c1a643b984a15b510647d8f760685" target="_blank">{chunks}</Link>
+            ),
+          })}
         </li>
         <li className="pt-3 min-[1920px]:pt-5">{t("FABdesc3")}</li>
         <li className="pt-3 min-[1920px]:pt-5">
