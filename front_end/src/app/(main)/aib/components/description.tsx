cae76b85a3930e5a16e4fc86b6fb7c23afe18e0e
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-07-30 00:10:54 -0600

    Reordered some imports

--- front_end/src/app/(main)/aib/components/description.tsx
+++ front_end/src/app/(main)/aib/components/description.tsx
@@ -1,5 +1,5 @@
-import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 import { ReactNode } from "react";
 
 function Description() {
