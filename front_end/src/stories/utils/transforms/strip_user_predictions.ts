0924c78ac848a9cddb9da76accee68d412c8e7b9
Nikita <nikita.develops.work@gmail.com>
2025-07-30 14:51:04 +0300

    fix: return community aggregations

--- front_end/src/stories/utils/transforms/strip_user_predictions.ts
+++ front_end/src/stories/utils/transforms/strip_user_predictions.ts
@@ -10,12 +10,7 @@ export const stripUserPredictions = (
         ...post.group_of_questions,
         questions: post.group_of_questions.questions.map((q) => ({
           ...q,
-          id: q.id ?? 0,
           my_forecasts: { history: [] },
-          aggregations: {
-            ...q.aggregations,
-            recency_weighted: { history: [] },
-          },
         })),
       },
     };
@@ -27,10 +22,6 @@ export const stripUserPredictions = (
       question: {
         ...post.question,
         my_forecasts: { history: [] },
-        aggregations: {
-          ...post.question.aggregations,
-          recency_weighted: { history: [] },
-        },
       },
     };
   }
