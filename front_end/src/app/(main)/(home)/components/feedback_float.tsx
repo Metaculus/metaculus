a516378fa56973d195db445b7086204469d1174e
Sylvain <74110469+SylvainChevalier@users.noreply.github.com>
2025-07-31 14:34:36 +0200

    paper airplane now links to contact us page (#3207)

--- front_end/src/app/(main)/(home)/components/feedback_float.tsx
+++ front_end/src/app/(main)/(home)/components/feedback_float.tsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FeedbackFloat: React.FC = () => (
   <a
-    href="https://github.com/metaculus/metaculus"
+    href="/services/#contact-us"
     target="_blank"
     rel="noopener noreferrer"
     className="fixed bottom-4 right-4 z-100 flex h-8 w-8 items-center justify-center rounded-full border border-transparent bg-blue-800 p-2 text-base text-white no-underline shadow-lg transition-colors hover:bg-blue-900 hover:text-blue-900 active:text-blue-700 disabled:text-blue-800 disabled:opacity-30 dark:text-blue-800-dark dark:hover:text-blue-900-dark dark:active:text-blue-700-dark disabled:dark:text-blue-800-dark"
-    aria-label="View on GitHub"
+    aria-label="Get in touch"
   >
     <FontAwesomeIcon icon={faPaperPlane} />
   </a>
