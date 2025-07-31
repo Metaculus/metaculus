0dcd44f29a5b5191d3a2f7ef2f6d44e5d7eab508
Nikita <nikita.develops.work@gmail.com>
2025-07-31 19:22:13 +0300

    feat: conditionaly hide timeline

--- front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/timeline/index.tsx
+++ front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/timeline/index.tsx
@@ -1,6 +1,6 @@
 import DetailedGroupCard from "@/components/detailed_question_card/detailed_group_card";
 import DetailedQuestionCard from "@/components/detailed_question_card/detailed_question_card";
-import { PostWithForecasts } from "@/types/post";
+import { PostWithForecasts, QuestionStatus } from "@/types/post";
 import {
   isGroupOfQuestionsPost,
   isQuestionPost,
@@ -11,15 +11,22 @@ type Props = {
 };
 
 const QuestionTimeline: React.FC<Props> = ({ postData }) => {
-  if (isQuestionPost(postData)) {
+  if (
+    isQuestionPost(postData) &&
+    postData.question.status !== QuestionStatus.UPCOMING
+  ) {
     return (
-      <div className="mt-8">
+      <div className="mt-8 hidden sm:block">
         <DetailedQuestionCard post={postData} />
       </div>
     );
   }
   if (isGroupOfQuestionsPost(postData)) {
-    return <DetailedGroupCard post={postData} />;
+    return (
+      <div className="mt-8 hidden sm:block">
+        <DetailedGroupCard post={postData} />
+      </div>
+    );
   }
   return null;
 };
