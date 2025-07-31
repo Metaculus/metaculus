5883c8b809d6a2ca266b7206fabae7f8efccfaf4
Nikita <nikita.develops.work@gmail.com>
2025-07-31 19:22:34 +0300

    feat: integrate continuous question prediction

--- front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/prediction/single_question_prediction/index.tsx
+++ front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/prediction/single_question_prediction/index.tsx
@@ -1,6 +1,7 @@
 import { QuestionType, QuestionWithForecasts } from "@/types/question";
 
 import BinaryQuestionPrediction from "./binary_question_prediction";
+import ContinuousQuestionPrediction from "./continuous_question_prediction";
 
 type Props = {
   question: QuestionWithForecasts;
@@ -16,6 +17,10 @@ const SingleQuestionPrediction: React.FC<Props> = ({
       return (
         <BinaryQuestionPrediction question={question} canPredict={canPredict} />
       );
+    case QuestionType.Numeric:
+    case QuestionType.Discrete:
+    case QuestionType.Date:
+      return <ContinuousQuestionPrediction question={question} />;
     default:
       return null;
   }
