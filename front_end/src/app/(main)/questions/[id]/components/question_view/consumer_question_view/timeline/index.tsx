a8ec51619db89ae869e5243be37d332021d8cde3
Nikita <nikita.develops.work@gmail.com>
2025-07-31 19:22:08 +0300

    chore: timeline for group questions

--- front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/timeline/index.tsx
+++ front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/timeline/index.tsx
@@ -1,6 +1,10 @@
+import DetailedGroupCard from "@/components/detailed_question_card/detailed_group_card";
 import DetailedQuestionCard from "@/components/detailed_question_card/detailed_question_card";
 import { PostWithForecasts } from "@/types/post";
-import { isQuestionPost } from "@/utils/questions/helpers";
+import {
+  isGroupOfQuestionsPost,
+  isQuestionPost,
+} from "@/utils/questions/helpers";
 
 type Props = {
   postData: PostWithForecasts;
@@ -13,6 +17,9 @@ const QuestionTimeline: React.FC<Props> = ({ postData }) => {
         <DetailedQuestionCard post={postData} />
       </div>
     );
+  }
+  if (isGroupOfQuestionsPost(postData)) {
+    return <DetailedGroupCard post={postData} />;
   }
   return null;
 };
