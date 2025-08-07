e5f711d1ec60a4dbe24a10b4949606aefb4dddf7
Alex Costea <alex.costea@metaculus.com>
2025-08-07 10:24:47 -0400

    only display resolved questions in PredictionSuccessLinks

--- front_end/src/components/forecast_maker/forecast_maker_question/prediction_success_links.tsx
+++ front_end/src/components/forecast_maker/forecast_maker_question/prediction_success_links.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import useCoherenceLinksContext from "@/app/(main)/components/coherence_links_provider";
 import { DisplayCoherenceLink } from "@/app/(main)/questions/components/coherence_links/display_coherence_link";
-import { PostWithForecasts } from "@/types/post";
+import ClientPostsApi from "@/services/api/posts/posts.client";
+import { CoherenceLink } from "@/types/coherence";
+import { PostWithForecasts, QuestionStatus } from "@/types/post";
+import { Question } from "@/types/question";
 
 interface Props {
   post: PostWithForecasts;
@@ -14,13 +17,44 @@ interface Props {
 export const PredictionSuccessLinks: FC<Props> = ({ post }) => {
   const { coherenceLinks, updateCoherenceLinks } = useCoherenceLinksContext();
   const t = useTranslations();
+  const [coherenceLinksData, setCoherenceLinksData] = useState<CoherenceLink[]>(
+    []
+  );
 
-  if (!coherenceLinks || coherenceLinks.size === 0) return null;
+  const questionID = post?.question?.id;
+
+  useEffect(() => {
+    const getOtherQuestions = async () => {
+      const questionData = new Map<number, Question>();
+      for (const link of coherenceLinks.data) {
+        const otherQuestionId =
+          questionID == link.question1_id
+            ? link.question2_id
+            : link.question1_id;
+        const otherQuestion = await ClientPostsApi.getQuestion(otherQuestionId);
+        questionData.set(link.id, otherQuestion);
+      }
+      return questionData;
+    };
+
+    const retrieveCoherenceLinks = async () => {
+      const otherQuestions = await getOtherQuestions();
+      setCoherenceLinksData(
+        coherenceLinks.data.filter(
+          (it) => otherQuestions.get(it.id)?.status == QuestionStatus.OPEN
+        )
+      );
+    };
+
+    void retrieveCoherenceLinks();
+  }, [coherenceLinks, questionID]);
+
+  if (coherenceLinksData.length === 0) return null;
 
   return (
     <div>
       {t("updateLinksRequest")}
-      {Array.from(coherenceLinks?.data ?? [], (link) => (
+      {Array.from(coherenceLinksData, (link) => (
         <DisplayCoherenceLink
           key={link.id}
           link={link}
