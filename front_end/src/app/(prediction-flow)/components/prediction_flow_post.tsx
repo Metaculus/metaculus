7f98c5d0bb29bcf0a80207efefda4f01accb3a25
Alex Costea <alex.costea@metaculus.com>
2025-08-11 11:02:33 -0400

    fix: CoherenceLinksProvider missing in Prediction Flow Post (#3280)

--- front_end/src/app/(prediction-flow)/components/prediction_flow_post.tsx
+++ front_end/src/app/(prediction-flow)/components/prediction_flow_post.tsx
@@ -3,6 +3,7 @@ import { isNil } from "lodash";
 import { useTranslations } from "next-intl";
 import { FC, useCallback, useEffect, useState } from "react";
 
+import { CoherenceLinksProvider } from "@/app/(main)/components/coherence_links_provider";
 import CommentsFeedProvider from "@/app/(main)/components/comments_feed_provider";
 import ForecastMaker from "@/components/forecast_maker";
 import BackgroundInfo from "@/components/question/background_info";
@@ -100,53 +101,55 @@ const PredictionFlowPost: FC<Props> = ({ tournamentSlug }) => {
 
   return (
     <HideCPProvider post={detailedPost} forceHideCP={forceHideCP}>
-      <div
-        className={cn("mx-4 mt-6 flex flex-col sm:mx-0", {
-          hidden: isMenuOpen,
-        })}
-      >
-        {shouldShowBanner && (
-          <RequireAttentionBanner detailedPost={currentFlowPost} />
-        )}
+      <CoherenceLinksProvider post={detailedPost}>
         <div
-          className={cn(
-            "flex w-full flex-col rounded bg-gray-0 p-4 py-3 dark:bg-gray-0-dark sm:p-8 sm:py-[26px]",
-            {
-              "rounded-t-none": shouldShowBanner,
-            }
-          )}
+          className={cn("mx-4 mt-6 flex flex-col sm:mx-0", {
+            hidden: isMenuOpen,
+          })}
         >
-          <div className="flex flex-col gap-4">
-            <h2 className="m-0 text-2xl font-bold leading-8 text-blue-800 dark:text-blue-800-dark">
-              {detailedPost?.title}
-            </h2>
+          {shouldShowBanner && (
+            <RequireAttentionBanner detailedPost={currentFlowPost} />
+          )}
+          <div
+            className={cn(
+              "flex w-full flex-col rounded bg-gray-0 p-4 py-3 dark:bg-gray-0-dark sm:p-8 sm:py-[26px]",
+              {
+                "rounded-t-none": shouldShowBanner,
+              }
+            )}
+          >
+            <div className="flex flex-col gap-4">
+              <h2 className="m-0 text-2xl font-bold leading-8 text-blue-800 dark:text-blue-800-dark">
+                {detailedPost?.title}
+              </h2>
 
-            <PredictionFlowQuestionCard post={detailedPost} />
+              <PredictionFlowQuestionCard post={detailedPost} />
 
-            <ForecastMaker
-              post={detailedPost}
-              onPredictionSubmit={onPredictionSubmit}
-              disableResolveButtons={true}
-            />
-
-            <div className="flex flex-col gap-2">
-              <ResolutionCriteria
+              <ForecastMaker
                 post={detailedPost}
-                defaultOpen={false}
-                className="my-0 gap-2"
+                onPredictionSubmit={onPredictionSubmit}
+                disableResolveButtons={true}
               />
-              <BackgroundInfo post={detailedPost} defaultOpen={false} />
 
-              <CommentsFeedProvider
-                postData={detailedPost}
-                rootCommentStructure={true}
-              >
-                <PredictionFlowCommentsSection postData={detailedPost} />
-              </CommentsFeedProvider>
+              <div className="flex flex-col gap-2">
+                <ResolutionCriteria
+                  post={detailedPost}
+                  defaultOpen={false}
+                  className="my-0 gap-2"
+                />
+                <BackgroundInfo post={detailedPost} defaultOpen={false} />
+
+                <CommentsFeedProvider
+                  postData={detailedPost}
+                  rootCommentStructure={true}
+                >
+                  <PredictionFlowCommentsSection postData={detailedPost} />
+                </CommentsFeedProvider>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </CoherenceLinksProvider>
     </HideCPProvider>
   );
 };
