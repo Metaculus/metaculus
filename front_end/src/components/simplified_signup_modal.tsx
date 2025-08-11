ddadeaaae4631b5cc2b38a44b4170271542be85c
hlbmtc <hlib@metaculus.com>
2025-08-11 16:33:48 +0200

    Frontend implementation

---
+++ front_end/src/components/simplified_signup_modal.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+import React, { FC, useEffect, useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { simplifiedSignUpAction } from "@/app/(main)/accounts/actions";
+import BaseModal from "@/components/base_modal";
+import Button from "@/components/ui/button";
+import { FormError, Input } from "@/components/ui/form_field";
+import { useAuth } from "@/contexts/auth_context";
+import { useModal } from "@/contexts/modal_context";
+import { ErrorResponse } from "@/types/fetch";
+
+const simplifiedSignupSchema = z.object({
+  username: z.string().min(1, { message: "Username is required" }),
+});
+type SimplifiedSignupSchema = z.infer<typeof simplifiedSignupSchema>;
+
+const SimplifiedSignupModal: FC = () => {
+  const { user, setUser } = useAuth();
+  const { setCurrentModal } = useModal();
+  const t = useTranslations();
+  const searchParams = useSearchParams();
+  const signupToken = useMemo(
+    () => searchParams?.get("signup_token") || "",
+    [searchParams]
+  );
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [submitErrors, setSubmitErrors] = useState<ErrorResponse>([]);
+
+  const { register, handleSubmit, formState, clearErrors } =
+    useForm<SimplifiedSignupSchema>({
+      mode: "all",
+      resolver: zodResolver(simplifiedSignupSchema),
+    });
+
+  useEffect(() => {
+    setIsOpen(!user && !!signupToken);
+  }, [user, signupToken]);
+
+  const onSubmit = async (values: SimplifiedSignupSchema) => {
+    if (!signupToken) return;
+
+    setIsLoading(true);
+    setSubmitErrors([]);
+    clearErrors();
+
+    try {
+      const response = await simplifiedSignUpAction(
+        values.username,
+        signupToken
+      );
+
+      if (response && "errors" in response && !!response.errors) {
+        setSubmitErrors(response.errors);
+      }
+
+      if (response && "user" in response) {
+        setUser(response.user);
+        setIsOpen(false);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <BaseModal
+      isOpen={isOpen}
+      onClose={() => {}}
+      isImmersive={true}
+      label={t("chooseYourUsername")}
+    >
+      <div className="mb-6 text-base leading-tight">
+        {t("registrationSignInHeading")}&nbsp;
+        <Button
+          variant="link"
+          size="md"
+          onClick={() => setCurrentModal({ type: "signin" })}
+        >
+          {t("logIn")}
+        </Button>
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex max-w-xs flex-col gap-4 "
+      >
+        <div className="w-full">
+          <Input
+            placeholder="Username"
+            autoComplete="username"
+            errors={formState.errors.username}
+            {...register("username")}
+            className="w-full rounded border border-gray-500 px-3 py-2 text-base dark:border-gray-500-dark dark:bg-blue-50-dark"
+          />
+          <FormError errors={submitErrors} />
+        </div>
+        <div>
+          <Button
+            type="submit"
+            variant="primary"
+            className="w-full"
+            disabled={isLoading}
+          >
+            Continue
+          </Button>
+        </div>
+        <div className="mt-6 text-center text-gray-700 dark:text-gray-700-dark">
+          {t.rich("continueTerms", {
+            terms: (chunks) => (
+              <Link target="_blank" href={"/terms-of-use/"}>
+                {chunks}
+              </Link>
+            ),
+            privacy: (chunks) => (
+              <Link target="_blank" href={"/privacy-policy/"}>
+                {chunks}
+              </Link>
+            ),
+          })}
+        </div>
+      </form>
+    </BaseModal>
+  );
+};
+
+export default SimplifiedSignupModal;
