853c4f649d314e0faaeab7201f9f74c8324040cd
Vasile Popescu <elisescu@elisescu.com>
2025-08-12 12:25:32 +0200

    Improve and fix the Weekly Top Comments navigation to past weeks

--- front_end/src/components/weekly_top_comments_feed/index.tsx
+++ front_end/src/components/weekly_top_comments_feed/index.tsx
@@ -7,19 +7,27 @@ import ServerProfileApi from "@/services/api/profile/profile.server";
 import { SearchParams } from "@/types/navigation";
 
 import CommentsOfWeekContent from "./components/comments_of_week_content";
+import { WEEK_START_DAY } from "./components/constants";
 
 const AwaitedWeeklyTopCommentsFeed: FC<{
   searchParams: SearchParams;
 }> = async ({ searchParams }) => {
-  const currentWeekStart = startOfWeek(new Date(), { weekStartsOn: 0 }); // Sunday
+  const currentWeekStart = startOfWeek(new Date(), {
+    weekStartsOn: WEEK_START_DAY,
+  });
   const startDate =
     typeof searchParams.start_date === "string"
       ? new Date(searchParams.start_date)
       : currentWeekStart;
 
   // Redirect to the last Sunday if the provided date is not a Sunday
-  if (typeof searchParams.start_date === "string" && getDay(startDate) !== 0) {
-    const lastSunday = startOfWeek(startDate, { weekStartsOn: 0 });
+  if (
+    typeof searchParams.start_date === "string" &&
+    getDay(startDate) !== WEEK_START_DAY
+  ) {
+    const lastSunday = startOfWeek(startDate, {
+      weekStartsOn: WEEK_START_DAY,
+    });
     redirect(
       `/questions/?weekly_top_comments=true&start_date=${format(lastSunday, "yyyy-MM-dd")}`
     );
