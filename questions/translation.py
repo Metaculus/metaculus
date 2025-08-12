b61973019accead1968d7b2621a01b72c795811e
Vasile Popescu <elisescu@elisescu.com>
2025-08-12 15:11:31 +0200

    Revert "Fix translations issue with empty strings"
    This reverts commit b173e06c744d0bbe2ef14d9a149ace69eda20731.

--- questions/translation.py
+++ questions/translation.py
@@ -1,16 +1,13 @@
 from modeltranslation.translator import TranslationOptions, translator
 from .models import Question, GroupOfQuestions
-from metaculus_web.settings import TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 class QuestionTranslationOptions(TranslationOptions):
     fields = ("description", "resolution_criteria", "fine_print", "label", "title")
-    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 class GroupOfQuestionsTranslationOptions(TranslationOptions):
     fields = ("description", "resolution_criteria", "fine_print")
-    fallback_undefined = TRANSLATIONS_FALLBACK_UNDEFINED
 
 
 translator.register(Question, QuestionTranslationOptions)
