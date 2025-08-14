4302f85d2bea3dbeba8fa9b7f54a06c343ee9572
lsabor <lukesabor@gmail.com>
2025-08-14 11:40:40 -0700

    count distinct users

--- scoring/utils.py
+++ scoring/utils.py
@@ -520,11 +520,14 @@ def calculate_medals_points_at_time(at_time):
     # Look only at leaderboard entries which are closed before the timestamp
     relevant_entries_qs = LeaderboardEntry.objects.filter(
         Q(leaderboard__end_time__lte=at_time)
-        | Q(leaderboard__project__close_date__lte=at_time),
+        | Q(
+            leaderboard__end_time__isnull=True,
+            leaderboard__project__close_date__lte=at_time,
+        ),
         leaderboard__project__default_permission=ObjectPermission.FORECASTER,
     )
 
-    # Get the age, in yeaers, for each leaderboard, and use it to
+    # Get the age, in years, for each leaderboard, and use it to
     # exp-decay the points associated with each medal (older medals weigh less)
     leaderboard_age_expr = Case(
         When(
@@ -610,7 +613,7 @@ def calculate_medals_points_at_time(at_time):
             points_type=points_type_expr,
         )
         .values("points_type")
-        .annotate(total_participants=Count("user"))
+        .annotate(total_participants=Count("user", distinct=True))
     )
 
     points = (
