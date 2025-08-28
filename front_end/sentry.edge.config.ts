38cf48c0708264c89fe9827963a395603764241a
Hlib <hlib@metaculus.com>
2025-08-28 16:28:07 +0200

    Unified sentry config and made transactions less noisy (#3375)

--- front_end/sentry.edge.config.ts
+++ front_end/sentry.edge.config.ts
@@ -1,16 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
-import {
-  beforeSentryAlertSend,
-  SENTRY_IGNORE_ERRORS,
-} from "@/utils/core/errors";
+import { buildSentryOptions } from "@/sentry/options";
 
 if (!!process.env.PUBLIC_FRONTEND_SENTRY_DSN) {
-  Sentry.init({
-    environment: process.env.METACULUS_ENV,
-    dsn: process.env.PUBLIC_FRONTEND_SENTRY_DSN,
-    tracesSampleRate: 0.1,
-    ignoreErrors: SENTRY_IGNORE_ERRORS,
-    beforeSend: beforeSentryAlertSend,
-  });
+  Sentry.init(buildSentryOptions(process.env.PUBLIC_FRONTEND_SENTRY_DSN));
 }
