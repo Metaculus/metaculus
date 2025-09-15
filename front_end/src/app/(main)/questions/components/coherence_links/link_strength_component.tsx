1e80636fdd331cfa9daf4539ba646f571fef5dcb
Alex Costea <alex.costea@metaculus.com>
2025-09-15 13:38:12 -0400

    component styling

--- front_end/src/app/(main)/questions/components/coherence_links/link_strength_component.tsx
+++ front_end/src/app/(main)/questions/components/coherence_links/link_strength_component.tsx
@@ -3,18 +3,32 @@ import { FC } from "react";
 
 import Button from "@/components/ui/button";
 import { Strengths } from "@/types/coherence";
+import cn from "@/utils/core/cn";
 
 type Props = {
   strength: Strengths;
   disabled: boolean;
 };
 
+const colorAccent = {
+  [Strengths.Low]: "orange-200",
+  [Strengths.Medium]: "orange-300",
+  [Strengths.High]: "orange-400",
+} as const;
+
 const LinkStrengthComponent: FC<Props> = ({ strength, disabled }) => {
   const t = useTranslations();
   const label = t(strength);
+  const color = colorAccent[strength];
+  const additionalStyling = `bg-${color} disabled:bg-${color} hover:bg-${color} active:bg-${color} 
+  dark:bg-${color}-dark disabled:dark:bg-${color}-dark dark:hover:bg-${color} dark:active:bg-${color} 
+  border-${color} dark:border-${color}-dark`;
   return (
     <Button
-      className={"text-bold mt-3 rounded-md border disabled:opacity-100"}
+      className={cn(
+        "text-bold mt-3 gap-1.5 rounded border px-2 py-1.5 disabled:opacity-100",
+        additionalStyling
+      )}
       disabled={disabled}
     >
       {label}
