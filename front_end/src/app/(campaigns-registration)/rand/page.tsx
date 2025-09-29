2b27e614539d0f748227c845ed13487954762f12
aseckin <atakanseckin@gmail.com>
2025-09-29 23:17:43 +0200

    Updated page metadata

--- front_end/src/app/(campaigns-registration)/rand/page.tsx
+++ front_end/src/app/(campaigns-registration)/rand/page.tsx
@@ -3,7 +3,7 @@ import GlobalHeader from "@/app/(main)/components/headers/global_header";
 import { Hero } from "./components/hero-section";
 
 export const metadata = {
-  title: "RAND x Metaculus National Forecasting Tournament - $10,000 in Prizes",
+  title: "RAND x Metaculus Policy Challenge - $10,000 in Prizes",
   description:
     "Join the national forecasting tournament for university students. Predict the future, inform public policy, and compete for $10,000 in prizes. Partnership between Metaculus and RAND Corporation.",
 };
