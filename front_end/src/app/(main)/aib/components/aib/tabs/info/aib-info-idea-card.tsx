076003a9d9ed4afb788b615e316d5696d299deff
Nikita <nikita.develops.work@gmail.com>
2025-10-27 15:49:45 +0200

    feat: update content

--- front_end/src/app/(main)/aib/components/aib/tabs/info/aib-info-idea-card.tsx
+++ front_end/src/app/(main)/aib/components/aib/tabs/info/aib-info-idea-card.tsx
@@ -14,7 +14,7 @@ const AIBInfoIdeaCard: React.FC<Props> = ({ icon, title, children }) => {
         className="text-[26px] text-blue-700 dark:text-blue-700-dark"
         icon={icon}
       />
-      <h4 className="m-0 mt-5 text-nowrap font-serif text-xl font-semibold text-blue-800 antialiased dark:text-blue-800-dark md:text-2xl">
+      <h4 className="m-0 mt-5 font-serif text-xl font-semibold text-blue-800 antialiased dark:text-blue-800-dark md:text-2xl">
         {title}
       </h4>
       <div className="mt-2.5 text-sm font-normal text-blue-700 dark:text-blue-700-dark md:text-base">
