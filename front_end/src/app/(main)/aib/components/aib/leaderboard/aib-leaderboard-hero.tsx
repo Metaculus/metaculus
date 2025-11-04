1e128cb01422f13b80c86ed6803f45c053f78af0
Nikita <nikita.develops.work@gmail.com>
2025-11-04 09:31:48 +0200

    feat: update leaderboard hero

--- front_end/src/app/(main)/aib/components/aib/leaderboard/aib-leaderboard-hero.tsx
+++ front_end/src/app/(main)/aib/components/aib/leaderboard/aib-leaderboard-hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 
@@ -18,17 +16,11 @@ const AIBLeaderboardHero: React.FC = () => {
       </Link>
 
       <h1 className="m-0 text-center text-[32px] font-bold leading-[116%] -tracking-[1.28px] text-blue-800 dark:text-blue-800-dark sm:text-5xl sm:-tracking-[1.92px]">
-        {t.rich("aibLbTitle", {
-          br: () => <br className="min-[376px]:hidden" />,
-        })}
+        {t("aibLbTitle")}
       </h1>
 
-      <div className="flex items-center gap-2 text-sm font-normal text-blue-700 dark:text-blue-700-dark sm:text-xl sm:font-medium">
+      <div className="flex items-center gap-2 text-center text-sm font-normal text-blue-800 opacity-60 dark:text-blue-800-dark sm:text-base">
         <span>{t("aibLbSubtitle")}</span>
-        <FontAwesomeIcon
-          icon={faQuestionCircle}
-          className="h-3 w-3 sm:h-5 sm:w-5"
-        />
       </div>
     </div>
   );
