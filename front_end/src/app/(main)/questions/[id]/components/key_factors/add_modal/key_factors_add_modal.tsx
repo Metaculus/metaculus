11ae0dfd916363bcd913bdecf710df6c8d607d01
Nikita <nikita.develops.work@gmail.com>
2025-11-05 18:53:05 +0200

    feat: key factor updates

--- front_end/src/app/(main)/questions/[id]/components/key_factors/add_modal/key_factors_add_modal.tsx
+++ front_end/src/app/(main)/questions/[id]/components/key_factors/add_modal/key_factors_add_modal.tsx
@@ -7,20 +7,13 @@ import { BECommentType } from "@/types/comment";
 import { PostWithForecasts } from "@/types/post";
 import { User } from "@/types/users";
 
-import { KeyFactorsProvider, useKeyFactorsCtx } from "../key_factors_context";
-<<<<<<< HEAD
+import { KeyFactorsProvider } from "../key_factors_context";
 import KeyFactorsTypePicker from "../key_factors_type_picker";
 import { KFType } from "../types";
 import KeyFactorsBaseRateCreationBlock from "./creation_blocks/key_factors_base_rate_creation_block";
 import KeyFactorsDriverCreationBlock from "./creation_blocks/key_factors_driver_creation_block";
+import KeyFactorsNewsCreationBlock from "./creation_blocks/key_factors_news_creation_block";
 import KeyFactorsBreadcrumbs from "./key_factors_breadcrumbs";
-=======
-import KeyFactorsBreadcrumbs from "./blocks/key_factors_breadcrumbs";
-import KeyFactorsDriverCreation from "./blocks/key_factors_driver_creation";
-import KeyFactorsModalPicker from "./blocks/key_factors_modal_picker";
-import Stub from "./blocks/stub";
->>>>>>> 78bf71fce (feat: add types in the modal)
-import KeyFactorsLoadingSuggested from "./key_factors_loading_suggested";
 
 type Props = {
   isOpen: boolean;
@@ -56,11 +49,7 @@ const KeyFactorsAddModal: FC<Props> = ({
       isImmersive
       withCloseButton
       closeButtonClassName="top-5 right-5 sm:top-[28px] sm:right-[28px] p-0 text-base [&>_svg]:size-6"
-<<<<<<< HEAD
       className="m-0 flex h-full max-w-[560px] flex-col overscroll-contain rounded-none md:w-auto md:rounded lg:m-auto lg:h-auto lg:w-full"
-=======
-      className="m-0 flex h-full w-full max-w-[560px] flex-col overscroll-contain rounded-none md:w-auto md:rounded lg:m-auto lg:h-auto"
->>>>>>> 78bf71fce (feat: add types in the modal)
     >
       <KeyFactorsProvider
         user={user}
@@ -79,18 +68,12 @@ const KeyFactorsAddModal: FC<Props> = ({
   );
 };
 
-<<<<<<< HEAD
-=======
-export type KFType = "driver" | "base_rate" | "news" | null;
-
->>>>>>> 78bf71fce (feat: add types in the modal)
 const KeyFactorsAddModalBody: React.FC<{
   post: PostWithForecasts;
   commentId?: number;
   onClose: () => void;
   onSuccess?: (c: BECommentType) => void;
 }> = ({ post, commentId, onClose, onSuccess }) => {
-  const { isLoadingSuggestedKeyFactors } = useKeyFactorsCtx();
   const [selectedType, setSelectedType] = useState<KFType>(null);
 
   return (
@@ -100,44 +83,31 @@ const KeyFactorsAddModalBody: React.FC<{
         onRootClick={() => setSelectedType(null)}
       />
 
-      {isLoadingSuggestedKeyFactors && <KeyFactorsLoadingSuggested />}
-<<<<<<< HEAD
-      {!isLoadingSuggestedKeyFactors && (
-        <div className="flex grow flex-col gap-2">
-          {!selectedType ? (
-            <KeyFactorsTypePicker onPick={setSelectedType} />
-          ) : selectedType === "driver" ? (
-            <KeyFactorsDriverCreationBlock
-=======
-
-      {!isLoadingSuggestedKeyFactors && (
-        <div className="flex grow flex-col gap-2">
-          {!selectedType ? (
-            <KeyFactorsModalPicker onPick={setSelectedType} />
-          ) : selectedType === "driver" ? (
-            <KeyFactorsDriverCreation
->>>>>>> 78bf71fce (feat: add types in the modal)
-              post={post}
-              commentId={commentId}
-              onClose={onClose}
-              onSuccess={onSuccess}
-            />
-<<<<<<< HEAD
-          ) : selectedType === "base_rate" ? (
-            <KeyFactorsBaseRateCreationBlock
-              post={post}
-              commentId={commentId}
-              onClose={onClose}
-              onSuccess={onSuccess}
-            />
-          ) : null}
-=======
-          ) : (
-            <Stub selectedType={selectedType} />
-          )}
->>>>>>> 78bf71fce (feat: add types in the modal)
-        </div>
-      )}
+      <div className="flex grow flex-col">
+        {!selectedType ? (
+          <KeyFactorsTypePicker onPick={setSelectedType} />
+        ) : selectedType === "driver" ? (
+          <KeyFactorsDriverCreationBlock
+            post={post}
+            commentId={commentId}
+            onClose={onClose}
+            onSuccess={onSuccess}
+          />
+        ) : selectedType === "base_rate" ? (
+          <KeyFactorsBaseRateCreationBlock
+            post={post}
+            commentId={commentId}
+            onClose={onClose}
+            onSuccess={onSuccess}
+          />
+        ) : selectedType === "news" ? (
+          <KeyFactorsNewsCreationBlock
+            post={post}
+            onClose={onClose}
+            onSuccess={onSuccess}
+          />
+        ) : null}
+      </div>
     </>
   );
 };
