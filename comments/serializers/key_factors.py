030c2a54e7c5183df3d5c2978cfd8376c41e3a7e
hlbmtc <hlib@metaculus.com>
2025-11-06 15:06:46 +0100

    Key Factor news object

--- comments/serializers/key_factors.py
+++ comments/serializers/key_factors.py
@@ -10,6 +10,7 @@ from comments.models import (
     ImpactDirection,
     KeyFactorVote,
     KeyFactorBaseRate,
+    KeyFactorNews,
 )
 from comments.services.key_factors.common import (
     get_votes_for_key_factors,
@@ -33,6 +34,18 @@ def serialize_key_factor_votes(
         ],
         "user_vote": user_vote,
         "count": len(vote_scores),
+    }
+
+
+def serialize_news(news: KeyFactorNews) -> dict:
+    """Serializes a KeyFactorNews object"""
+    return {
+        "url": news.url,
+        "title": news.title,
+        "img_url": news.img_url,
+        "source": news.source,
+        "published_at": news.published_at,
+        "itn_article_id": news.itn_article_id,
     }
 
 
@@ -74,6 +87,7 @@ def serialize_key_factor(
             if key_factor.base_rate
             else None
         ),
+        "news": (serialize_news(key_factor.news) if key_factor.news else None),
         "post": {
             "id": key_factor.comment.on_post_id,
             "question_type": question_type,
@@ -91,7 +105,12 @@ def serialize_key_factors_many(
         KeyFactor.objects.filter(pk__in=ids)
         .filter_active()
         .select_related(
-            "comment__author", "comment__on_post", "question", "driver", "base_rate"
+            "comment__author",
+            "comment__on_post",
+            "question",
+            "driver",
+            "base_rate",
+            "news",
         )
     )
 
@@ -231,9 +250,37 @@ class BaseRateSerializer(serializers.ModelSerializer):
                 raise ValidationError({field: f"{field} is required"})
 
 
+class KeyFactorNewsSerializer(serializers.ModelSerializer):
+    """
+    Serializer for creating/updating KeyFactorNews
+    """
+
+    class Meta:
+        model = KeyFactorNews
+        fields = (
+            "itn_article_id",
+            "url",
+            "title",
+            "img_url",
+            "source",
+            "published_at",
+        )
+
+    def validate(self, attrs):
+        itn_article_id = attrs.get("itn_article_id")
+
+        if not itn_article_id:
+            for field in ["url", "title", "source"]:
+                if not attrs.get(field):
+                    raise ValidationError({field: f"{field} is required"})
+
+        return attrs
+
+
 class KeyFactorWriteSerializer(serializers.ModelSerializer):
     driver = KeyFactorDriverSerializer(required=False)
     base_rate = BaseRateSerializer(required=False)
+    news = KeyFactorNewsSerializer(required=False)
     question_id = serializers.IntegerField(required=False, allow_null=True)
 
     class Meta:
@@ -243,10 +290,11 @@ class KeyFactorWriteSerializer(serializers.ModelSerializer):
             "question_option",
             "driver",
             "base_rate",
+            "news",
         )
 
     def validate(self, attrs: dict):
-        key_factor_types = ["driver", "base_rate"]
+        key_factor_types = ["driver", "base_rate", "news"]
 
         if len([True for kf_type in key_factor_types if attrs.get(kf_type)]) != 1:
             raise ValidationError(
