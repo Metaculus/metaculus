53ed85435ff897b071effdab82df9f1a2f6ed998
hlbmtc <hlib@metaculus.com>
2025-11-20 19:46:30 +0100

    Small fix

--- front_end/src/app/(main)/questions/[id]/components/post_score_data/resolution_score_cards.tsx
+++ front_end/src/app/(main)/questions/[id]/components/post_score_data/resolution_score_cards.tsx
@@ -41,7 +41,7 @@ const ResolutionScoreCards: FC<Props> = ({
   const baselineKey = spot ? "spot_baseline" : "baseline";
 
   const renderPrimaryCards = () => (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
       <ScoreCard
         type="peer"
         userScore={getScore(userScores, peerKey)}
