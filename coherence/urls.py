fa763cb23b3e1200c8d966bffb504ae3acd97b28
hlbmtc <hlib@metaculus.com>
2025-12-04 17:13:37 +0000

    Question Link Votes & Freshness implementation

--- coherence/urls.py
+++ coherence/urls.py
@@ -2,21 +2,30 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path(
         "coherence/links/create/",
         views.create_link_api_view,
         name="coherence-create-link",
     ),
+    # TODO: this is confusing because `/links/:id` represents the question ID, not the link ID.
+    #   We should improve this in the future so that the URL always refers to the actual object ID.
+    #   The question layer should be explicitly defined with a `/question/` prefix.
     path(
         "coherence/links/<int:pk>/",
         views.get_links_for_question_api_view,
-        name="get-links-for-question",
+        name="get-links-for-question-old",
     ),
     path(
         "coherence/aggregate-links/<int:pk>/",
         views.get_aggregate_links_for_question_api_view,
-        name="get-aggregate-links-for-question",
+        name="get-aggregate-links-for-question-old",
+    ),
+    path(
+        "coherence/aggregate-links/<int:pk>/votes/",
+        views.aggregate_links_vote_view,
+        name="aggregate-links-votes",
     ),
     path(
         "coherence/links/<int:pk>/delete/",
@@ -27,5 +36,16 @@ urlpatterns = [
         "coherence/links/<int:pk>/needs-update",
         views.get_questions_requiring_update,
         name="needs-update",
+    ),
+    # Question-level links
+    path(
+        "coherence/question/<int:pk>/links/",
+        views.get_links_for_question_api_view,
+        name="get-links-for-question",
+    ),
+    path(
+        "coherence/question/<int:pk>/aggregate-links/",
+        views.get_aggregate_links_for_question_api_view,
+        name="get-aggregate-links-for-question",
     ),
 ]
---
+++ tests/unit/test_coherence/test_views.py
@@ -0,0 +1,35 @@
+import pytest
+from django.urls import reverse
+
+from tests.unit.test_questions.conftest import *  # noqa
+from .factories import factory_aggregate_coherence_link
+
+
+def test_aggregate_question_link_vote(
+    user1, user2_client, user1_client, question_binary, question_numeric
+):
+    aggregation = factory_aggregate_coherence_link(
+        question1=question_binary, question2=question_numeric
+    )
+
+    url = reverse("aggregate-links-votes", kwargs={"pk": aggregation.pk})
+
+    # User2 votes with 1
+    response = user2_client.post(url, data={"vote": 1}, format="json")
+    assert response.status_code == 200
+    assert response.data["count"] == 1
+
+    # User1 votes with -1
+    response = user1_client.post(url, data={"vote": -1}, format="json")
+    assert response.status_code == 200
+    assert response.data["count"] == 2
+
+    # Check votes response
+    url = reverse(
+        "get-aggregate-links-for-question", kwargs={"pk": question_numeric.pk}
+    )
+    response = user2_client.get(url)
+
+    assert response.data["data"][0]["freshness"] == pytest.approx(0.66, rel=0.1)
+    votes_response = response.data["data"][0]["votes"]
+    assert {x["score"] for x in votes_response["aggregated_data"]} == {1, -1}
