fea52d54b5c75c2f91d3cba86f9b56cf04233f7a
Vasile Popescu <elisescu@elisescu.com>
2025-12-04 12:04:21 +0100

    New homepage: Add the tournaments section

---
+++ front_end/src/app/(main)/(home)/new/components/tournaments_section.tsx
@@ -0,0 +1,71 @@
+import { getTranslations } from "next-intl/server";
+import { FC } from "react";
+
+import Button from "@/components/ui/button";
+import TournamentCard from "@/components/tournament_card";
+import ServerProjectsApi from "@/services/api/projects/projects.server";
+import { TournamentType } from "@/types/projects";
+import cn from "@/utils/core/cn";
+import WithServerComponentErrorBoundary from "@/components/server_component_error_boundary";
+
+const TournamentsSection: FC<{ className?: string }> = async ({
+  className,
+}) => {
+  const t = await getTranslations();
+  const tournaments = await ServerProjectsApi.getTournaments({
+    show_on_homepage: true,
+  });
+  const allTournaments = (await ServerProjectsApi.getTournaments()).filter(
+    (t) => t.is_ongoing
+  );
+
+  return (
+    <div
+      className={cn(
+        "border-y border-gray-300 bg-gray-100 dark:border-gray-300-dark  dark:bg-gray-100-dark md:mt-16",
+        className
+      )}
+    >
+      <div className="flex flex-wrap items-start justify-between gap-4">
+        <div className="flex flex-col gap-1 text-gray-1000 dark:text-gray-1000-dark">
+          <h2 className="m-0 text-xl font-bold leading-7">
+            {t("forecasting")} {t("tournaments")}
+          </h2>
+          <p className="m-0 max-w-[420px] text-base font-medium leading-6">
+            {t("joinTournaments")}
+          </p>
+        </div>
+        <Button
+          href="/tournaments/"
+          variant="secondary"
+          size="md"
+          className="whitespace-nowrap"
+        >
+          {t("exploreNTournaments", { count: allTournaments.length })} →
+        </Button>
+      </div>
+      <div className="mt-10 grid gap-6 sm:grid-cols-2 xl:grid-cols-4">
+        {tournaments.map((tournament) => (
+          <TournamentCard
+            key={tournament.id}
+            href={
+              tournament.slug
+                ? `/tournament/${tournament.slug}`
+                : `/tournament/${tournament.id}`
+            }
+            headerImageSrc={tournament.header_image}
+            name={tournament.name}
+            questionsCount={tournament.questions_count}
+            closeDate={tournament.close_date}
+            showCloseDate={tournament.type !== TournamentType.QuestionSeries}
+            prizePool={tournament.prize_pool}
+            withCount={true}
+            isPrivate={tournament.default_permission === null}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WithServerComponentErrorBoundary(TournamentsSection);
