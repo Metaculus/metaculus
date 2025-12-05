a2ac1d98a6791713a22110732d9595d76ffcee49
Nikita <nikita.develops.work@gmail.com>
2025-12-05 17:37:57 +0200

    feat: add dynamic chart height adaptation

--- front_end/src/app/(embed)/questions/components/embed_question_header.tsx
+++ front_end/src/app/(embed)/questions/components/embed_question_header.tsx
@@ -7,14 +7,37 @@ import {
 } from "@/utils/questions/helpers";
 
 import TruncatableQuestionTitle from "./truncatable_question_title";
+import { useEffect, useRef } from "react";
 
 type Props = {
   post: PostWithForecasts;
+  onHeightChange?: (height: number) => void;
 };
 
-const EmbedQuestionHeader: React.FC<Props> = ({ post }) => {
+const EmbedQuestionHeader: React.FC<Props> = ({ post, onHeightChange }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!onHeightChange) return;
+    const el = containerRef.current;
+    if (!el) return;
+
+    const update = () => {
+      onHeightChange(el.getBoundingClientRect().height);
+    };
+
+    update();
+
+    const observer = new ResizeObserver(() => {
+      update();
+    });
+    observer.observe(el);
+
+    return () => observer.disconnect();
+  }, [onHeightChange]);
+
   return (
-    <div className="flex items-center">
+    <div ref={containerRef} className="flex items-center">
       <TruncatableQuestionTitle
         className="!text-[20px] !leading-[125%]"
         maxLines={4}
