dd8d4bdeba11a04f765a5e747c5947d3414643a5
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-12-12 20:37:49 -0700

    Finishing fall 2025 page

--- front_end/src/app/(main)/aib/components/leaderboard-fall.tsx
+++ front_end/src/app/(main)/aib/components/leaderboard-fall.tsx
@@ -1,8 +1,4 @@
-import {
-  faArrowRight,
-  faChartBar,
-  faTrophy,
-} from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faTrophy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
@@ -12,29 +8,19 @@ function FallBotLeaderboard() {
   return (
     <Link
       href="/tournament/fall-aib-2025/"
-      className="group flex size-full max-h-[420px] min-h-[300px] flex-col items-center justify-center gap-4 overflow-y-auto rounded bg-white p-4 no-underline transition-all hover:bg-blue-200/50 dark:bg-blue-100-dark dark:hover:bg-blue-200-dark md:gap-5 min-[1920px]:max-h-[680px] min-[1920px]:gap-8 min-[1920px]:p-12"
+      className="group flex size-full max-h-[420px] min-h-[300px] flex-col items-center justify-center gap-2 overflow-y-auto rounded bg-white p-3 no-underline transition-all hover:bg-blue-200/50 dark:bg-blue-100-dark dark:hover:bg-blue-200-dark md:gap-4 md:p-6 min-[1920px]:max-h-[680px]"
     >
-      <div className="flex flex-col items-center gap-3 text-center">
-        <div className="flex items-center gap-4">
-          <FontAwesomeIcon
-            icon={faTrophy}
-            className="text-4xl opacity-20 md:text-5xl"
-          />
-          <FontAwesomeIcon
-            icon={faChartBar}
-            className="text-4xl opacity-20 md:text-5xl"
-          />
-        </div>
-        <p className="m-0 text-lg text-blue-700 dark:text-blue-700-dark md:text-xl">
-            View the leaderboard and questions.
-        </p>
-        <p className="m-0 text-base text-blue-600 opacity-70 dark:text-blue-600-dark md:text-lg">
-            The Fall 2025 Bot Tournament closes January 5th, and will be finalized in March.
-        </p>
-      </div>
+      <FontAwesomeIcon
+        icon={faTrophy}
+        className="text-4xl opacity-20 transition-all group-hover:opacity-80 md:text-6xl xl:text-8xl"
+      />
+      <p className="m-0 text-lg text-blue-700 dark:text-blue-700-dark md:text-2xl">
+        View the leaderboard and questions.
+      </p>
       <Button
+        size="lg"
         variant="secondary"
-        className="mt-2 gap-2 opacity-70 transition-all group-hover:opacity-100"
+        className="mb-3 mt-2 gap-2 opacity-70 transition-all group-hover:opacity-100 md:mb-0 lg:mt-4"
       >
         View Tournament
         <FontAwesomeIcon icon={faArrowRight} />
