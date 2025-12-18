0575ba2bfdce948df447874ad3748da308956eb2
Vasile Popescu <elisescu@elisescu.com>
2025-12-18 15:25:52 +0100

    Refactor tournaments section to filter ongoing tournaments for homepage display

--- projects/serializers/common.py
+++ projects/serializers/common.py
@@ -95,6 +95,7 @@ class TournamentShortSerializer(serializers.ModelSerializer):
             "score_type",
             "default_permission",
             "visibility",
+            "show_on_homepage",
             "is_current_content_translated",
             "bot_leaderboard_status",
         )
