7707f78cbfcce2e1bf66cb67b6078a3e3b664142
Nikita <nikita.develops.work@gmail.com>
2025-12-24 17:51:51 +0200

    feat: add cards mobile adaptation

--- front_end/src/app/(main)/(tournaments)/tournaments/components/new/tournaments_grid/tournaments_grid.tsx
+++ front_end/src/app/(main)/(tournaments)/tournaments/components/new/tournaments_grid/tournaments_grid.tsx
@@ -15,7 +15,9 @@ const TournamentsGrid: React.FC<Props> = ({ items, renderItem }) => {
       className="
         grid
         grid-cols-2
-        gap-3 sm:gap-5
+        gap-3
+        sm:grid-cols-3
+        sm:gap-5 md:grid-cols-4
         lg:grid-cols-3
         xl:grid-cols-4
       "
