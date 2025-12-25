1a8db1d722d4d3c67c0d1622201f1da244cbc319
Nikita <nikita.develops.work@gmail.com>
2025-12-25 18:40:17 +0200

    refactor: tournament cards

--- front_end/src/app/(main)/(tournaments)/tournaments/components/new/tournaments_grid/question_series_card.tsx
+++ front_end/src/app/(main)/(tournaments)/tournaments/components/new/tournaments_grid/question_series_card.tsx
@@ -1,34 +1,20 @@
 "use client";
 
-import Link from "next/link";
 import { useTranslations } from "next-intl";
 import React from "react";
 
 import { TournamentPreview } from "@/types/projects";
-import cn from "@/utils/core/cn";
-import { getProjectLink } from "@/utils/navigation";
 
-type Props = {
-  item: TournamentPreview;
-};
+import TournamentCardShell from "./tournament_card_shell";
+
+type Props = { item: TournamentPreview };
 
 const QuestionSeriesCard: React.FC<Props> = ({ item }) => {
   const t = useTranslations();
-
-  const href = getProjectLink(item);
   const questionsCount = item.questions_count ?? 0;
 
   return (
-    <Link
-      href={href}
-      className={cn(
-        "group block no-underline",
-        "rounded-lg border border-blue-400 dark:border-blue-400-dark lg:rounded",
-        "bg-gray-0/50 dark:bg-gray-0-dark/50",
-        "shadow-sm transition-shadow hover:shadow-md",
-        "overflow-hidden"
-      )}
-    >
+    <TournamentCardShell item={item}>
       <div className="relative h-[64px] w-full overflow-hidden  bg-blue-100/40 p-3 pb-0 dark:bg-blue-100-dark/20 lg:h-[80px] lg:p-4">
         {item.header_image ? (
           // eslint-disable-next-line @next/next/no-img-element
@@ -58,7 +44,7 @@ const QuestionSeriesCard: React.FC<Props> = ({ item }) => {
           {item.name}
         </h6>
       </div>
-    </Link>
+    </TournamentCardShell>
   );
 };
 
