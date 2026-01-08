8fa9c67cf609389777b4eaffb4c59202a1508552
Vasile Popescu <elisescu@elisescu.com>
2026-01-08 11:57:17 +0100

    Small adjustments to the Explore all SVG image position

--- front_end/src/app/(main)/(home)/new/components/ExploreImagesGrid.tsx
+++ front_end/src/app/(main)/(home)/new/components/ExploreImagesGrid.tsx
@@ -9,6 +9,7 @@ export const ExploreImagesGrid: FC<{ className?: string }> = ({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
+      preserveAspectRatio="xMinYMax meet"
       className={className}
     >
       <g filter="url(#filter0_d_5427_3842)">
