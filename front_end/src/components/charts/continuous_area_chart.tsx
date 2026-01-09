44cff64aad31e50d44406dd2207de4d801d7dc63
Luke Sabor <32885230+lsabor@users.noreply.github.com>
2026-01-08 17:16:37 -0800

    mc/3841/setup/non-prediction-values (#3842)
    add support for None in forecast values
    support null values on frontend
    add types for numeric vs multiple choice forecasts and the downstream dependent types

--- front_end/src/components/charts/continuous_area_chart.tsx
+++ front_end/src/components/charts/continuous_area_chart.tsx
@@ -1118,6 +1118,14 @@ export function getContinuousAreaChartData({
   };
   isClosed?: boolean;
 }): ContinuousAreaGraphInput {
+  if (
+    question.type !== QuestionType.Numeric &&
+    question.type !== QuestionType.Discrete &&
+    question.type !== QuestionType.Date
+  ) {
+    return [];
+  }
+
   const chartData: ContinuousAreaGraphInput = [];
 
   const latest =
