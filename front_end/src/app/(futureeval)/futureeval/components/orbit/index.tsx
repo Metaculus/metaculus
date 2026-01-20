97b480d2ad42b9acde91c951ddf2085753b3548e
Atakan Seckin <atakanseckin@gmail.com>
2026-01-20 17:32:44 +0100

    fix(futureeval): update orbit index exports after refactoring

--- front_end/src/app/(futureeval)/futureeval/components/orbit/index.tsx
+++ front_end/src/app/(futureeval)/futureeval/components/orbit/index.tsx
@@ -3,7 +3,7 @@
 import FutureEvalOrbit from "./futureeval-orbit";
 
 export { FutureEvalOrbit };
-export { ORBIT_ROTATION_SPEED, ORBIT_CONFIG } from "./futureeval-orbit";
-export type { OrbitItem } from "./orbit-circle";
+export { ORBIT_ROTATION_SPEED, ORBIT_CONFIG } from "./orbit-constants";
+export type { OrbitItem } from "./orbit-constants";
 
 export default FutureEvalOrbit;
