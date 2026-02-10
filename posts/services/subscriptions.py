c1ccf511b99bd4be99df1f018805e2ab1f23dc95
Hlib <hlib@metaculus.com>
2026-02-10 18:27:44 +0000

    Fixed missing user forecast date for CP change notifications (#4309)
    * Fixed missing user forecast date for CP change notifications
    
    * Small fix

--- posts/services/subscriptions.py
+++ posts/services/subscriptions.py
@@ -66,9 +66,11 @@ def _get_question_data_for_cp_change_notification(
             direction, magnitude = difference_display[i]
             data.cp_change_label = direction
             data.cp_change_value = magnitude
-            data.user_forecast = (
-                user_forecast.probability_yes_per_category[i] if user_forecast else None
-            )
+
+            if user_forecast:
+                data.user_forecast = user_forecast.probability_yes_per_category[i]
+                data.forecast_date = user_forecast.start_time.isoformat()
+
             question_data.append(data)
     else:  # continuous
         data = CPChangeData(question=NotificationQuestionParams.from_question(question))
@@ -101,14 +103,16 @@ def _get_question_data_for_cp_change_notification(
         direction, magnitude = difference_display[0]
         data.cp_change_label = direction
         data.cp_change_value = magnitude
-        user_q1, user_median, user_q3 = None, None, None
         if user_forecast:
             user_q1, user_median, user_q3 = get_scaled_quartiles_from_cdf(
                 user_forecast.continuous_cdf, question
             )
-        data.user_q1 = user_q1
-        data.user_median = user_median
-        data.user_q3 = user_q3
+
+            data.user_q1 = user_q1
+            data.user_median = user_median
+            data.user_q3 = user_q3
+            data.forecast_date = user_forecast.start_time.isoformat()
+
         question_data.append(data)
     return question_data
 
