dd4735586d29e60045a13cca6098192c56a87983
Vasile Popescu <elisescu@elisescu.com>
2026-02-11 13:32:46 +0100

    Add the missing Create button on the mobile menu

--- front_end/src/app/(main)/components/headers/components/create_question_button.tsx
+++ front_end/src/app/(main)/components/headers/components/create_question_button.tsx
@@ -5,11 +5,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
 
-const CreateQuestionButton: FC = () => {
+import cn from "@/utils/core/cn";
+
+const CreateQuestionButton: FC<{ className?: string }> = ({ className }) => {
   const t = useTranslations();
 
   return (
-    <div className="flex h-full items-center rounded-full bg-blue-700 px-2 py-1 capitalize">
+    <div
+      className={cn(
+        "flex h-full items-center rounded-full bg-blue-700 px-2 py-1 capitalize",
+        className
+      )}
+    >
       <FontAwesomeIcon size="xs" className="mr-1" icon={faPlus} />
       {t("create")}
     </div>
