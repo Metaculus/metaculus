2ae77bae5bb27822b86e87e459c0adf81eb6d87a
lsabor <lukesabor@gmail.com>
2026-02-16 10:03:32 -0800

    remove unused constants duplication

--- notifications/constants.py
+++ notifications/constants.py
@@ -4,7 +4,6 @@ from django.db import models
 class MailingTags(models.TextChoices):
     # news
     WEEKLY_TOP_COMMENTS = "weekly_top_comments"
-    METACULUS_NEWS = "metaculus_news"
 
     # keeping up
     COMMENT_MENTIONS = "comment_mentions"
@@ -12,13 +11,3 @@ class MailingTags(models.TextChoices):
     TOURNAMENT_NEW_QUESTIONS = "tournament_new_questions"
     FORECASTED_CP_CHANGE = "cp_change"
     BEFORE_PREDICTION_AUTO_WITHDRAWAL = "before_prediction_auto_withdrawal"
-
-
-class MailingTags(models.TextChoices):
-    FORECASTED_QUESTION_RESOLUTION = "question_resolution"
-    COMMENT_MENTIONS = "comment_mentions"
-    # Forecasted post CP change
-    FORECASTED_CP_CHANGE = "cp_change"
-    TOURNAMENT_NEW_QUESTIONS = "tournament_new_questions"
-    BEFORE_PREDICTION_AUTO_WITHDRAWAL = "before_prediction_auto_withdrawal"
-    WEEKLY_TOP_COMMENTS = "weekly_top_comments"
