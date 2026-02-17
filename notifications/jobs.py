8bc9505a7635cf1f590492a01f0c0b1f4335a7a8
hlbmtc <hlib@metaculus.com>
2026-02-17 16:04:01 +0100

    Batch tournament open question notification emails every 30 minutes

--- notifications/jobs.py
+++ notifications/jobs.py
@@ -4,7 +4,11 @@ from collections import defaultdict
 import dramatiq
 
 from notifications.models import Notification
-from notifications.services import get_notification_handler_by_type
+from notifications.services import (
+    get_notification_handler_by_type,
+    NotificationPostStatusChange,
+)
+from posts.models import Post
 
 logger = logging.getLogger(__name__)
 
@@ -12,21 +16,47 @@ logger = logging.getLogger(__name__)
 @dramatiq.actor
 def job_send_notification_groups():
     """
-    Aggregates and sends grouped notifications to the recipients
+    Aggregates and sends grouped notifications to the recipients.
+    Excludes post_status_change notifications with event=open,
+    which are handled by job_send_open_status_notifications.
     """
 
     # { user_id: { NotificationType: list[Notification] } }
     grouped_notifications = defaultdict(lambda: defaultdict(list))
 
     # Grouping notifications
-    for notifications in Notification.objects.filter(
-        email_sent=False, read_at__isnull=True
+    for notification in Notification.objects.filter_pending_email().exclude(
+        type=NotificationPostStatusChange.type, params__event=Post.PostStatusChange.OPEN
     ):
-        grouped_notifications[notifications.recipient][notifications.type].append(
-            notifications
+        grouped_notifications[notification.recipient][notification.type].append(
+            notification
         )
 
-    # Running notifications scheduling
+    _send_grouped_notifications(grouped_notifications)
+
+
+@dramatiq.actor
+def job_send_open_status_notifications():
+    """
+    Sends only post_status_change notifications with event=open.
+    Runs every 30 minutes for faster delivery of OPEN status changes.
+    """
+
+    # { user_id: { NotificationType: list[Notification] } }
+    grouped_notifications = defaultdict(lambda: defaultdict(list))
+
+    for notification in Notification.objects.filter_pending_email().filter(
+        type=NotificationPostStatusChange.type,
+        params__event=Post.PostStatusChange.OPEN,
+    ):
+        grouped_notifications[notification.recipient][notification.type].append(
+            notification
+        )
+
+    _send_grouped_notifications(grouped_notifications)
+
+
+def _send_grouped_notifications(grouped_notifications):
     for recipient, groups in grouped_notifications.items():
         for notification_type, notifications in groups.items():
             handler_cls = get_notification_handler_by_type(notification_type)
