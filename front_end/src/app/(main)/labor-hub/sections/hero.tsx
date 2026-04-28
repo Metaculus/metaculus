9e411c66a4d513ef35f4d22d793c94d63f7e58e3
Cemre Inanc <cemreinanc@gmail.com>
2026-04-28 09:11:59 +0200

    Update labor hub content for forecast changes (#4664)
    * Update labor hub content for forecast changes
    
    * small fix
    
    * fix date

--- front_end/src/app/(main)/labor-hub/sections/hero.tsx
+++ front_end/src/app/(main)/labor-hub/sections/hero.tsx
@@ -35,7 +35,7 @@ export function HeroSection({
               </span>
             </div>
             <div className="flex items-center gap-1 text-xs text-blue-600 dark:text-blue-500-dark">
-              Commentary last updated: <time dateTime="2026-04-20">Apr 20</time>
+              Commentary last updated: <time dateTime="2026-04-28">Apr 28</time>
             </div>
           </div>
         </div>
