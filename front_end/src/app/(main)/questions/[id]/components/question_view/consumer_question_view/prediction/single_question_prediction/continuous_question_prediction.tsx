c168f7496631f9646da0c45ddc926b6c84e17df6
Nikita <nikita.develops.work@gmail.com>
2026-05-06 14:57:10 +0300

    fix: sort tailwind classes to satisfy prettier lint rule

--- front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/prediction/single_question_prediction/continuous_question_prediction.tsx
+++ front_end/src/app/(main)/questions/[id]/components/question_view/consumer_question_view/prediction/single_question_prediction/continuous_question_prediction.tsx
@@ -36,7 +36,7 @@ const ContinuousQuestionPrediction: React.FC<Props> = ({
       />
       {!shouldHideChart && (
         <>
-          <div className="w-full overflow-visible pb-1 md:pt-4 md:pb-4">
+          <div className="w-full overflow-visible pb-1 md:pb-4 md:pt-4">
             <div className="origin-center transform-gpu md:scale-150">
               <MinifiedContinuousAreaChart
                 question={question}
