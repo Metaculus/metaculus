5f9d734810aa04b785fef8d298e845cd6aa9fdf4
Hlib <hlib@metaculus.com>
2026-07-27 16:15:26 +0200

    Social auth flow: fixed accounts associations match (#5094)
    * Social auth flow: fixed accounts associations
    
    * Small fix

--- metaculus_web/settings.py
+++ metaculus_web/settings.py
@@ -253,7 +253,7 @@ SOCIAL_AUTH_PIPELINE = (
     "social_core.pipeline.social_auth.auth_allowed",
     "social_core.pipeline.social_auth.social_user",
     "social_core.pipeline.user.get_username",
-    "social_core.pipeline.social_auth.associate_by_email",
+    "authentication.social_pipeline.associate_by_email",
     "authentication.social_pipeline.create_user",
     "social_core.pipeline.social_auth.associate_user",
     "social_core.pipeline.social_auth.load_extra_data",
